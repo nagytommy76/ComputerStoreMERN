5029890634cd335d30365b1125d414041e493f0e
nagytommy76 <nagytommy76@gmail.com>
2021-07-26 19:55:33 +0200

    Axios interceptors not working as it should...

--- react-frontend/src/AxiosSetup/AxiosSetup.tsx
+++ react-frontend/src/AxiosSetup/AxiosSetup.tsx
@@ -1,14 +1,20 @@
 import axios from 'axios'
+import React from 'react'
+import { useAppSelector } from '../app/hooks'
 import { store } from '../app/store'
 import { generateNewAccessToken, logoutUser, setAccessToken } from '../app/slices/AuthSlice'
 
 export default function AxiosSetup(accessToken: string | null, refreshToken: string | null) {
+   // const accessToken2 = useAppSelector((state) => state.auth.accessToken)
    axios.defaults.baseURL = 'http://localhost:5050/api'
    axios.defaults.headers['Content-Type'] = 'Application/json'
-
    axios.interceptors.request.use(
       (config) => {
-         if (accessToken) config.headers.Authorization = `Barer ${accessToken}`
+         if (accessToken) {
+            console.log(accessToken)
+            config.headers.Authorization = `Barer ${accessToken}`
+            // console.log(accessToken)
+         }
          return config
       },
       (error) => Promise.reject(error)
@@ -23,12 +29,14 @@ export default function AxiosSetup(accessToken: string | null, refreshToken: str
             // Ekkor kell egy új accessToken (Forbidden)
             console.log('403 error, tehát lejárt az accessToken')
             return await axios.post('/auth/refresh-token', { refreshToken }).then((newAccessToken) => {
-               error.config._retry = true
                if (newAccessToken.status === 200) {
-                  console.log(newAccessToken.data)
+                  console.log(error.response)
+                  error.response.config._retry = true
+                  error.response.config.__isRetryRequest = true
+                  // console.log(newAccessToken.data)
                   store.dispatch(setAccessToken(newAccessToken.data))
-                  error.config.headers['Authorization'] = `Barer ${newAccessToken.data}`
-                  return axios(error.config)
+                  error.response.config.headers.Authorization = `Barer ${newAccessToken.data}`
+                  return axios(error.response.config)
                }
             })
 
@@ -47,6 +55,7 @@ export default function AxiosSetup(accessToken: string | null, refreshToken: str
       }
    )
 }
+
 // https://github.com/axios/axios/issues/934
 // https://github.com/axios/axios/issues/1266
 // https://medium.com/swlh/authentication-using-jwt-and-refresh-token-part-2-a86150d25152
