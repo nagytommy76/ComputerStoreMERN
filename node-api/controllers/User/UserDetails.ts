822cc696525553e3c232d503bf00a4cad4cbbfc5
nagytommy76 <nagytommy76@gmail.com>
2021-09-05 15:17:28 +0200

    Created a custom phone number validator & get user details controller

--- node-api/controllers/User/UserDetails.ts
+++ node-api/controllers/User/UserDetails.ts
@@ -24,4 +24,15 @@ export const insertUserDetailsController = async (req: RequestBodyType, res: Res
    }
 }
 
-export const getUserDetailsController = async (req: GetUserAuthInfoRequest, res: Response) => {}
+export const getUserDetailsController = async (req: GetUserAuthInfoRequest, res: Response) => {
+   try {
+      const foundUser = await User.findOne({ _id: req.user?._id })
+      if (foundUser == null) return res.status(404).json({ message: 'Felhasználó nem található' })
+      if (foundUser.userDetails.firstName == undefined || foundUser.userDetails.firstName == '') {
+         return res.status(200).json({ userDetails: null, isDetailsFilled: false })
+      }
+      return res.status(200).json({ userDetails: foundUser.userDetails, isDetailsFilled: true })
+   } catch (error) {
+      res.status(500).json(error)
+   }
+}
--- node-api/routes/api/User/Validators/UserDetailsValidator.ts
+++ node-api/routes/api/User/Validators/UserDetailsValidator.ts
@@ -1,16 +1,17 @@
-import { body } from 'express-validator'
+import { body, CustomValidator } from 'express-validator'
 
 const notEmptyFieldWithMessage = (fieldName: string, messageBody: string, minimum: number = 3) =>
    body(fieldName).isLength({ min: minimum }).trim().withMessage(`A(z) ${messageBody} mező kitöltése kötelező!`)
 
-const notZeroValueWithMessage = (fieldName: string, messageBody: string) =>
-   body(fieldName).isInt({ min: 1000, max: 9999 }).withMessage(`A(z) ${messageBody} mező nem lehet nulla!`)
-
-// 'hu-HU': /^(\+?36)(20|30|70)\d{7}$/ PHONE
+const phoneNumberValidator: CustomValidator = (value: string) => {
+   const regex = /^(\+?36|06)(1|20|30|50|70)\d{7}$/
+   if (!regex.test(value)) throw new Error('Csak magyar vezetékes/vonalas telefonszám formátum lehetséges')
+   return true
+}
 
 export const insertUserDetailsValidator = [
    body('userDetails.address.zipCode').isPostalCode('HU').withMessage(`Az irányítósám mezőnek 1000 és 9999 között kell elnnie`),
-   body('userDetails.phone').isMobilePhone('hu-HU').withMessage('Telefonszám formátum szükséges'),
+   body('userDetails.phone').custom(phoneNumberValidator),
    notEmptyFieldWithMessage('userDetails.firstName', 'Vezetéknév'),
    notEmptyFieldWithMessage('userDetails.lastName', 'Keresztnév'),
    notEmptyFieldWithMessage('userDetails.address.city', 'Város', 2),
