002cbb3b23116eed08ff35a6e44f94e765cec7f4
nagytommy76 <nagytommy76@gmail.com>
2021-09-19 13:05:50 +0200

    Added material UI to project

--- react-frontend/src/page/ShopPages/BaseComponents/ProductDetailsPage/Ratings/Rating.tsx
+++ react-frontend/src/page/ShopPages/BaseComponents/ProductDetailsPage/Ratings/Rating.tsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLocation } from 'react-router'
 import { LocationType } from '../../../BaseTypes'
 import { RatingContainer } from './RatingStyle'
+import { Rating as RatingMU, Typography, Button, TextField } from '@mui/material'
 
 const Rating = () => {
    const location = useLocation<LocationType>()
    const { _id } = location.state
+   const [rating, setRating] = useState<number | null>(null)
    return (
       <RatingContainer>
-         <h1>Itt lehe értékelni {_id}</h1>
+         <Typography>Értékelés</Typography>
+         <RatingMU
+            name='simple-controlled'
+            size='large'
+            value={rating}
+            onChange={(event, newValue) => {
+               setRating(newValue)
+            }}
+         />
+         <TextField label='Multiline' multiline rows={7} />
+         <Button color='success' variant='outlined' size='large'>
+            Külés
+         </Button>
       </RatingContainer>
    )
 }
--- react-frontend/src/page/ShopPages/BaseComponents/ProductDetailsPage/Ratings/RatingStyle.ts
+++ react-frontend/src/page/ShopPages/BaseComponents/ProductDetailsPage/Ratings/RatingStyle.ts
@@ -2,8 +2,12 @@ import styled from 'styled-components'
 
 export const RatingContainer = styled.div`
    width: 70%;
-   min-height: 40vh;
-   display: flex;
+   height: 35vh;
+   background-color: #fff;
+   border-radius: 5px;
+   margin: 0 auto 2rem auto;
+   /* display: flex;
    flex-direction: column;
    align-items: center;
+   justify-content: center; */
 `
