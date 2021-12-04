4dad0f1c83caa0d0e18fad9d9ad151ffc889ef53
nagytommy76 <nagytommy76@gmail.com>
2021-12-04 16:06:16 +0100

    Refactored the base table component, moved everything from vga/cpu delete component

--- react-frontend/src/page/Admin/Components/DeleteComponents/BaseTable.tsx
+++ react-frontend/src/page/Admin/Components/DeleteComponents/BaseTable.tsx
@@ -1,33 +1,56 @@
 import React from 'react'
+import useDeleteProduct from '../../Hooks/GetDeleteProduct'
+import usePaginate from '../../Hooks/Paginate'
+
+import { SnackbarStateTypes } from './Types'
 
 import Table from '@mui/material/Table'
 import TableContainer from '@mui/material/TableContainer'
+import TableBody from '@mui/material/TableBody'
 import TableRow from '@mui/material/TableRow'
 import TableFooter from '@mui/material/TableFooter'
 import TablePagination from '@mui/material/TablePagination'
 import Paper from '@mui/material/Paper'
 
 const TableHeadSection = React.lazy(() => import('./TableHead'))
+const SingleTableRow = React.lazy(() => import('./SingleTableRow'))
+const DeleteButton = React.lazy(() => import('./DeleteButton'))
 
 const BaseTable: React.FC<{
-   detailedProductLength: number
-   rowsPerPage: number
-   currentPage: number
-   handleChangePage: (event: React.MouseEvent<HTMLButtonElement, MouseEvent> | null, newPage: number) => void
-   handleChangeRowsPerPage: (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void
-}> = ({ children, detailedProductLength, rowsPerPage, currentPage, handleChangePage, handleChangeRowsPerPage }) => {
+   productType: string
+   setIsSnackOpen: React.Dispatch<React.SetStateAction<SnackbarStateTypes>>
+}> = ({ productType, setIsSnackOpen }) => {
+   const { setAllDetailedProduct, allDetailedProduct } = useDeleteProduct(productType)
+   const { rowsPerPage, currentPage, handleChangePage, handleChangeRowsPerPage } = usePaginate()
+
    return (
       <TableContainer component={Paper}>
          <Table sx={{ minWidth: 750, minHeight: 130 }}>
             <caption>Termékek törlése</caption>
             <TableHeadSection />
-            {children}
+            <TableBody>
+               {(rowsPerPage > 0
+                  ? allDetailedProduct.slice(currentPage * rowsPerPage, currentPage * rowsPerPage + rowsPerPage)
+                  : allDetailedProduct
+               ).map((product) => (
+                  <SingleTableRow product={product} key={product._id}>
+                     <DeleteButton
+                        productNameForSnackbar={product.type}
+                        setIsSnackOpen={setIsSnackOpen}
+                        allProducts={allDetailedProduct}
+                        setAllProducts={setAllDetailedProduct}
+                        productTypeForURL='cpu'
+                        productID={product._id}
+                     />
+                  </SingleTableRow>
+               ))}
+            </TableBody>
             <TableFooter>
                <TableRow>
                   <TablePagination
                      rowsPerPageOptions={[10, 25, 50, { label: 'Összes', value: -1 }]}
                      colSpan={6}
-                     count={detailedProductLength}
+                     count={allDetailedProduct.length}
                      rowsPerPage={rowsPerPage}
                      page={currentPage}
                      onPageChange={handleChangePage}
--- react-frontend/src/page/Admin/Vga/Delete/DeleteVga.tsx
+++ react-frontend/src/page/Admin/Vga/Delete/DeleteVga.tsx
@@ -1,47 +1,15 @@
 import React, { useState } from 'react'
-import useDeleteProduct from '../../Hooks/GetDeleteProduct'
-import usePaginate from '../../Hooks/Paginate'
-
 import { SnackbarStateTypes } from '../../Components/DeleteComponents/Types'
 
-import TableBody from '@mui/material/TableBody'
-
 const BaseTable = React.lazy(() => import('../../Components/DeleteComponents/BaseTable'))
-const SingleTableRow = React.lazy(() => import('../../Components/DeleteComponents/SingleTableRow'))
-const DeleteButton = React.lazy(() => import('../../Components/DeleteComponents/DeleteButton'))
 const SnackBar = React.lazy(() => import('../../Components/DeleteComponents/SnackBar'))
 
 const DeleteVga = () => {
-   const { setAllDetailedProduct, allDetailedProduct } = useDeleteProduct('vga')
-   const { rowsPerPage, currentPage, handleChangePage, handleChangeRowsPerPage } = usePaginate()
    const [isSnackOpen, setIsSnackOpen] = useState<SnackbarStateTypes>({ isOpen: false, deletedProductName: '' })
 
    return (
       <>
-         <BaseTable
-            currentPage={currentPage}
-            detailedProductLength={allDetailedProduct.length}
-            rowsPerPage={rowsPerPage}
-            handleChangePage={handleChangePage}
-            handleChangeRowsPerPage={handleChangeRowsPerPage}>
-            <TableBody>
-               {(rowsPerPage > 0
-                  ? allDetailedProduct.slice(currentPage * rowsPerPage, currentPage * rowsPerPage + rowsPerPage)
-                  : allDetailedProduct
-               ).map((product) => (
-                  <SingleTableRow product={product} key={product._id}>
-                     <DeleteButton
-                        productNameForSnackbar={product.type}
-                        setIsSnackOpen={setIsSnackOpen}
-                        allProducts={allDetailedProduct}
-                        setAllProducts={setAllDetailedProduct}
-                        productTypeForURL='cpu'
-                        productID={product._id}
-                     />
-                  </SingleTableRow>
-               ))}
-            </TableBody>
-         </BaseTable>
+         <BaseTable productType='vga' setIsSnackOpen={setIsSnackOpen} />
          <SnackBar isSnackOpen={isSnackOpen} setIsSnackOpen={setIsSnackOpen} />
       </>
    )
