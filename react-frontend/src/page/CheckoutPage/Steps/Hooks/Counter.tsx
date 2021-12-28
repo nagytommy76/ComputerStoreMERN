cbb49788718522263270613745e72cb54c9039e9
nagytommy76 <nagytommy76@gmail.com>
2021-12-28 11:56:09 +0100

    Fixed a useEffect memory leak bug in Counter hook

--- react-frontend/src/page/CheckoutPage/Steps/Hooks/Counter.tsx
+++ react-frontend/src/page/CheckoutPage/Steps/Hooks/Counter.tsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react'
 
 const useCounter = () => {
-   const [counter, setCounter] = useState(7)
+   const [counter, setCounter] = useState(5)
    const [startCounter, setStartCounter] = useState<boolean>(false)
 
    useEffect(() => {
       if (startCounter) {
          counter > 0 && setTimeout(() => setCounter(counter - 1), 1000)
+      }
+      return () => {
+         if (counter === 0) setStartCounter(false)
       }
    }, [counter, startCounter])
 
