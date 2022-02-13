2f4d24836370d5ddd7cd8f9017fe0f6f88b87445
nagytommy76 <nagytommy76@gmail.com>
2022-02-13 13:57:53 +0100

    Added the dispatches to side filter

--- react-frontend/src/page/ShopPages/Cpu/CpuShop.tsx
+++ react-frontend/src/page/ShopPages/Cpu/CpuShop.tsx
@@ -6,6 +6,14 @@ import {
    setSelectedBaseFrequencyRange,
    setCoreCounts,
    setSelectedCoreCountRange,
+   setTurboFrequencyRange,
+   setSelectedTurboFrequencyRange,
+   setL3CacheRange,
+   setSelectedL3CacheRange,
+   setTDPRange,
+   setSelectedTDPRange,
+   setThreadCounts,
+   setSelectedThreadCountRange,
 } from '../../../app/slices/Filter/CpuFilterSlice'
 
 const BaseShop = lazy(() => import('../BaseComponents/BaseShopPage/BaseShop'))
@@ -24,12 +32,26 @@ const CpuShop = () => {
    const extraDispatches = (params: IncomingParamsType) => {
       dispatch(setAllSockets(params.allSockets))
       const baseFrequencyRange = [params.minBaseFrequency, params.maxBaseFrequency]
+      const turboFrequencyRange = [params.minTurboFrequency, params.maxTurboFrequency]
       dispatch(setBaseFrequencyRange(baseFrequencyRange))
       dispatch(setSelectedBaseFrequencyRange(baseFrequencyRange))
+      dispatch(setTurboFrequencyRange(turboFrequencyRange))
+      dispatch(setSelectedTurboFrequencyRange(turboFrequencyRange))
 
       const coreCountRange = [params.minCoreCount, params.maxCoreCount]
       dispatch(setCoreCounts(coreCountRange))
       dispatch(setSelectedCoreCountRange(coreCountRange))
+      const threadRange = [params.minThreadCount, params.maxThreadCount]
+      dispatch(setThreadCounts(threadRange))
+      dispatch(setSelectedThreadCountRange(threadRange))
+
+      const l3CacheRange = [params.minL3Cache, params.maxL3Cache]
+      dispatch(setL3CacheRange(l3CacheRange))
+      dispatch(setSelectedL3CacheRange(l3CacheRange))
+
+      const tdpRange = [params.minTDP, params.maxTDP]
+      dispatch(setTDPRange(tdpRange))
+      dispatch(setSelectedTDPRange(tdpRange))
    }
 
    return (
