7e795177c1b42ca3c91dc413ea3191acd7924985
nagytommy76 <nagytommy76@gmail.com>
2022-02-15 19:15:45 +0100

    Reworked the BaseSelect too

--- react-frontend/src/page/ShopPages/BaseComponents/SideFilter/Base/BaseSelect.tsx
+++ react-frontend/src/page/ShopPages/BaseComponents/SideFilter/Base/BaseSelect.tsx
@@ -1,17 +1,25 @@
 import React from 'react'
+import { setIsPriceRangeSet } from '../../../../../app/slices/Filter/BaseFilterDataSlice'
+import { useAppDispatch } from '../../../../../app/hooks'
 
 import { InputContainer } from '../../../BaseComponents/SideFilter/FilterStyle'
 import TextField from '@mui/material/TextField'
+import { ActionCreatorWithPayload } from '@reduxjs/toolkit'
 
 const BySelect: React.FC<Props> = ({
    labelText,
    helperText,
    allOption,
    selectedOption,
-   handleChange,
+   setSelectedDispatchValue,
    postFix,
    children,
 }) => {
+   const dispatch = useAppDispatch()
+   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      dispatch(setSelectedDispatchValue(event.target.value))
+      dispatch(setIsPriceRangeSet(true))
+   }
    return (
       <InputContainer>
          <TextField
@@ -47,6 +55,6 @@ type Props = {
    helperText: string
    allOption: string[] | number[]
    selectedOption: string | number
-   handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+   setSelectedDispatchValue: ActionCreatorWithPayload<string, string>
    postFix?: string
 }
