fdf70946a5efb8045c9eccf161affd1754b421ca
nagytommy76 <nagytommy76@gmail.com>
2022-02-16 19:53:58 +0100

    Added a type definiton && finished the extraDispatches function

--- react-frontend/src/page/ShopPages/Vga/Hook/useExtraDispatch.tsx
+++ react-frontend/src/page/ShopPages/Vga/Hook/useExtraDispatch.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useAppDispatch } from '../../../../app/hooks'
 import {
    setBaseClockRange,
@@ -9,13 +8,10 @@ import {
    setPowerConsuptions,
    setSelectedBaseClockRange,
    setSelectedBoostClockRange,
-   setSelectedGpuManufacturer,
    setSelectedLength,
-   setSelectedPcie,
    setSelectedPowerConsuption,
    setSelectedVramBandwidth,
    setSelectedVramCapRange,
-   setSelectedVramType,
    setVramBandwidths,
    setVramCapacitiyRange,
    setVramTypes,
@@ -24,11 +20,55 @@ import {
 const useExtraDispatch = () => {
    const dispatch = useAppDispatch()
 
-   const extraDispatches = (params: IncomingParamsType) => {}
+   const extraDispatches = (params: IncomingParamsType) => {
+      dispatch(setGpuManufacturers(params.gpuManufacturer))
+      dispatch(setPcieTypes(params.pciType))
+      dispatch(setVramTypes(params.vramType))
+
+      const baseClockRange = [params.minBaseClock, params.maxBaseClock]
+      dispatch(setBaseClockRange(baseClockRange))
+      dispatch(setSelectedBaseClockRange(baseClockRange))
+
+      const boostClockRange = [params.minBoostClock, params.maxBoostClock]
+      dispatch(setBoostClockRange(boostClockRange))
+      dispatch(setSelectedBoostClockRange(boostClockRange))
+
+      const lengthRange = [params.minLength, params.maxLength]
+      dispatch(setLengths(lengthRange))
+      dispatch(setSelectedLength(lengthRange))
+
+      const TDPRange = [params.minTdp, params.maxTdp]
+      dispatch(setPowerConsuptions(TDPRange))
+      dispatch(setSelectedPowerConsuption(TDPRange))
+
+      const vramBandwidthRange = [params.minVramBandwidth, params.maxVramBandwidth]
+      dispatch(setVramBandwidths(vramBandwidthRange))
+      dispatch(setSelectedVramBandwidth(vramBandwidthRange))
+
+      const vramCapacityRange = [params.minVramCapacity, params.maxVramCapacity]
+      dispatch(setVramCapacitiyRange(vramCapacityRange))
+      dispatch(setSelectedVramCapRange(vramCapacityRange))
+   }
 
    return extraDispatches
 }
 
 export default useExtraDispatch
 
-type IncomingParamsType = {}
+type IncomingParamsType = {
+   minBaseClock: number
+   maxBaseClock: number
+   minBoostClock: number
+   maxBoostClock: number
+   gpuManufacturer: string[]
+   minLength: number
+   maxLength: number
+   pciType: string[]
+   minTdp: number
+   maxTdp: number
+   minVramBandwidth: number
+   maxVramBandwidth: number
+   minVramCapacity: number
+   maxVramCapacity: number
+   vramType: string[]
+}
