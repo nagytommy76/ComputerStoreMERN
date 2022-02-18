504a58bc86f8d10bad944dea71aa82de709bf4b0
nagytommy76 <nagytommy76@gmail.com>
2022-02-18 19:41:06 +0100

    Added rating routes
    - get-memory-rates
    - get-memory-comments

--- node-api/routes/api/Memory/Memory.ts
+++ node-api/routes/api/Memory/Memory.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express'
 import MemoryProduct from '../../../controllers/Products/Memory/Memory'
+import {
+   getAllMemoryComments,
+   getMemoryRatingSummaryController,
+} from '../../../controllers/Products/Memory/MemoryRatings'
 
 const router = Router()
 const memoryProduct = new MemoryProduct()
 
 router.get('/', memoryProduct.getAllMemoryProductController)
 router.get('/filter-data', memoryProduct.getMemoryFilterData)
+
+// Ratings
+router.get('/get-memory-rates', getMemoryRatingSummaryController)
+router.get('/get-memory-comments', getAllMemoryComments)
 
 module.exports = router
