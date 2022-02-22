6e92f9aa105be015d6f67ae4fbb048b1eb513f4a
nagytommy76 <nagytommy76@gmail.com>
2022-02-22 19:48:25 +0100

    Imported the base admin controller functions

--- node-api/controllers/Admin/Cpu/AdminCpucontroller.ts
+++ node-api/controllers/Admin/Cpu/AdminCpucontroller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import { CpuProduct } from '../../../models/Products/Cpu/CpuSchema'
 import { CpuProductType } from '../../../models/Products/Cpu/CpuTypes'
+import baseAdminController from '../BaseController'
 
 type RequestWithBodyType = Request & {
    body: CpuProductType
@@ -12,9 +13,11 @@ type RequestWithCpuID = Request & {
    }
 }
 
+const AdminController = baseAdminController(CpuProduct)
+
 export const getAllCpuItemsController = async (req: Request, res: Response) => {
    try {
-      const cpuProducts = await CpuProduct.find()
+      const cpuProducts = await AdminController.getAllProduct()
       return res.status(200).json({ allProducts: cpuProducts })
    } catch (error) {
       return res.status(500).json(error)
@@ -23,36 +26,31 @@ export const getAllCpuItemsController = async (req: Request, res: Response) => {
 
 export const insertCpuController = async (req: RequestWithBodyType, res: Response) => {
    try {
-      const createdCpu = new CpuProduct({
-         itemNumber: req.body.itemNumber,
-         type: req.body.type,
-         typeCode: req.body.typeCode,
-         manufacturer: req.body.manufacturer,
-         price: req.body.price,
-         pictureUrls: req.body.pictureUrls,
-         isHighlighted: req.body.isHighlighted,
-         details: {
-            coreCount: req.body.details.coreCount,
-            threadCount: req.body.details.threadCount,
-            baseClock: req.body.details.baseClock,
-            boostClock: req.body.details.boostClock,
-            TDP: req.body.details.TDP,
-            l2Cache: req.body.details.l2Cache,
-            l3Cache: req.body.details.l3Cache,
-            socket: req.body.details.socket,
-            manufacturerUrl: req.body.details.manufacturerUrl,
-            description: req.body.details.description,
-            integratedGraphicsName: req.body.details.integratedGraphicsName,
-            manufacturerPageUrl: req.body.details.manufacturerPageUrl,
-            architecture: req.body.details.architecture,
-            cpuCodeName: req.body.details.cpuCodeName,
-            stockCooler: req.body.details.stockCooler,
-            stockCoolerName: req.body.details.stockCoolerName,
-            warranity: req.body.warranity
-         }
-      })
-      await createdCpu.save()
-      return res.sendStatus(201)
+      const {
+         itemNumber,
+         type,
+         typeCode,
+         manufacturer,
+         price,
+         pictureUrls,
+         isHighlighted,
+         inStockQuantity,
+         details,
+      } = req.body as CpuProductType
+      const toInsertProperties = {
+         itemNumber: itemNumber,
+         type: type,
+         typeCode: typeCode,
+         manufacturer: manufacturer,
+         price: price,
+         pictureUrls: pictureUrls,
+         isHighlighted: isHighlighted,
+         ratingValues: [],
+         inStockQuantity,
+      }
+      const createdCpu = await AdminController.insert(details, toInsertProperties)
+      const saved = await createdCpu.save()
+      if (saved) return res.sendStatus(201)
    } catch (error) {
       return res.status(500).json(error)
    }
@@ -60,23 +58,20 @@ export const insertCpuController = async (req: RequestWithBodyType, res: Respons
 
 export const modifyCpuProductController = async (req: Request, res: Response) => {
    try {
-      CpuProduct.findById(req.body._id)
-         .then((cpu) => {
-            if (cpu) {
-               cpu.details = req.body.details
-               cpu.itemNumber = req.body.itemNumber
-               cpu.type = req.body.type
-               cpu.typeCode = req.body.typeCode
-               cpu.manufacturer = req.body.manufacturer
-               cpu.price = req.body.price
-               cpu.pictureUrls = req.body.pictureUrls
-               cpu.inStockQuantity = req.body.inStockQuantity
-               cpu.isHighlighted = req.body.isHighlighted
-               cpu.save()
-            }
-         })
-         .catch((errors) => console.log(errors))
-      return res.sendStatus(201)
+      const toModifyCpuProduct = await AdminController.getProductToModify(req.body._id)
+      if (toModifyCpuProduct) {
+         toModifyCpuProduct.details = req.body.details
+         toModifyCpuProduct.itemNumber = req.body.itemNumber
+         toModifyCpuProduct.type = req.body.type
+         toModifyCpuProduct.typeCode = req.body.typeCode
+         toModifyCpuProduct.manufacturer = req.body.manufacturer
+         toModifyCpuProduct.price = req.body.price
+         toModifyCpuProduct.pictureUrls = req.body.pictureUrls
+         toModifyCpuProduct.inStockQuantity = req.body.inStockQuantity
+         toModifyCpuProduct.isHighlighted = req.body.isHighlighted
+         toModifyCpuProduct.save()
+         return res.sendStatus(201)
+      } else return res.sendStatus(404)
    } catch (error) {
       return res.status(500).json(error)
    }
@@ -86,22 +81,17 @@ export const modifyCpuProductController = async (req: Request, res: Response) =>
 
 export const getAllCpuItemsForDeleteController = async (req: Request, res: Response) => {
    try {
-      const cpuProducts = await CpuProduct.find()
-         .select(['manufacturer', 'price', 'type', 'inStockQuantity'])
-         .sort({ price: 'asc' })
-      return res.status(200).json({ allProducts: cpuProducts })
+      const cpuProducts = await AdminController.getAllToDeleteProducts()
+      res.status(200).json({ allProducts: cpuProducts })
    } catch (error) {
       return res.status(500).json(error)
    }
 }
 
-export const deleteCpuProductByIdController = (req: RequestWithCpuID, res: Response) => {
+export const deleteCpuProductByIdController = async (req: RequestWithCpuID, res: Response) => {
    try {
-      CpuProduct.findByIdAndRemove(req.body.productID)
-         .then(() => {
-            return res.status(200).json({ msg: 'sikeres törlés', deleted: true })
-         })
-         .catch((error) => res.status(500).json(error))
+      await AdminController.delete(req.body.productID)
+      return res.status(200).json({ msg: 'sikeres törlés', deleted: true })
    } catch (error) {
       return res.status(500).json(error)
    }
