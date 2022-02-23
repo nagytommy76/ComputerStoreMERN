e4d1d545d63d55bc819172d9922a27ec1dc3838c
nagytommy76 <nagytommy76@gmail.com>
2022-02-23 19:58:44 +0100

    Finished the 3 newly created functions (modify/getToDelete/delete)

--- node-api/controllers/Admin/Memory/Memory.ts
+++ node-api/controllers/Admin/Memory/Memory.ts
@@ -45,12 +45,59 @@ export const insertMemoryProduct = async (request: Request, response: Response) 
    }
 }
 
-export const modifyMemoryProductController = async (request: Request, response: Response) => {}
+export const modifyMemoryProductController = async (request: Request, response: Response) => {
+   try {
+      const {
+         _id,
+         details,
+         inStockQuantity,
+         manufacturer,
+         pictureUrls,
+         price,
+         type,
+         isHighlighted,
+         itemNumber,
+         typeCode,
+      } = request.body as BodyType
+      const foundMemoryProduct = await AdminController.getProductToModify(_id)
+      if (foundMemoryProduct) {
+         foundMemoryProduct.details = details
+         foundMemoryProduct.inStockQuantity = inStockQuantity
+         foundMemoryProduct.manufacturer = manufacturer
+         foundMemoryProduct.pictureUrls = pictureUrls
+         foundMemoryProduct.price = price
+         foundMemoryProduct.type = type
+         foundMemoryProduct.isHighlighted = isHighlighted
+         foundMemoryProduct.itemNumber = itemNumber
+         foundMemoryProduct.typeCode = typeCode
+         foundMemoryProduct.save()
+         return response.sendStatus(201)
+      }
+      response.sendStatus(404)
+   } catch (error) {
+      response.status(500).json(error)
+   }
+}
 
-export const getAllMemoryItemsForDeleteController = async (request: Request, response: Response) => {}
+export const getAllMemoryItemsForDeleteController = async (request: Request, response: Response) => {
+   try {
+      const allMemories = await AdminController.getAllToDeleteProducts()
+      return response.status(200).json({ allProducts: allMemories })
+   } catch (error) {
+      response.status(500).json(error)
+   }
+}
 
-export const deleteMemoryProductByIdController = async (request: Request, response: Response) => {}
+export const deleteMemoryProductByIdController = async (request: Request, response: Response) => {
+   try {
+      await AdminController.delete(request.body.productID)
+      response.status(200).json({ msg: 'sikeres törlés', deleted: true })
+   } catch (error) {
+      response.status(500).json(error)
+   }
+}
 
 type BodyType = BaseProductProperties & {
+   _id: string
    details: MemoryDetails
 }
