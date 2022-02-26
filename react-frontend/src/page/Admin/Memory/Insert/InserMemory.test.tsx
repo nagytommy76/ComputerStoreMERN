b6c848d30fef20ba36c1418beca4731bbb648298
nagytommy76 <nagytommy76@gmail.com>
2022-02-26 15:43:01 +0100

    Tested the inputs and responses

--- react-frontend/src/page/Admin/Memory/Insert/InserMemory.test.tsx
+++ react-frontend/src/page/Admin/Memory/Insert/InserMemory.test.tsx
@@ -5,6 +5,7 @@ import MemoryInsert from './MemoryInsert'
 import axios from 'axios'
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
+// jest.setTimeout(10000)
 
 const mockResolvedInsert = {
    status: 201,
@@ -22,22 +23,12 @@ describe('Testing memory insert', () => {
       screen.getByRole('checkbox', { name: /A termék ki legyen emelve a főoldalon/i })
       screen.getByRole('option', { name: /DDR4/i })
    })
-   test('The new link button should create a new input field', async () => {
-      render(<MemoryInsert />)
-      const newLinkButton = screen.getByRole('button', { name: /Új link/i })
-      userEvent.click(newLinkButton)
-      const linkInput = await screen.findByRole('textbox', { name: /Kép URL/i })
-      userEvent.type(
-         linkInput,
-         'https://media.icdn.hu/product/GalleryMod/2021-07/709351/resp/1675337_kingston_fury_16gb_beast_ddr4_3200mhz_cl16_kit_kf432c16bbk2_16.webp'
-      )
-   })
    test('the admin should enter values into the inputs, and send to the server', async () => {
       render(<MemoryInsert />)
       const isHighlighted = await screen.findByRole('checkbox', {
          name: /A termék ki legyen emelve a főoldalon/i,
       })
-      const itemNumber = screen.getByRole('textbox', { name: /Termék szám/i })
+      const itemNumber = await screen.findByRole('textbox', { name: /Termék szám/i })
       const typeName = screen.getByRole('textbox', { name: /Típus név/i })
       const typeCode = screen.getByRole('textbox', { name: /Típus kód/i })
       const itemManufacturer = screen.getByRole('textbox', { name: /Termék gyártó/i })
@@ -52,9 +43,38 @@ describe('Testing memory insert', () => {
       const productManPage = screen.getByRole('textbox', { name: /Gyártói honlap link/i })
       const description = screen.getByRole('textbox', { name: /Leírás/i })
 
-      const memoryTypes = screen.getAllByRole('option', { name: /DDR/i })
-      const linkInput = await screen.findByRole('textbox', { name: /Kép URL/i })
+      const memoryTypes = screen.getByRole('combobox')
 
-      //   screen.debug('')
-   })
+      expect(screen.queryByRole('textbox', { name: /Kép url/i })).not.toBeInTheDocument()
+      const newLinkButton = screen.getByRole('button', { name: /Új link/i })
+      userEvent.click(newLinkButton)
+      const linkInput = await screen.findByRole('textbox', { name: /Kép url/i })
+      userEvent.type(
+         linkInput,
+         'https://media.icdn.hu/product/GalleryMod/2021-07/709351/resp/1675337_kingston_fury_16gb_beast_ddr4_3200mhz_cl16_kit_kf432c16bbk2_16.webp'
+      )
+
+      userEvent.type(itemNumber, 'semmi')
+      userEvent.type(typeName, 'Fury 16GB Beast DDR4 3200MHz CL16 KIT')
+      userEvent.type(typeCode, 'KF432C16BBK2/16')
+      userEvent.type(itemManufacturer, 'KINGSTON')
+      userEvent.type(price, '27990')
+      userEvent.type(inStockQty, '2')
+      userEvent.type(warranity, '24')
+      userEvent.type(capacity, '16')
+      userEvent.type(frequency, '3200')
+      userEvent.type(latency, '16')
+      userEvent.type(voltage, '1.35')
+      userEvent.type(moduleNumber, '2')
+      userEvent.type(productManPage, 'https://www.kingston.com/dataSheets/KF432C16BBK2_16.pdf')
+      userEvent.type(
+         description,
+         '2 darab Kingston Fury, összesen 16GB DDR4 3200Mhz memória CL16 késleltetéssel.'
+      )
+      mockedAxios.post.mockResolvedValue(mockResolvedInsert)
+      userEvent.selectOptions(memoryTypes, 'DDR4')
+      userEvent.click(screen.getByRole('button', { name: /Bevitel/i }))
+      expect(await screen.findByText(/sikeres bevitel/i)).toBeInTheDocument()
+      // screen.getByRole('')
+   }, 50000)
 })
