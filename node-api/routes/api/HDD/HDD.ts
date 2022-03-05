fa06fc8a311a1117626c136055e4584dc85ef8c4
nagytommy76 <nagytommy76@gmail.com>
2022-03-05 10:30:31 +0100

    Created the filterData controller

--- node-api/routes/api/HDD/HDD.ts
+++ node-api/routes/api/HDD/HDD.ts
@@ -1,4 +1,10 @@
 import { Router } from 'express'
 import { authenticateAccessToken } from '../../../middlewares/AuthenticateAccessOrRefreshTokens'
+import HDDProduct from '../../../controllers/Products/HDD/HDD'
+
+const hddProduct = new HDDProduct()
 
 const router = Router()
+
+router.get('/', hddProduct.getAllHDDProductController)
+router.get('/filter-data', hddProduct.getHDDFilterData)
