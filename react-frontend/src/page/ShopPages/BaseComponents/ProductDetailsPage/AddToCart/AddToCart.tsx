6b75e341e31650f1124c862bf54efc15512baaf7
nagytommy76 <nagytommy76@gmail.com>
2022-03-06 13:00:57 +0100

    Replaced useLocation to props

--- react-frontend/src/page/ShopPages/BaseComponents/ProductDetailsPage/AddToCart/AddToCart.tsx
+++ react-frontend/src/page/ShopPages/BaseComponents/ProductDetailsPage/AddToCart/AddToCart.tsx
@@ -1,9 +1,6 @@
 import React, { useState } from 'react'
-import { useLocation } from 'react-router-dom'
-
 import { sendCartItemToSaveInDB } from '../../../../../app/slices/CartSlice'
 import { useAppDispatch } from '../../../../../app/hooks'
-import { LocationType } from '../../../BaseTypes'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
@@ -14,12 +11,18 @@ import {
    StyledLabel,
 } from './CartStyle'
 
-const AddToCart = () => {
+const AddToCart: React.FC<{
+   _id: string
+   manufacturer: string
+   type: string
+   typeCode: string
+   price: number
+   pictureUrls: string[]
+   productType: string
+}> = ({ _id, manufacturer, pictureUrls, price, productType, type, typeCode }) => {
    const dispatch = useAppDispatch()
    const [quantity, setQuantity] = useState<string>('1')
-   let location = useLocation()
-   const state = location.state as LocationType
-   const { _id, manufacturer, type, typeCode, price, pictureUrls, productType } = state
+
    const addItemToCart = () => {
       dispatch(
          sendCartItemToSaveInDB({
