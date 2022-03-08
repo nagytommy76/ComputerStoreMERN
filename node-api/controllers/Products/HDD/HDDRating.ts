f8ca015cf9261ad7515d27ecd59b18802aaef74c
nagytommy76 <nagytommy76@gmail.com>
2022-03-08 20:10:27 +0100

    Created the rating controller

--- node-api/controllers/Products/HDD/HDDRating.ts
+++ node-api/controllers/Products/HDD/HDDRating.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 import { HddProduct } from '../../../models/Products/HDD/Hdd'
 import { RequestWithQueryId } from '../../Types'
-import BaseRatingController from '../Ratings/BaseRating'
+import BaseRatingController, { RateQueryRequest } from '../Ratings/BaseRating'
 
 const BaseRating = BaseRatingController(HddProduct)
 
@@ -20,5 +20,23 @@ export const getAllHDDComments = async (req: RequestWithQueryId, res: Response) 
       return res.status(200).json(allComments.ratingValues)
    } catch (error) {
       return res.status(500).json(error)
+   }
+}
+
+export const rateHDDProductController = async (req: RateQueryRequest, res: Response) => {
+   try {
+      const modifiedProduct = await BaseRating.saveRateProductHelper(
+         req.body.productId,
+         req.body.rating,
+         req.body.comment,
+         req.body.userName,
+         req.user?._id
+      )
+      if (modifiedProduct !== undefined) {
+         modifiedProduct.save()
+         return res.sendStatus(201)
+      } else return res.sendStatus(422)
+   } catch (error) {
+      return res.status(500).json({ error })
    }
 }
