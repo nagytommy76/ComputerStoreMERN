c26f09ad6b6b0d299bad035f666e42bea00b6e25
nagytommy76 <nagytommy76@gmail.com>
2022-03-14 10:34:27 +0100

    Added the save/removeProductAnswerController

--- node-api/controllers/Products/Ratings/BaseResponse.ts
+++ node-api/controllers/Products/Ratings/BaseResponse.ts
@@ -1,48 +1,48 @@
-import { Response, Request } from 'express'
-import { Model } from 'mongoose'
+import { ObjectId } from 'mongoose'
 import { RatingValues } from '../../../models/Products/BaseTypes'
 
 import { UserTypes } from '../../../models/User/UserTypes'
 import { CommentAnswerType } from '../../../models/Products/BaseTypes'
 
-export type SaveRequesType = Request & {
-   user?: UserTypes
-   body: {
-      productId: string
-      commentId: string
-      answer: string
-   }
-}
+export const canSaveProductAnswer = (getRatingValuesByProductId: (productId: ObjectId) => Promise<any>) => ({
+   saveProductAnswerController: async (
+      productId: ObjectId,
+      commentId: ObjectId,
+      answer: string,
+      user: UserTypes | undefined
+   ) => {
+      const foundProduct = await getRatingValuesByProductId(productId)
+      const foundComment = foundProduct.ratingValues.find((comment: RatingValues) => comment._id == commentId)
+      const foundCommentIndex = foundProduct.ratingValues.findIndex(
+         (comment: RatingValues) => comment._id == commentId
+      )
+      if (foundComment && user) {
+         foundComment.commentAnswers.push({
+            answer: answer,
+            answeredAt: new Date(),
+            userId: user._id,
+            userName: user.userName,
+         })
+      }
+      return {
+         newCommentAnswers: foundProduct.ratingValues[foundCommentIndex].commentAnswers,
+         foundProduct,
+      }
+   },
+})
 
-export type RemoveRequesType = Request & {
-   user?: UserTypes
-   body: {
-      productId: string
-      commentId: string
-      answerId: string
-   }
-}
-
-export const saveProductAnswerController = (req: SaveRequesType, foundProduct: any) => {
-   const foundComment = foundProduct.ratingValues.find((comment: RatingValues) => comment._id == req.body.commentId)
-   const foundCommentIndex = foundProduct.ratingValues.findIndex((comment: RatingValues) => comment._id == req.body.commentId)
-   if (foundComment && req.user) {
-      foundComment.commentAnswers.push({
-         answer: req.body.answer,
-         answeredAt: new Date(),
-         userId: req.user._id,
-         userName: req.user.userName
-      })
-   }
-   return foundProduct.ratingValues[foundCommentIndex].commentAnswers
-}
-
-// Remove Answer
-export const removeProductAnswerController = (req: RemoveRequesType, foundProduct: any) => {
-   const foundComment = foundProduct.ratingValues.find((comment: RatingValues) => comment._id == req.body.commentId)
-   if (foundComment) {
-      const filteredAnswers = foundComment.commentAnswers.filter((answer: CommentAnswerType) => answer._id != req.body.answerId)
-      foundComment.commentAnswers = filteredAnswers
-   }
-   return foundComment.commentAnswers
-}
+export const canRemoveProductAnswer = (
+   getRatingValuesByProductId: (productId: ObjectId) => Promise<any>
+) => ({
+   removeProductAnswerController: async (productId: ObjectId, commentId: ObjectId, answerId: ObjectId) => {
+      const foundProduct = await getRatingValuesByProductId(productId)
+      const foundComment = foundProduct.ratingValues.find((comment: RatingValues) => comment._id == commentId)
+      if (foundComment) {
+         const filteredAnswers = foundComment.commentAnswers.filter(
+            (answer: CommentAnswerType) => answer._id != answerId
+         )
+         foundComment.commentAnswers = filteredAnswers
+      }
+      return { foundComment: foundComment.commentAnswers, foundProduct }
+   },
+})
