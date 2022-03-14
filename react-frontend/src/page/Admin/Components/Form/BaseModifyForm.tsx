77dbf783dc8d85c09df2d5c2bb74df1e7a390fa9
nagytommy76 <nagytommy76@gmail.com>
2022-03-14 11:37:32 +0100

    Added base components: ProductSelector, PicUrlInput, Description to simplify the product modify page

--- react-frontend/src/page/Admin/Components/Form/BaseModifyForm.tsx
+++ react-frontend/src/page/Admin/Components/Form/BaseModifyForm.tsx
@@ -1,43 +1,63 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, lazy } from 'react'
 import { AdminContext } from '../../Context/AdminContext'
 import axios from 'axios'
 import { ValidationErrorWithAxiosError } from '../../Vga/Types'
 import { StyledForm } from '../../Components/Form/FormStyle'
 
+import useAdminStates from '../../Hooks/AdminStates'
+
 const BaseForm = React.lazy(() => import('./BaseForm'))
 const SubmitButton = React.lazy(() => import('../../Components/InputFields/SubmitButton/SubmitButton'))
+
+const ProductSelector = lazy(() => import('../InputFields/ProductSelector/ProductSelector'))
+const DescriptionTextArea = lazy(() => import('../InputFields/TextArea/DescriptionTextArea'))
+const PicUrlInput = lazy(() => import('../InputFields/PicUrlInput/PicUrlInput'))
 
 const BaseModifyForm: React.FC<{
    mainTitle: string
    productType: string
    submitButtonText: string
-}> = ({ mainTitle, productType, submitButtonText, children }) => {
+   productProperties: any
+}> = ({ mainTitle, productType, submitButtonText, productProperties, children }) => {
+   const states = useAdminStates(productProperties)
    const [inputSuccess, setInputSuccess] = useState<boolean>(false)
-   const { productInputs, selectedProductPictureUrls, setValidationErrors } = useContext(AdminContext)
 
    const sendModifyRequest = (event: React.FormEvent) => {
       event.preventDefault()
-      const filteredPictureArray = selectedProductPictureUrls.map(x => x.pictureUrl)
+      const filteredPictureArray = states.selectedProductPictureUrls.map(x => x.pictureUrl)
       axios
-         .post(`admin/${productType}/modify`, { ...productInputs, pictureUrls: filteredPictureArray })
+         .post(`admin/${productType}/modify`, { ...states.productInputs, pictureUrls: filteredPictureArray })
          .then(() => setInputSuccess(true))
          .catch((errors: ValidationErrorWithAxiosError) => {
-            if (errors.response?.data) setValidationErrors(errors.response?.data.errors)
+            if (errors.response?.data) states.setValidationErrors(errors.response?.data.errors)
          })
    }
-
    return (
-      <BaseForm
-         alertTextAndServerity={{ serverity: 'info', text: 'Sikeres módosítás!' }}
-         inputSuccess={inputSuccess}
-         mainTitle={mainTitle}
-         setInputSuccess={setInputSuccess}
-      >
-         <StyledForm onSubmit={sendModifyRequest}>
-            {children}
-            <SubmitButton>{submitButtonText}</SubmitButton>
-         </StyledForm>
-      </BaseForm>
+      <AdminContext.Provider value={states}>
+         <BaseForm
+            alertTextAndServerity={{ serverity: 'info', text: 'Sikeres módosítás!' }}
+            inputSuccess={inputSuccess}
+            mainTitle={mainTitle}
+            setInputSuccess={setInputSuccess}
+         >
+            <StyledForm onSubmit={sendModifyRequest}>
+               <ProductSelector productProperties={productProperties} productType={productType} />
+               {children}
+               <DescriptionTextArea
+                  labelText='Leírás'
+                  value={states.productInputs.details.description || ''}
+                  onChangeEvent={event =>
+                     states.setProductInputs({
+                        ...states.productInputs,
+                        details: { ...states.productInputs.details, description: event.target.value },
+                     })
+                  }
+               />
+               <PicUrlInput />
+               <SubmitButton>{submitButtonText}</SubmitButton>
+            </StyledForm>
+         </BaseForm>
+      </AdminContext.Provider>
    )
 }
 
