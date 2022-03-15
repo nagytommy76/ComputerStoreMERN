984d25cc000d963d913bdf8e1ea4851bbaed0e9b
nagytommy76 <nagytommy76@gmail.com>
2022-03-15 15:29:24 +0100

    Moved the setLoading inside the if statement

--- react-frontend/src/page/Auth/Hooks/useRegister.tsx
+++ react-frontend/src/page/Auth/Hooks/useRegister.tsx
@@ -21,28 +21,34 @@ const useRegister = () => {
 
    const registerUser = (event: React.FormEvent) => {
       event.preventDefault()
-      setIsLoadingForResponse(true)
       resetErrors()
       // Hibaüzenetek:
-      if (userName.value === '') return setUserName({ ...userName, hasError: true, errorMessage: 'Kérem a Felhasználónevet!' })
-      if (email.value === '') return setEmail({ ...email, hasError: true, errorMessage: 'Kérem az e-mail címet' })
+      if (userName.value === '')
+         return setUserName({ ...userName, hasError: true, errorMessage: 'Kérem a Felhasználónevet!' })
+      if (email.value === '')
+         return setEmail({ ...email, hasError: true, errorMessage: 'Kérem az e-mail címet' })
       if (firstPassword.value === '')
          return setFirstPassword({ ...firstPassword, hasError: true, errorMessage: 'Kérem a jelszó' })
       if (secondPassword.value === '')
-         return setSecondPassword({ ...secondPassword, hasError: true, errorMessage: 'Kérem a második jelszót' })
+         return setSecondPassword({
+            ...secondPassword,
+            hasError: true,
+            errorMessage: 'Kérem a második jelszót',
+         })
       if (!userName.hasError && !email.hasError && !firstPassword.hasError && !secondPassword.hasError) {
+         setIsLoadingForResponse(true)
          axios
             .post('/auth/register', {
                email: email.value,
                userName: userName.value,
                firstPassword: firstPassword.value,
-               secondPassword: secondPassword.value
+               secondPassword: secondPassword.value,
             })
             .then((response: AxiosResponse) => {
                if (response.status === 201) {
                   setIsLoadingForResponse(false)
                   navigate('/login', {
-                     state: { isSuccess: true, message: response.data.message }
+                     state: { isSuccess: true, message: response.data.message },
                   })
                }
             })
@@ -71,7 +77,7 @@ const useRegister = () => {
       firstPassword,
       setFirstPassword,
       secondPassword,
-      setSecondPassword
+      setSecondPassword,
    }
 }
 
