89243f60abea00a1787bf9722854eda28572d40d
nagytommy76 <nagytommy76@gmail.com>
2022-03-17 19:40:40 +0100

    replaced the prop to useContext

--- react-frontend/src/page/Navbar/Cart/CartButton/CartButton.tsx
+++ react-frontend/src/page/Navbar/Cart/CartButton/CartButton.tsx
@@ -1,18 +1,24 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState, useContext } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { NavbarCartButtonStyle, ItemsInCartStyle } from './CartButtonStyle'
 import { useAppSelector } from '../../../../app/hooks'
 import { CSSTransition } from 'react-transition-group'
 import styles from './Qty.module.css'
+import { NavbarContext } from '../../NavbarContext'
 
-const CartButton: React.FC<Props> = ({ onClickEvent }) => {
-   const { totalQuantity, cartItems } = useAppSelector((state) => state.cart)
+const CartButton: React.FC = () => {
+   const { totalQuantity, cartItems } = useAppSelector(state => state.cart)
+   const { setIsCartOpen } = useContext(NavbarContext)
    const [showQty, setShowQty] = useState<boolean>(false)
    const qtyRef = useRef(null)
 
    useEffect(() => {
       setShowQty(cartItems.length > 0)
    }, [cartItems.length])
+
+   const onClickEvent = () => {
+      setIsCartOpen(prev => !prev)
+   }
 
    return (
       <NavbarCartButtonStyle onClick={onClickEvent}>
@@ -26,17 +32,14 @@ const CartButton: React.FC<Props> = ({ onClickEvent }) => {
                enter: styles.QtyEnter,
                enterActive: styles.QtyEnterActive,
                exit: styles.QtyExit,
-               exitActive: styles.QtyExitActive
-            }}>
+               exitActive: styles.QtyExitActive,
+            }}
+         >
             <ItemsInCartStyle ref={qtyRef}>{totalQuantity}</ItemsInCartStyle>
          </CSSTransition>
          <FontAwesomeIcon icon={['fas', 'cart-arrow-down']} size='3x' />
       </NavbarCartButtonStyle>
    )
-}
-
-type Props = {
-   onClickEvent: () => void
 }
 
 export default CartButton
