eaa48760a761dd3f0818a33317d26caef47c7227
nagytommy76 <nagytommy76@gmail.com>
2022-03-21 20:23:17 +0100

    Created a function which is renders a string from .mjml files

--- node-api/config/Mail/handlebars.ts
+++ node-api/config/Mail/handlebars.ts
@@ -5,7 +5,6 @@ import { UnknownObject } from 'express-handlebars/types'
 import { URL_PATH } from '../endpoints.config'
 
 import mjml from 'mjml'
-import path from 'path'
 
 export default abstract class Handlebars {
    private expressHbsInstance
@@ -31,119 +30,14 @@ export default abstract class Handlebars {
       return this.#renderMainHandlebarsModule(renderedModule)
    }
 
-   testFuntion(confirmationCode: string, userName: string, EMAIL_TOKEN_EXPIRESIN: string) {
-      const renderedText = compile(`
-      <mjml>
-      <mj-head>
-         <mj-title>ComputerStore Email validáció</mj-title>
-         <mj-font name="Roboto" href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500"></mj-font>
-         <mj-attributes>
-            <mj-all font-family="Montserrat, Helvetica, Arial, sans-serif"></mj-all>
-            <mj-text font-weight="400" font-size="16px" color="#000000" line-height="24px"></mj-text>
-            <mj-section padding="0px"></mj-section>
-         </mj-attributes>
-      </mj-head>
-      <mj-body background-color="#F2F2F2">
-         <mj-section padding="20px 20px 0 20px" background-color="#FFFFFF">
-            <mj-column width="35%">
-               <mj-text align="left" font-size="20px" font-weight="500">ComputerStore</mj-text>
-            </mj-column>
-            <mj-column width="65%">
-            <mj-text align="right" font-size="20px" font-weight="500"
-            >Kedves: {{userName}}</mj-text></mj-column>
-         </mj-section>
-         <mj-section padding="20px 20px 0 20px" background-color="#FFFFFF">
-            <mj-column>
-               <mj-text
-                  align="center"
-                  font-weight="300"
-                  padding="30px 40px 10px 40px"
-                  font-size="32px"
-                  line-height="40px"
-                  color="#5FA91D"
-                  >Kérlek aktiváld az email címed a megadott linken keresztül!</mj-text
-               >
-            </mj-column>
-         </mj-section>
-         <mj-section padding="10px 20px" background-color="#FFFFFF">
-            <mj-column>
-               <mj-divider width="300px" border-width="3px" border-color="#9B9B9B"></mj-divider>
-               <mj-text align="center"
-                  >Ha nem működik a gomb kérlek másold be a linket a böngésződ keresősávjába.</mj-text
-               >
-            </mj-column>
-         </mj-section>
-         <mj-section
-            background-url="http://nimus.de/share/tpl-card/bg.jpg"
-            vertical-align="middle"
-            background-size="cover"
-            background-repeat="no-repeat"
-         >
-            <mj-column width="100%">
-               <mj-image
-                  src="http://nimus.de/share/tpl-card/lineshadow.png"
-                  alt=""
-                  align="center"
-                  border="none"
-                  padding="0px"
-               ></mj-image>
-               <mj-text align="center" padding="50px 40px 0 40px" font-weight="300">{{URL_PATH}}email-confirm/{{confirmationCode}}</mj-text>
-               <mj-button
-                  align="center"
-                  background-color="#5FA91D"
-                  color="#FFFFFF"
-                  border-radius="2px"
-                  href="#"
-                  inner-padding="15px 30px"
-                  padding-bottom="100px"
-                  padding-top="20px"
-                  >Aktiválás</mj-button
-               >
-            </mj-column>
-         </mj-section>
-         <mj-section padding="50px 0 0 0" background-color="#FFFFFF">
-            <mj-column>
-               <mj-image
-                  src="http://nimus.de/share/tpl-card/bottom.png"
-                  alt="bottom border"
-                  align="center"
-                  border="none"
-                  padding="0px"
-               ></mj-image>
-            </mj-column>
-         </mj-section>
-         <mj-section padding="10px 0 20px 0">
-            <mj-column>
-               <mj-text align="center" color="#000" font-size="16px"
-                  >Készítette:
-                  <a
-                     target="_blank"
-                     href="https://www.linkedin.com/in/tam%C3%A1s-nagy-27355116b/"
-                     style="color: #000"
-                     >Nagy Tamás</a
-                  >
-               </mj-text>
-            </mj-column>
-         </mj-section>
-      </mj-body>
-   </mjml>
-      `)
+   renderAnyMjmlToPlainHtml(renderEmailFolderAndName: string, contextObject: any) {
+      const readMjmlAsString = fs.readFileSync(`./views/${renderEmailFolderAndName}.mjml`, 'utf8')
 
-      const context = {
-         confirmationCode,
-         userName,
-         EMAIL_TOKEN_EXPIRESIN,
-         URL_PATH,
-      }
-      const mjmlText = renderedText(context)
+      const renderedText = compile(readMjmlAsString)
+
+      const mjmlText = renderedText(contextObject)
       const html = mjml(mjmlText)
       return html.html
-      // fs.readFile('./views/main.mjml', (error, data) => {
-      //    // console.log(data)
-      //    console.log(error?.message)
-      //    if (error == null) {
-      //    }
-      // })
    }
 
    async #renderMainHandlebarsModule(renderedHbsString: string) {
