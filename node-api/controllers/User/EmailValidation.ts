68db110312ac5c3ee9546562beeeec70e4255d07
nagytommy76 <nagytommy76@gmail.com>
2022-03-22 19:47:00 +0100

    Called the sendEmailUserRegistersAndResendEmail insted of resendMail function

--- node-api/controllers/User/EmailValidation.ts
+++ node-api/controllers/User/EmailValidation.ts
@@ -12,7 +12,7 @@ export const ValidateEmailRegistrationController = (req: Request, res: Response)
       jwt.verify(confirmCode, EMAIL_SECRET, (error, decoded) => {
          if (error) return res.status(403).json({ errorMsg: error.message })
          const { email } = decoded as DecodedUserType
-         User.findOne({ email }).then((foundUser) => {
+         User.findOne({ email }).then(foundUser => {
             if (foundUser) {
                foundUser.isEmailConfirmed = true
                foundUser.save()
@@ -26,24 +26,44 @@ export const ValidateEmailRegistrationController = (req: Request, res: Response)
 }
 
 export const ResendEmailController = async (req: Request, res: Response) => {
-   const { confirmCode, userEmailOrUsername } = req.body as { confirmCode: string | null; userEmailOrUsername: string | null }
+   const { confirmCode, userEmailOrUsername } = req.body as {
+      confirmCode: string | null
+      userEmailOrUsername: string | null
+   }
    try {
       // Ha érvénytelen vagy hibás a valid kód
       if (confirmCode !== null) {
-         const { email, userName } = jwt.decode(confirmCode) as { email: string; userName: string; exp: number; iat: number }
+         const { email, userName } = jwt.decode(confirmCode) as {
+            email: string
+            userName: string
+            exp: number
+            iat: number
+         }
          const emailToken = signAnEmailTokenWithUserEmailAndName(userName, email)
-         await nodemailer.resendEmailWhenTokenExpiresOrInvalid(email, emailToken)
+         await nodemailer.sendEmailUserRegistersAndResendEmail(
+            email,
+            'Megerősítő kód újraküldése',
+            userName,
+            emailToken
+         )
          return res
             .status(200)
-            .json({ message: `Az új regisztrációs kód el lett küldve a korábban megadott email címre: ${email}` })
+            .json({
+               message: `Az új regisztrációs kód el lett küldve a korábban megadott email címre: ${email}`,
+            })
       } else if (userEmailOrUsername !== null) {
          // A login oldalon, ha még nincs validálva
          const foundUser = await User.findOne({
-            $or: [{ email: userEmailOrUsername }, { userName: userEmailOrUsername }]
+            $or: [{ email: userEmailOrUsername }, { userName: userEmailOrUsername }],
          }).select('userName email')
          if (foundUser) {
             const emailToken = signAnEmailTokenWithUserEmailAndName(foundUser.userName, foundUser.email)
-            await nodemailer.resendEmailWhenTokenExpiresOrInvalid(foundUser.email, emailToken)
+            await nodemailer.sendEmailUserRegistersAndResendEmail(
+               foundUser.email,
+               'Megerősítő kód újraküldése',
+               foundUser.userName,
+               emailToken
+            )
             return res.status(200).json({ message: 'Az email sikeresen elküldve az email címedre!' })
          }
       }
