dad594500b2b4f53d05823b546a5c74484a1ce67
nagytommy76 <nagytommy76@gmail.com>
2022-03-28 20:13:02 +0200

    Made an axios call & working on the error msg display

--- react-frontend/src/page/Auth/ForgotPassword/ForgotPassword.tsx
+++ react-frontend/src/page/Auth/ForgotPassword/ForgotPassword.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
+import useErrorsState from '../Hooks/useErrorsState'
 
 import { AuthContainer, AuthFormStyle, ImageStyle } from '../BaseForm/BaseStyle'
 import ValidationImage from '../Validation/Validation.jpg'
@@ -12,16 +13,67 @@ const ForgotPassword = () => {
    const [isLoading, setIsLoading] = useState<boolean>(false)
    const [firstPassword, setFirstPassword] = useState<string>('')
    const [secondPassword, setSecondPassword] = useState<string>('')
+   const { errors, setErrors } = useErrorsState()
 
-   let { forgotPassToken } = useParams() as { forgotPassToken: string }
+   let { forgotPassToken, userId } = useParams() as { forgotPassToken: string; userId: string }
 
-   const handlePasswordReset = (event: React.FormEvent) => {
+   const handlePasswordReset = async (event: React.FormEvent) => {
       event.preventDefault()
+      setIsLoading(true)
       try {
+         const resetResponse = await axios.post('/auth/reset-password', {
+            passwordToken: forgotPassToken,
+            userId,
+            firstPassword,
+            secondPassword,
+         })
+         console.table(resetResponse.data)
+         setIsLoading(false)
       } catch (error) {
          if (axios.isAxiosError(error)) {
-            console.log(error)
+            // console.log(error.response)
+            // if (error.response?.status === 403) {
+            //    error.response.data.errorMsg === /jwt expired/i &&
+            //       setErrors({
+            //          hasError: true,
+            //          messageTitle: 'Eltelt 15 perc! Lejárt a kód!',
+            //          message: 'Kérlek kérj egy új emailt a lenti gombbal.',
+            //          errorType: 'jwt expired',
+            //       })
+            //    error.response.data.errorMsg === /invalid signature/i ||
+            //       ('invalid token' &&
+            //          setErrors({
+            //             hasError: true,
+            //             messageTitle: 'Helytelen megerősítő kód! Kérlek ellenőrizd, vagy kérj egy újat!',
+            //             errorType: 'invalid signature',
+            //          }))
+            // } else {
+            //    setErrors({
+            //       hasError: true,
+            //       messageTitle: 'Egyéb hiba!',
+            //       message: error.response?.data.message || 'Egyéb hiba',
+            //       errorType: 'invalid signature',
+            //    })
+            // }
+            switch (error.response?.status) {
+               case 403:
+                  setErrors({
+                     hasError: true,
+                     messageTitle: error.response.data.errors[0].msg,
+                     message: 'Kérlek kérj egy új emailt a lenti gombbal.',
+                     errorType: 'jwt expired',
+                  })
+                  break
+               case 422:
+                  console.log('HAHHHÓÓÓÓ')
+                  setErrors({
+                     hasError: true,
+                     messageTitle: error.response.data.errors[0].msg,
+                  })
+                  break
+            }
          }
+         setIsLoading(false)
       }
    }
    return (
@@ -34,6 +86,7 @@ const ForgotPassword = () => {
                onSubmitEvent={handlePasswordReset}
             >
                <InputFields
+                  error={errors}
                   firstPassword={firstPassword}
                   secondPassword={secondPassword}
                   setFirstPassword={setFirstPassword}
