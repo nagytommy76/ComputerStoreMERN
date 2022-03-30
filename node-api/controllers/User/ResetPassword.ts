fa13f49b4aad51909eb2264ea24137c88964a14c
nagytommy76 <nagytommy76@gmail.com>
2022-03-30 19:52:09 +0200

    Modified the userId url param to userEmail

--- node-api/controllers/User/ResetPassword.ts
+++ node-api/controllers/User/ResetPassword.ts
@@ -36,7 +36,7 @@ export const forgotPasswordController = async (request: ForgotPasswordRequestTyp
             expiresIn: '10m',
          }
       )
-      const validationLink = `${URL_PATH}forgot-password/${checkUserRegistered._id}/${forgotPassToken}`
+      const validationLink = `${URL_PATH}forgot-password/${checkUserRegistered.email}/${forgotPassToken}`
       await NodeMailerInstance.sendResetPasswordLinkEmail(validationLink, checkUserRegistered.email)
       response.status(200).json({
          message: 'A jelszó emlékeztető email sikeresen elküldve a megadott email címre!',
@@ -49,12 +49,12 @@ export const forgotPasswordController = async (request: ForgotPasswordRequestTyp
 
 // A frontendről megkapom a 2 jelszót, meg az előzőleg elküldött tokent -> validálni kell
 export const resetPasswordController = async (request: ResetPassRequestType, response: Response) => {
-   const { passwordToken, firstPassword, userId } = request.body
+   const { passwordToken, firstPassword, userEmail } = request.body
 
    const validationErrors = validationResult(request)
    if (!validationErrors.isEmpty()) return response.status(422).json({ errors: validationErrors.array() })
 
-   const foundUser = await User.findOne({ _id: userId })
+   const foundUser = await User.findOne({ email: userEmail })
    if (foundUser === null) return response.status(404).json(ErrorResponse(true, 'Felhasználó nem található!'))
 
    try {
@@ -63,7 +63,7 @@ export const resetPasswordController = async (request: ResetPassRequestType, res
          passwordToken,
          PASSWORD_SECRET + foundUser.password,
          async (err: VerifyErrors | null, decoded: JwtPayload | undefined) => {
-            if (err) return response.status(403).json({ errorMessage: 'refresh token expired' })
+            if (err) return response.status(403).json({ errorMessage: 'password token expired' })
             if (decoded) {
                foundUser.password = hashedNewPass
                // foundUser.save()
