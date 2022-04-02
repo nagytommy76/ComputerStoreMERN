556d3465503ab702eb380f48933684333a75cbbc
nagytommy76 <nagytommy76@gmail.com>
2022-04-02 16:16:25 +0200

    Fixed the email confirm protected route

--- react-frontend/src/Routes.tsx
+++ react-frontend/src/Routes.tsx
@@ -62,9 +62,9 @@ const Routes = () => {
             <Route
                path='/email-confirm/:confirmCode'
                element={
-                  <AuthProtectedRoute>
+                  <GuestsRoute>
                      <EmailValidation />
-                  </AuthProtectedRoute>
+                  </GuestsRoute>
                }
             />
             <Route
