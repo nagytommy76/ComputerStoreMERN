5ae3e15f136bb7495cb0ba140c903717b9dae850
nagytommy76 <nagytommy76@gmail.com>
2022-04-03 11:35:38 +0200

    Created a separated hook

---
+++ react-frontend/src/page/Auth/Hooks/useEmailValidation.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import useErrorsState from './useErrorsState'
+
+const useEmailValidation = (confirmCode: string) => {
+   const navigate = useNavigate()
+   const [isPending, setIsPending] = useState<boolean>(false)
+   const { errors, setErrors } = useErrorsState()
+
+   const handleValidationForm = async (event: React.FormEvent) => {
+      event.preventDefault()
+      setIsPending(true)
+      try {
+         const verificationResponse = await axios.post('/auth/confirm-email', { confirmCode })
+         if (verificationResponse.status === 200) {
+            setIsPending(false)
+            navigate('/login', {
+               state: { isSuccess: true, message: 'Sikeres email cím megerősítés, most már beléphetsz!' },
+            })
+         }
+      } catch (error) {
+         if (axios.isAxiosError(error)) {
+            switch (error.response?.status) {
+               case 403:
+                  switch (error.response.data.errorMsg) {
+                     case 'jwt expired':
+                        setErrors({
+                           hasError: true,
+                           messageTitle: 'Lejárt a validációs kód!',
+                           message: 'Kérlek kérj egy új emailt a lenti gombbal.',
+                           errorType: 'jwt expired',
+                        })
+                        break
+                     case 'invalid signature':
+                        setErrors({
+                           hasError: true,
+                           messageTitle: 'Hibás kódot adtál meg! Ellenőrizd vagy:',
+                           message: 'Kérj egy új emailt a lenti gombbal.',
+                           errorType: 'invalid signature',
+                        })
+                        break
+                  }
+                  break
+               default:
+                  setErrors({
+                     hasError: true,
+                     messageTitle: 'Egyéb hiba!',
+                     message: error.response?.data.errorMsg || 'Egyéb hiba',
+                     errorType: 'invalid signature',
+                  })
+                  break
+            }
+            setIsPending(false)
+         } else console.log(error)
+      }
+   }
+   return { isPending, errors, handleValidationForm }
+}
+
+export default useEmailValidation
--- react-frontend/src/page/Auth/Validation/EmailValidation.tsx
+++ react-frontend/src/page/Auth/Validation/EmailValidation.tsx
@@ -1,69 +1,25 @@
 import React, { useEffect, useState } from 'react'
-import useErrorsState from '../Hooks/useErrorsState'
-import axios from 'axios'
-import { useParams, useNavigate } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 import { AuthContainer, AuthFormStyle, ImageStyle } from '../BaseForm/BaseStyle'
 import ValidImage from './Validation.jpg'
 
 import TextField from '@mui/material/TextField'
+import useEmailValidation from '../Hooks/useEmailValidation'
 
 const ValidateForm = React.lazy(() => import('../BaseForm/Form'))
 const ErrorAlert = React.lazy(() => import('./ErrorAlert'))
 
 const EmailValidation = () => {
    let params = useParams() as { confirmCode: string }
-   const navigate = useNavigate()
-
    const [code, setCode] = useState<string>('')
-   const [isPending, setIsPending] = useState<boolean>(false)
-   const { errors, setErrors } = useErrorsState()
+   const { handleValidationForm, errors, isPending } = useEmailValidation(code)
 
    useEffect(() => {
       if (params) setCode(params.confirmCode)
    }, [params])
 
    const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => setCode(event.target.value)
-
-   const handleValidationForm = async (event: React.FormEvent) => {
-      event.preventDefault()
-      setIsPending(true)
-      try {
-         const verificationResponse = await axios.post('/auth/confirm-email', { confirmCode: code })
-         if (verificationResponse.status === 200) {
-            setIsPending(false)
-            navigate('/login', {
-               state: { isSuccess: true, message: 'Sikeres email cím megerősítés, most már beléphetsz!' },
-            })
-         }
-      } catch (error) {
-         if (axios.isAxiosError(error)) {
-            if (error.response?.status === 403) {
-               error.response.data.param === /jwt expired/i &&
-                  setErrors({
-                     hasError: true,
-                     messageTitle: error.response.data.msg,
-                     message: 'Kérlek kérj egy új emailt a lenti gombbal.',
-                     errorType: 'jwt expired',
-                  })
-               error.response.data.param === /firstPassword/i &&
-                  setErrors({
-                     hasError: true,
-                     messageTitle: error.response.data.msg,
-                  })
-            } else {
-               setErrors({
-                  hasError: true,
-                  messageTitle: 'Egyéb hiba!',
-                  message: error.response?.data.message || 'Egyéb hiba',
-                  errorType: 'invalid signature',
-               })
-            }
-
-            setIsPending(false)
-         } else console.log(error)
-      }
-   }
 
    return (
       <AuthContainer>
