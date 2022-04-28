a224818db199b61cfc0eb9a9e9ea9382c23c25c0
nagytommy76 <nagytommy76@gmail.com>
2022-04-28 19:48:31 +0200

    Catching a jwt malformed error

--- react-frontend/src/page/Auth/Hooks/useEmailValidation.tsx
+++ react-frontend/src/page/Auth/Hooks/useEmailValidation.tsx
@@ -19,40 +19,46 @@ const useEmailValidation = (confirmCode: string) => {
                state: { isSuccess: true, message: 'Sikeres email cím megerősítés, most már beléphetsz!' },
             })
          }
-      } catch (error) {
-         if (axios.isAxiosError(error)) {
-            switch (error.response?.status) {
-               case 403:
-                  switch (error.response.data.errorMsg) {
-                     case 'jwt expired':
-                        setErrors({
-                           hasError: true,
-                           messageTitle: 'Lejárt a validációs kód!',
-                           message: 'Kérlek kérj egy új emailt a lenti gombbal.',
-                           errorType: 'jwt expired',
-                        })
-                        break
-                     case 'invalid signature':
-                        setErrors({
-                           hasError: true,
-                           messageTitle: 'Hibás kódot adtál meg! Ellenőrizd vagy:',
-                           message: 'Kérj egy új emailt a lenti gombbal.',
-                           errorType: 'invalid signature',
-                        })
-                        break
-                  }
-                  break
-               default:
-                  setErrors({
-                     hasError: true,
-                     messageTitle: 'Egyéb hiba!',
-                     message: error.response?.data.errorMsg || 'Egyéb hiba',
-                     errorType: 'invalid signature',
-                  })
-                  break
-            }
-            setIsPending(false)
-         } else console.log(error)
+      } catch (error: any) {
+         switch (error.response?.status) {
+            case 403:
+               switch (error.response.data.errorMsg) {
+                  case 'jwt expired':
+                     setErrors({
+                        hasError: true,
+                        messageTitle: 'Lejárt a validációs kód!',
+                        message: 'Kérlek kérj egy új emailt a lenti gombbal.',
+                        errorType: 'jwt expired',
+                     })
+                     break
+                  case 'invalid signature':
+                     setErrors({
+                        hasError: true,
+                        messageTitle: 'Hibás kódot adtál meg! Ellenőrizd vagy:',
+                        message: 'Kérj egy új emailt a lenti gombbal.',
+                        errorType: 'invalid signature',
+                     })
+                     break
+                  case 'jwt malformed':
+                     setErrors({
+                        hasError: true,
+                        messageTitle: 'Nem megfelelő kód formátumot adtál meg',
+                        message: 'Kérj egy új emailt a lenti gombbal.',
+                        errorType: 'invalid signature',
+                     })
+                     break
+               }
+               break
+            default:
+               setErrors({
+                  hasError: true,
+                  messageTitle: 'Egyéb hiba!',
+                  message: error.response?.data.errorMsg || 'Egyéb hiba',
+                  errorType: 'invalid signature',
+               })
+               break
+         }
+         setIsPending(false)
       }
    }
    return { isPending, errors, handleValidationForm }
