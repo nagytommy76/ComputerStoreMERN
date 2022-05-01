1a5abe77edc29d3f054d2cfc69d9129b8dabf574
nagytommy76 <nagytommy76@gmail.com>
2022-05-01 15:12:50 +0200

    Added a data-testid attribute

--- react-frontend/src/SuspenseComponents/DetailsPage/RatingSuspense/RatingSuspense.tsx
+++ react-frontend/src/SuspenseComponents/DetailsPage/RatingSuspense/RatingSuspense.tsx
@@ -10,14 +10,32 @@ import { mobileWindowSize } from '../../../Theme/GlobalStyles'
 
 const RatingSuspense = () => {
    return (
-      <RatingStyle>
+      <RatingStyle data-testid='ratingSuspense'>
          <Card>
             <CardContentStyle>
                <LeftContent>
-                  <Skeleton sx={{ marginBottom: 3 }} height={40} width={350} variant='rectangular' animation='wave' />
-                  <Skeleton sx={{ marginBottom: 2 }} height={40} width={200} variant='rectangular' animation='wave' />
+                  <Skeleton
+                     sx={{ marginBottom: 3 }}
+                     height={40}
+                     width={350}
+                     variant='rectangular'
+                     animation='wave'
+                  />
+                  <Skeleton
+                     sx={{ marginBottom: 2 }}
+                     height={40}
+                     width={200}
+                     variant='rectangular'
+                     animation='wave'
+                  />
                   <Skeleton sx={{ marginBottom: 2 }} variant='rectangular' height={200} animation='wave' />
-                  <Skeleton sx={{ marginBottom: 4 }} variant='rectangular' height={45} width={200} animation='wave' />
+                  <Skeleton
+                     sx={{ marginBottom: 4 }}
+                     variant='rectangular'
+                     height={45}
+                     width={200}
+                     animation='wave'
+                  />
                </LeftContent>
                <SummarySuspense />
             </CardContentStyle>
@@ -33,8 +51,8 @@ const RatingStyle = styled('section')({
    flexDirection: 'column',
    margin: '0 auto',
    [`@media(max-width: ${mobileWindowSize})`]: {
-      marginTop: '2rem'
-   }
+      marginTop: '2rem',
+   },
 })
 
 export const CardContentStyle = styled(CardContent)({
@@ -42,8 +60,8 @@ export const CardContentStyle = styled(CardContent)({
    display: 'flex',
    flexDirection: 'row',
    [`@media(max-width: ${mobileWindowSize})`]: {
-      flexDirection: 'column'
-   }
+      flexDirection: 'column',
+   },
 })
 
 const LeftContent = styled('section')({
@@ -51,8 +69,8 @@ const LeftContent = styled('section')({
    paddingRight: '.9rem',
    [`@media(max-width: ${mobileWindowSize})`]: {
       width: '100%',
-      paddingRight: 0
-   }
+      paddingRight: 0,
+   },
 })
 
 export default RatingSuspense
