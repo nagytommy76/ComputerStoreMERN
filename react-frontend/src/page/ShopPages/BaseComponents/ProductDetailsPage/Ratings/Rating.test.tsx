f71e1f44909e92919486eba2281ec465aa436e44
nagytommy76 <nagytommy76@gmail.com>
2022-05-03 14:55:38 +0200

    Working on the Rating test

--- react-frontend/src/page/ShopPages/BaseComponents/ProductDetailsPage/Ratings/Rating.test.tsx
+++ react-frontend/src/page/ShopPages/BaseComponents/ProductDetailsPage/Ratings/Rating.test.tsx
@@ -1,38 +1,36 @@
-// import { render as rtlRender } from '@testing-library/react'
-// import { Provider } from 'react-redux'
-// import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit'
-
 import { render, screen } from '../../../../../test-utils'
 import { render as authRender } from '../../../../../test-utils-auth'
 import Rating from './AddNew/Rating'
 import axios from 'axios'
 import { waitForElementToBeRemoved } from '@testing-library/dom'
 import userEvent from '@testing-library/user-event'
+import DetailsContext from '../../../Context/DetailsContext'
 
-// jest.setTimeout(20000)
+jest.setTimeout(27000)
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
 
-// const mockInitialState = {
-//    userLoggedIn: false,
-// }
-// const AuthSlice = createSlice({
-//    name: 'auth',
-//    initialState: mockInitialState,
-//    reducers: {
-//       setUserLoggedIn: (state, action: PayloadAction<boolean>) => {
-//          state.userLoggedIn = action.payload
-//       },
-//    },
-// })
+jest.mock('react-router-dom', () => ({
+   ...jest.requireActual('react-router-dom'), // use actual for all non-hook parts
+   useParams: () => ({
+      productId: '6111500be834831dd445052b',
+   }),
+   useRouteMatch: () => ({
+      productId: '6111500be834831dd445052b',
+   }),
+}))
 
-// const renderComponent = (isUserLoggedIn: boolean = false) =>{
-//    rtlRender(
-//       <Provider store={configureStore({ reducer: { auth: AuthSlice.reducer } })}>
-//          <Rating />
-//       </Provider>
-//    )
-// }
+const formatDate = (date: string) => {
+   const formattedDate = new Date(date)
+   return formattedDate.toLocaleDateString('hu-HU', {
+      year: 'numeric',
+      month: '2-digit',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+   })
+}
 
 const mockAvgRating = {
    status: 200,
@@ -48,6 +46,16 @@ const mockGetComments = {
       {
          _id: '61534c48f49a34129c48c0fc',
          comment: 'Az árához képest so-so',
+         commentAnswers: [
+            {
+               answer: 'Az egy jó kártya volt a maga idejében :)',
+               answeredAt: '2021-11-15T19:28:45.924Z',
+               responses: [],
+               userId: '6134ac45824b6c129cd0859b',
+               userName: 'senki123',
+               _id: '6192b4ede1b9411844247064',
+            },
+         ],
          ratedAt: '2021-09-28T17:09:28.765Z',
          rating: 2.5,
          userId: '614f39ce51587c3450377112',
@@ -64,6 +72,7 @@ const mockGetComments = {
       {
          _id: '61534c68f49a34129c48c10e',
          comment: 'Ár érték arányban nem rossz ,de a világot nem fogja megváltani :)',
+         commentAnswers: [],
          ratedAt: '2021-09-28T17:10:00.695Z',
          rating: 4.5,
          userId: '60f3f0b9c7f8211424864a2c',
@@ -80,6 +89,7 @@ const mockGetComments = {
       {
          _id: '61534c93f49a34129c48c120',
          comment: 'Olcsó húsnak híg a leve',
+         commentAnswers: [],
          ratedAt: '021-09-28T17:10:43.634Z',
          rating: 1.5,
          userId: '60f3f0b9c7f8211424864a2c',
@@ -102,6 +112,7 @@ const mockWithNewComment = {
       {
          _id: '61534c48f49a34129c48c0fc',
          comment: 'Az árához képest so-so',
+         commentAnswers: [],
          ratedAt: '2021-09-28T17:09:28.765Z',
          rating: 2.5,
          userId: '614f39ce51587c3450377112',
@@ -118,6 +129,7 @@ const mockWithNewComment = {
       {
          _id: '61534c68f49a34129c48c10e',
          comment: 'Ár érték arányban nem rossz ,de a világot nem fogja megváltani :)',
+         commentAnswers: [],
          ratedAt: '2021-09-28T17:10:00.695Z',
          rating: 4.5,
          userId: '60f3f0b9c7f8211424864a2c',
@@ -134,6 +146,7 @@ const mockWithNewComment = {
       {
          _id: '61534c93f49a34129c48c120',
          comment: 'Olcsó húsnak híg a leve',
+         commentAnswers: [],
          ratedAt: '021-09-28T17:10:43.634Z',
          rating: 1.5,
          userId: '60f3f0b9c7f8211424864a2c',
@@ -150,6 +163,7 @@ const mockWithNewComment = {
       {
          _id: '615hggfdrt546654fzdt645654',
          comment: 'Teszi a dolgát, nekem nagyon bejött',
+         commentAnswers: [],
          ratedAt: '021-11-07T10:10:43.634Z',
          rating: 1.5,
          userId: '60f3f0brwerewrew2c',
@@ -195,35 +209,45 @@ describe('Testing the ratings', () => {
       expect(await screen.findByText(/Teszi a dolgát, nekem nagyon bejött/i)).toBeInTheDocument()
    })
 
-   // test('should add a new comment', async () => {
-   // expect(
-   //    screen.getByRole('heading', { name: formatDate(mockGetComments.data[0].ratedAt) })
-   // ).toBeInTheDocument()
-   // expect(screen.getByText(/Az árához képest so-so/i)).toBeInTheDocument()
+   test('should add a new answer', async () => {
+      mockedAxios.get.mockResolvedValue(mockGetComments).mockResolvedValueOnce(mockAvgRating)
+      authRender(
+         <DetailsContext.Provider
+            value={{
+               details: {},
+               manufacturer: '',
+               pictureUrls: [''],
+               price: 0,
+               productId: '6111500be834831dd445052b',
+               productType: 'vga',
+               type: '',
+               typeCode: '',
+            }}
+         >
+            <Rating />,
+         </DetailsContext.Provider>,
+         true
+      )
+      await waitForElementToBeRemoved(() =>
+         screen.getByRole('heading', { name: /Nem érkezett még értékelés/i })
+      )
+      expect(await screen.findByText(/Összesen 3 értékelés/i)).toBeInTheDocument()
+      // screen.getByRole('')
+      const answerBtn = await screen.findAllByRole('button', { name: /Válasz/i })
+      expect(await screen.findByText(/Az árához képest so-so/i)).toBeInTheDocument()
+      expect(screen.getByRole('heading', { name: /nagytommy76/i })).toBeInTheDocument()
 
-   // expect(screen.getByRole('heading', { name: /nagytommy76/i })).toBeInTheDocument()
-   // expect(
-   //    screen.getByRole('heading', { name: formatDate(mockGetComments.data[1].ratedAt) })
-   // ).toBeInTheDocument()
-   // expect(
-   //    screen.getByText(/Ár érték arányban nem rossz ,de a világot nem fogja megváltani/i)
-   // ).toBeInTheDocument()
+      // expect(
+      //    screen.getByRole('heading', { name: formatDate(mockGetComments.data[1].ratedAt) })
+      // ).toBeInTheDocument()
+      // expect(
+      //    screen.getByText(/Ár érték arányban nem rossz ,de a világot nem fogja megváltani/i)
+      // ).toBeInTheDocument()
 
-   // expect(screen.getByRole('heading', { name: /senki123/i })).toBeInTheDocument()
-   // expect(
-   //    screen.getByRole('heading', { name: formatDate(mockGetComments.data[2].ratedAt) })
-   // ).toBeInTheDocument()
-   // expect(screen.getByText(/Olcsó húsnak híg a leve/i)).toBeInTheDocument()
-   // })
-
-   // test('should add a new comment, if the user not logged in', async () => {
-   //    render(<Rating />)
-   //    const textInput = await screen.findByRole('textbox', { name: /Hozzászólás/i })
-   //    const button = await screen.findByRole('button', { name: /Értékelés leadása/i })
-
-   //    // mockedAxios.get.mockResolvedValue(mockAvgRating).mockResolvedValue(mockWithNewComment)
-   //    userEvent.type(textInput, 'Teszi a dolgát, nekem nagyon bejött')
-   //    userEvent.click(button)
-   //    expect(screen.getByText(/Az értékeléshez kérlek jelentkezz be!/i)).toBeInTheDocument()
-   // })
+      // expect(screen.getByRole('heading', { name: /senki123/i })).toBeInTheDocument()
+      // expect(
+      //    screen.getByRole('heading', { name: formatDate(mockGetComments.data[2].ratedAt) })
+      // ).toBeInTheDocument()
+      // expect(screen.getByText(/Olcsó húsnak híg a leve/i)).toBeInTheDocument()
+   })
 })
