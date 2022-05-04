0c6bd8a8752009c9241fc018979e00250eea482a
nagytommy76 <nagytommy76@gmail.com>
2022-05-04 14:25:21 +0200

    Testing the Rating component

--- react-frontend/src/page/ShopPages/BaseComponents/ProductDetailsPage/Ratings/Rating.test.tsx
+++ react-frontend/src/page/ShopPages/BaseComponents/ProductDetailsPage/Ratings/Rating.test.tsx
@@ -86,23 +86,6 @@ const mockGetComments = {
             },
          ],
       },
-      {
-         _id: '61534c93f49a34129c48c120',
-         comment: 'Olcsó húsnak híg a leve',
-         commentAnswers: [],
-         ratedAt: '021-09-28T17:10:43.634Z',
-         rating: 1.5,
-         userId: '60f3f0b9c7f8211424864a2c',
-         userName: 'senki123',
-         responses: [
-            { isLike: true, userId: '614f39ce51587c3450377112', _id: '6159b4cc95d8e103842d1321' },
-            {
-               isLike: true,
-               userId: '60f3f0b9c7f8211424864a2c',
-               _id: '6159bb8495d8e103842d13b3',
-            },
-         ],
-      },
    ],
 }
 
@@ -173,6 +156,20 @@ const mockWithNewComment = {
    ],
 }
 
+const mockNewCommentAnswer = {
+   status: 201,
+   data: [
+      {
+         answer: 'Teszt üzenetet küldök!!!',
+         answeredAt: '2022-05-04T11:34:58.916Z',
+         responses: [],
+         userId: '60f3f0b9c7f8211424864a2c',
+         userName: 'nagytommy76',
+         _id: '627264e24f3313001e18ebaf',
+      },
+   ],
+}
+
 describe('Testing the ratings', () => {
    test('should render the add new comment section', async () => {
       mockedAxios.get.mockResolvedValue(mockAvgRating)
@@ -209,7 +206,7 @@ describe('Testing the ratings', () => {
       expect(await screen.findByText(/Teszi a dolgát, nekem nagyon bejött/i)).toBeInTheDocument()
    })
 
-   test('should add a new answer', async () => {
+   test('should add a new answer to the comment', async () => {
       mockedAxios.get.mockResolvedValue(mockGetComments).mockResolvedValueOnce(mockAvgRating)
       authRender(
          <DetailsContext.Provider
@@ -226,28 +223,41 @@ describe('Testing the ratings', () => {
          >
             <Rating />,
          </DetailsContext.Provider>,
-         true
+         true,
+         'nagytommy76',
+         '60f3f0b9c7f8211424864a2c'
       )
       await waitForElementToBeRemoved(() =>
          screen.getByRole('heading', { name: /Nem érkezett még értékelés/i })
       )
+      // Több comment rész van azért getAllBy*
+      await waitForElementToBeRemoved(() => screen.getAllByTestId('commentSuspense'))
+
       expect(await screen.findByText(/Összesen 3 értékelés/i)).toBeInTheDocument()
-      // screen.getByRole('')
-      const answerBtn = await screen.findAllByRole('button', { name: /Válasz/i })
       expect(await screen.findByText(/Az árához képest so-so/i)).toBeInTheDocument()
-      expect(screen.getByRole('heading', { name: /nagytommy76/i })).toBeInTheDocument()
+      expect(await screen.findByText(/Az egy jó kártya volt a maga idejében/i)).toBeInTheDocument()
+      expect(
+         await screen.findByText(/Ár érték arányban nem rossz ,de a világot nem fogja megváltani/i)
+      ).toBeInTheDocument()
+      expect(await screen.findByRole('heading', { name: /senki321/i })).toBeInTheDocument()
+      expect(await screen.findByRole('heading', { name: /nagytommy76/i })).toBeInTheDocument()
 
-      // expect(
-      //    screen.getByRole('heading', { name: formatDate(mockGetComments.data[1].ratedAt) })
-      // ).toBeInTheDocument()
-      // expect(
-      //    screen.getByText(/Ár érték arányban nem rossz ,de a világot nem fogja megváltani/i)
-      // ).toBeInTheDocument()
+      // megkeresem a válasz gombot, rákattintok majd, előjön a textarea amibe írni tudok?!
+      const answerBtn = await screen.findAllByRole('button', { name: /Válasz/i })
+      userEvent.click(answerBtn[0])
+      const answerTextArea = await screen.findByLabelText(/Válasz üzenet senki321 részére/i)
+      expect(answerTextArea).toBeInTheDocument()
 
-      // expect(screen.getByRole('heading', { name: /senki123/i })).toBeInTheDocument()
-      // expect(
-      //    screen.getByRole('heading', { name: formatDate(mockGetComments.data[2].ratedAt) })
-      // ).toBeInTheDocument()
-      // expect(screen.getByText(/Olcsó húsnak híg a leve/i)).toBeInTheDocument()
+      const sendAnswerBtn = await screen.findByRole('button', { name: /Válasz küldése/i })
+      expect(sendAnswerBtn).toBeInTheDocument()
+      userEvent.click(sendAnswerBtn)
+      expect(await screen.findByText(/Kérlek írj kommentet!/i)).toBeInTheDocument()
+
+      mockedAxios.post.mockResolvedValue(mockNewCommentAnswer)
+      userEvent.type(answerTextArea, 'Teszt üzenetet küldök!!!')
+      userEvent.click(sendAnswerBtn)
+
+      expect(await screen.findByText(/A Válaszodat fogadtuk!/i)).toBeInTheDocument()
+      expect(await screen.findByText(/Teszt üzenetet küldök!!!/i)).toBeInTheDocument()
    })
 })
