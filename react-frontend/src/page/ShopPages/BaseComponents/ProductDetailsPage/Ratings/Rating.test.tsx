69c0feef0bf6abd3e32fef68cf6f90e92fc65e1b
nagytommy76 <nagytommy76@gmail.com>
2022-05-05 20:06:36 +0200

    Tested the remove an answer functionality

--- react-frontend/src/page/ShopPages/BaseComponents/ProductDetailsPage/Ratings/Rating.test.tsx
+++ react-frontend/src/page/ShopPages/BaseComponents/ProductDetailsPage/Ratings/Rating.test.tsx
@@ -1,9 +1,17 @@
 import { render, screen } from '../../../../../test-utils'
 import { render as authRender } from '../../../../../test-utils-auth'
-import Rating from './AddNew/Rating'
-import axios from 'axios'
 import { waitForElementToBeRemoved } from '@testing-library/dom'
 import userEvent from '@testing-library/user-event'
+import {
+   mockAvgRating,
+   mockGetCommentsToAddNew,
+   mockGetCommentsToDeleteAnswer,
+   mockNewCommentAnswer,
+   mockWithNewComment,
+} from './TestMocks'
+
+import axios from 'axios'
+import Rating from './AddNew/Rating'
 import DetailsContext from '../../../Context/DetailsContext'
 
 jest.setTimeout(27000)
@@ -19,144 +27,6 @@ jest.mock('react-router-dom', () => ({
       productId: '6111500be834831dd445052b',
    }),
 }))
-
-const mockAvgRating = {
-   status: 200,
-   data: {
-      avgRating: 2.8333333333333335,
-      rateCount: 3,
-   },
-}
-
-const mockGetComments = {
-   status: 200,
-   data: [
-      {
-         _id: '61534c48f49a34129c48c0fc',
-         comment: 'Az árához képest so-so',
-         commentAnswers: [
-            {
-               answer: 'Az egy jó kártya volt a maga idejében :)',
-               answeredAt: '2021-11-15T19:28:45.924Z',
-               responses: [],
-               userId: '6134ac45824b6c129cd0859b',
-               userName: 'senki123',
-               _id: '6192b4ede1b9411844247064',
-            },
-         ],
-         ratedAt: '2021-09-28T17:09:28.765Z',
-         rating: 2.5,
-         userId: '614f39ce51587c3450377112',
-         userName: 'senki321',
-         responses: [
-            { isLike: false, userId: '6134ac45824b6c129cd0859b', _id: '61535adc634bea29b4542ab1' },
-            {
-               isLike: true,
-               userId: '60f3f0b9c7f8211424864a2c',
-               _id: '6159bb7d95d8e103842d13af',
-            },
-         ],
-      },
-      {
-         _id: '61534c68f49a34129c48c10e',
-         comment: 'Ár érték arányban nem rossz ,de a világot nem fogja megváltani :)',
-         commentAnswers: [],
-         ratedAt: '2021-09-28T17:10:00.695Z',
-         rating: 4.5,
-         userId: '60f3f0b9c7f8211424864a2c',
-         userName: 'nagytommy76',
-         responses: [
-            { isLike: true, userId: '6134ac45824b6c129cd0859b', _id: '6153508d2ba2282324b2c824' },
-            {
-               isLike: true,
-               userId: '614f39ce51587c3450377112',
-               _id: '6159b51995d8e103842d132a',
-            },
-         ],
-      },
-   ],
-}
-
-const mockWithNewComment = {
-   status: 200,
-   data: [
-      {
-         _id: '61534c48f49a34129c48c0fc',
-         comment: 'Az árához képest so-so',
-         commentAnswers: [],
-         ratedAt: '2021-09-28T17:09:28.765Z',
-         rating: 2.5,
-         userId: '614f39ce51587c3450377112',
-         userName: 'senki321',
-         responses: [
-            { isLike: false, userId: '6134ac45824b6c129cd0859b', _id: '61535adc634bea29b4542ab1' },
-            {
-               isLike: true,
-               userId: '60f3f0b9c7f8211424864a2c',
-               _id: '6159bb7d95d8e103842d13af',
-            },
-         ],
-      },
-      {
-         _id: '61534c68f49a34129c48c10e',
-         comment: 'Ár érték arányban nem rossz ,de a világot nem fogja megváltani :)',
-         commentAnswers: [],
-         ratedAt: '2021-09-28T17:10:00.695Z',
-         rating: 4.5,
-         userId: '60f3f0b9c7f8211424864a2c',
-         userName: 'nagytommy76',
-         responses: [
-            { isLike: true, userId: '6134ac45824b6c129cd0859b', _id: '6153508d2ba2282324b2c824' },
-            {
-               isLike: true,
-               userId: '614f39ce51587c3450377112',
-               _id: '6159b51995d8e103842d132a',
-            },
-         ],
-      },
-      {
-         _id: '61534c93f49a34129c48c120',
-         comment: 'Olcsó húsnak híg a leve',
-         commentAnswers: [],
-         ratedAt: '021-09-28T17:10:43.634Z',
-         rating: 1.5,
-         userId: '60f3f0b9c7f8211424864a2c',
-         userName: 'senki123',
-         responses: [
-            { isLike: true, userId: '614f39ce51587c3450377112', _id: '6159b4cc95d8e103842d1321' },
-            {
-               isLike: true,
-               userId: '60f3f0b9c7f8211424864a2c',
-               _id: '6159bb8495d8e103842d13b3',
-            },
-         ],
-      },
-      {
-         _id: '615hggfdrt546654fzdt645654',
-         comment: 'Teszi a dolgát, nekem nagyon bejött',
-         commentAnswers: [],
-         ratedAt: '021-11-07T10:10:43.634Z',
-         rating: 1.5,
-         userId: '60f3f0brwerewrew2c',
-         userName: 'nagytommy76',
-         responses: [],
-      },
-   ],
-}
-
-const mockNewCommentAnswer = {
-   status: 201,
-   data: [
-      {
-         answer: 'Teszt üzenetet küldök!!!',
-         answeredAt: '2022-05-04T11:34:58.916Z',
-         responses: [],
-         userId: '60f3f0b9c7f8211424864a2c',
-         userName: 'nagytommy76',
-         _id: '627264e24f3313001e18ebaf',
-      },
-   ],
-}
 
 describe('Testing the ratings', () => {
    test('should render the add new comment section', async () => {
@@ -195,7 +65,7 @@ describe('Testing the ratings', () => {
    })
 
    test('should add a new answer to the comment', async () => {
-      mockedAxios.get.mockResolvedValue(mockGetComments).mockResolvedValueOnce(mockAvgRating)
+      mockedAxios.get.mockResolvedValue(mockGetCommentsToAddNew).mockResolvedValueOnce(mockAvgRating)
       authRender(
          <DetailsContext.Provider
             value={{
@@ -215,10 +85,10 @@ describe('Testing the ratings', () => {
          'nagytommy76',
          '60f3f0b9c7f8211424864a2c'
       )
-      await waitForElementToBeRemoved(() =>
-         screen.getByRole('heading', { name: /Nem érkezett még értékelés/i })
-      )
-      // expect(await screen.findByRole('heading', { name: /Összesen 3 értékelés/i })).toBeInTheDocument()
+      // await waitForElementToBeRemoved(
+      //    () => screen.getByRole('heading', { name: /Nem érkezett még értékelés/i })
+      // )
+      expect(await screen.findByRole('heading', { name: /Összesen 3 értékelés/i })).toBeInTheDocument()
       // Több comment rész van azért getAllBy*
       await waitForElementToBeRemoved(() => screen.getAllByTestId('commentSuspense'))
 
@@ -251,7 +121,7 @@ describe('Testing the ratings', () => {
    })
 
    test('should delete an answer', async () => {
-      mockedAxios.get.mockResolvedValue(mockGetComments).mockResolvedValueOnce(mockAvgRating)
+      mockedAxios.get.mockResolvedValue(mockGetCommentsToDeleteAnswer).mockResolvedValueOnce(mockAvgRating)
       authRender(
          <DetailsContext.Provider
             value={{
@@ -271,6 +141,34 @@ describe('Testing the ratings', () => {
          'nagytommy76',
          '60f3f0b9c7f8211424864a2c'
       )
-      await waitForElementToBeRemoved(() => screen.getAllByTestId('commentSuspense'))
+      await waitForElementToBeRemoved(() =>
+         screen.getByRole('heading', { name: /Nem érkezett még értékelés/i })
+      )
+      expect(await screen.findByRole('heading', { name: /Összesen 3 értékelés/i })).toBeInTheDocument()
+
+      const removeAnswerBtn = await screen.findByRole('button', { name: /Válasz törlése/i })
+      expect(
+         await screen.findByText(/Ez egy teszt válasz senki321 részére, nagytommy76-tól/i)
+      ).toBeInTheDocument()
+
+      mockedAxios.delete.mockResolvedValue({
+         status: 200,
+         data: [
+            {
+               answer: 'Az egy jó kártya volt a maga idejében :)',
+               answeredAt: '2021-11-15T19:28:45.924Z',
+               responses: [],
+               userId: '6134ac45824b6c129cd0859b',
+               userName: 'senki123',
+               _id: '6192b4ede1b9411844247064',
+            },
+         ],
+      })
+      userEvent.click(removeAnswerBtn)
+
+      await waitForElementToBeRemoved(() =>
+         screen.getByText(/Ez egy teszt válasz senki321 részére, nagytommy76-tól/i)
+      )
+      expect(await screen.findByText(/Az egy jó kártya volt a maga idejében/i)).toBeInTheDocument()
    })
 })
