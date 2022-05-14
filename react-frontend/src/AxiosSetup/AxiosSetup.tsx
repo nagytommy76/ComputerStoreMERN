dd199f283d4191fd67696edd313bb7db4f1ab81c
nagytommy76 <nagytommy76@gmail.com>
2022-05-14 15:08:56 +0200

    Imported the new axios instance

--- react-frontend/src/AxiosSetup/AxiosSetup.tsx
+++ react-frontend/src/AxiosSetup/AxiosSetup.tsx
@@ -1,11 +1,10 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router'
-import axios from 'axios'
+import { axiosInstance as axios } from './AxiosInstance'
 
 import { logoutUser, setAccessToken } from '../app/slices/AuthSlice'
 import { restoreUserDetails } from '../app/slices/Checkout/UserDetailsSlice'
 import { useAppDispatch, useAppSelector } from '../app/hooks'
-// import { store } from '../app/store'
 
 // Az app megnyitásakor, ha a user ba van jelentkezve megvizsgálom, hogy érvényes-e az accessToken-je
 // Ha nem akkor a refreshToken-nel kérek egy újat,
@@ -17,20 +16,6 @@ const useAxiosSetup = () => {
    const accessToken = useAppSelector(state => state.auth.accessToken)
    const refreshToken = useAppSelector(state => state.auth.refreshToken)
 
-   // const DEPLOY_URL = 'https://computerstorebackend.firebaseapp.com/'
-   const PRODUCTION_URL = 'http://localhost:3000'
-   // Docker port
-   // const DOCKER_FRONTEND_URL = 'http://localhost:8080'
-   // const DOCKER_BACKEND_URL = 'http://localhost:5040/api'
-
-   axios.defaults.baseURL = 'http://localhost:5040/api'
-   // axios.defaults.baseURL = 'http://localhost:5050/api'
-   // axios.defaults.baseURL = DOCKER_BACKEND_URL
-   // axios.defaults.baseURL = 'https://compstorebackend.herokuapp.com/api'
-
-   axios.defaults.headers.common['Content-Type'] = 'Application/json'
-   axios.defaults.headers.common['Access-Control-Allow-Origin'] = PRODUCTION_URL
-   axios.defaults.headers.common['Access-Control-Allow-Credentials'] = 'true'
    axios.defaults.headers.common.Authorization = `Barer ${accessToken}`
    useEffect(() => {
       axios.interceptors.response.use(
@@ -38,6 +23,7 @@ const useAxiosSetup = () => {
             return response
          },
          async error => {
+            console.log('ERROR VAN AXIOS: ', axios.defaults.headers.common.Authorization)
             if (error.config && error.response && !error.config._retry && error.response.status === 403) {
                // Ekkor kell egy új accessToken (Forbidden) / 403 error, tehát lejárt az accessToken
                if (error.response.data.errorMessage === 'accessToken token expired') {
@@ -82,7 +68,7 @@ const useAxiosSetup = () => {
             return await Promise.reject(error)
          }
       )
-   }, [accessToken, navigate, refreshToken, dispatch])
+   }, [navigate, refreshToken, dispatch])
 }
 
 export default useAxiosSetup
