02ab3868ec8b283da1ad66c9c1454b0b69829081
nagytommy76 <nagytommy76@gmail.com>
2022-05-15 11:52:23 +0200

    Created a button suspense component

---
+++ react-frontend/src/SuspenseComponents/ButtonSuspense.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Skeleton from '@mui/material/Skeleton'
+
+const ButtonSuspense = () => {
+   return <Skeleton height={60} width='40%' />
+}
+
+export default ButtonSuspense
