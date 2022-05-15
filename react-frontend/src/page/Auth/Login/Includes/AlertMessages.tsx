d212d47a3015b274000dfa452cc71733b70437d3
nagytommy76 <nagytommy76@gmail.com>
2022-05-15 11:54:44 +0200

    Fixed the maximum render error: Suspense added to resendBtn

--- react-frontend/src/page/Auth/Login/Includes/AlertMessages.tsx
+++ react-frontend/src/page/Auth/Login/Includes/AlertMessages.tsx
@@ -4,6 +4,7 @@ import { InputTypes } from '../../DefaultProperties'
 import Alert from '@mui/material/Alert'
 import Collapse from '@mui/material/Collapse'
 import Snackbar from '@mui/material/Snackbar'
+import ButtonSuspense from '../../../../SuspenseComponents/ButtonSuspense'
 import useSnackbar from '../../Hooks/useSnackbar'
 
 const ResendEmailButton = React.lazy(() => import('../../Validation/ResendEmailButton'))
@@ -36,12 +37,15 @@ const AlertMessages: React.FC<{
          <Collapse unmountOnExit mountOnEnter in={invalidPassAttempt >= 2}>
             <Alert variant='standard' color='info'>
                Elfelejtetted a jelszavad? Küldjünk egy jelszó emlékeztető emailt?
-               <ResendEmailButton
-                  path='forgot-password'
-                  confirmCode={null}
-                  userEmail={emailOrUsername.value}
-                  onSnackbarOpen={setIsSnackOpen}
-               />
+               <br />
+               <React.Suspense fallback={<ButtonSuspense />}>
+                  <ResendEmailButton
+                     path='forgot-password'
+                     confirmCode={null}
+                     userEmail={emailOrUsername.value}
+                     onSnackbarOpen={setIsSnackOpen}
+                  />
+               </React.Suspense>
             </Alert>
          </Collapse>
          <Snackbar
