9a8f50116682cb04ac96636d93197449e8cb1cee
nagytommy76 <nagytommy76@gmail.com>
2022-06-02 19:47:46 +0200

    Created the SSD types

---
+++ react-frontend/src/page/ShopPages/SSD/SSDTypes.ts
@@ -0,0 +1,18 @@
+import { BaseProductType } from '../BaseTypes'
+
+export type SSDProductType = BaseProductType & {
+   details: SSDDetailsType
+}
+
+export type SSDDetailsType = {
+   connection: string
+   size: string
+   capacity: number
+   readingSpeed: number
+   writingSpeed: number
+   nandTechnology: string
+   tbw: number
+   warranity: number
+   description?: string
+   manufacturerPageUrl?: string
+}
