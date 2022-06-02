674cef09f6deb55bad0b4dd5fdcfff546439a73f
nagytommy76 <nagytommy76@gmail.com>
2022-06-02 19:48:21 +0200

    Created the SSD input form

---
+++ react-frontend/src/page/Admin/SSD/BaseInputFields.tsx
@@ -0,0 +1,140 @@
+import React, { useContext } from 'react'
+import { AdminContext } from '../Context/AdminContext'
+import { FormContainerStyle } from '../Components/Form/FormStyle'
+
+const BaseFields = React.lazy(() => import('../Components/BaseInputs'))
+const TextOrNumberInput = React.lazy(() => import('../Components/InputFields/TextOrNumberInput'))
+
+const BaseInputFields = () => {
+   const { productInputs, setProductInputs, validationErrors } = useContext(AdminContext)
+
+   return (
+      <FormContainerStyle>
+         <BaseFields
+            product={productInputs}
+            setProduct={setProductInputs}
+            validationErrors={validationErrors}
+         />
+         <TextOrNumberInput
+            id='warranity'
+            labelText='Garancia (hónap)'
+            onChangeEvent={event =>
+               setProductInputs({
+                  ...productInputs,
+                  details: { ...productInputs.details, warranity: parseInt(event.target.value) },
+               })
+            }
+            value={productInputs.details.warranity || 12}
+            validationErrorLocation='details.warranity'
+            validationErrors={validationErrors}
+         />
+         <TextOrNumberInput
+            id='capacity'
+            labelText='Kapacitás (GB)'
+            onChangeEvent={event =>
+               setProductInputs({
+                  ...productInputs,
+                  details: { ...productInputs.details, capacity: parseInt(event.target.value) },
+               })
+            }
+            value={productInputs.details.capacity || 0}
+            validationErrorLocation='details.capacity'
+            validationErrors={validationErrors}
+         />
+         <TextOrNumberInput
+            required={false}
+            id='manufacturerPageUrl'
+            labelText='Gyártói honlap link'
+            onChangeEvent={event =>
+               setProductInputs({
+                  ...productInputs,
+                  details: { ...productInputs.details, manufacturerPageUrl: event.target.value },
+               })
+            }
+            value={productInputs.details.manufacturerPageUrl || ''}
+            validationErrorLocation='details.manufacturerPageUrl'
+            validationErrors={validationErrors}
+         />
+         <TextOrNumberInput
+            id='size'
+            labelText='Méret'
+            onChangeEvent={event =>
+               setProductInputs({
+                  ...productInputs,
+                  details: { ...productInputs.details, size: event.target.value },
+               })
+            }
+            value={productInputs.details.size || ''}
+            validationErrorLocation='details.size'
+            validationErrors={validationErrors}
+         />
+         <TextOrNumberInput
+            id='connection'
+            labelText='Csatoló felület'
+            onChangeEvent={event =>
+               setProductInputs({
+                  ...productInputs,
+                  details: { ...productInputs.details, connection: event.target.value },
+               })
+            }
+            value={productInputs.details.connection || ''}
+            validationErrorLocation='details.connection'
+            validationErrors={validationErrors}
+         />
+         <TextOrNumberInput
+            id='nandTechnology'
+            labelText='NAND Technológia '
+            onChangeEvent={event =>
+               setProductInputs({
+                  ...productInputs,
+                  details: { ...productInputs.details, nandTechnology: event.target.value },
+               })
+            }
+            value={productInputs.details.nandTechnology || ''}
+            validationErrorLocation='details.nandTechnology'
+            validationErrors={validationErrors}
+         />
+         <TextOrNumberInput
+            id='maxReadSpeed'
+            labelText='Maximális olvasási sebesség (MB/s)'
+            onChangeEvent={event =>
+               setProductInputs({
+                  ...productInputs,
+                  details: { ...productInputs.details, readingSpeed: parseInt(event.target.value) },
+               })
+            }
+            value={productInputs.details.readingSpeed || 0}
+            validationErrorLocation='details.readingSpeed'
+            validationErrors={validationErrors}
+         />
+         <TextOrNumberInput
+            id='maxWriteSpeed'
+            labelText='Minimum írási sebesség (MB/s)'
+            onChangeEvent={event =>
+               setProductInputs({
+                  ...productInputs,
+                  details: { ...productInputs.details, writingSpeed: parseInt(event.target.value) },
+               })
+            }
+            value={productInputs.details.writingSpeed || 0}
+            validationErrorLocation='details.writingSpeed'
+            validationErrors={validationErrors}
+         />
+         <TextOrNumberInput
+            id='tbw'
+            labelText='TBW (TB)'
+            onChangeEvent={event =>
+               setProductInputs({
+                  ...productInputs,
+                  details: { ...productInputs.details, tbw: parseInt(event.target.value) },
+               })
+            }
+            value={productInputs.details.tbw || 0}
+            validationErrorLocation='details.tbw'
+            validationErrors={validationErrors}
+         />
+      </FormContainerStyle>
+   )
+}
+
+export default BaseInputFields
--- react-frontend/src/page/Admin/SSD/Insert/InsertSsd.tsx
+++ react-frontend/src/page/Admin/SSD/Insert/InsertSsd.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { lazy, useState } from 'react'
+import { AdminContext } from '../../Context/AdminContext'
+
+import { ssdProperties } from '../SSDProperties'
+import { ValidationError } from '../../AdminTypes'
+import { PictureUrlType } from '../../Vga/Types'
+import { SSDProductType } from '../../../ShopPages/SSD/SSDTypes'
+
+const BaseProductInsert = lazy(() => import('../../Components/InsertComponent/BaseInsert'))
+const BaseSSDInputFields = lazy(() => import('../BaseInputFields'))
 
 const InsertSsd = () => {
+   const [SsdProducts, setSsdProducts] = useState<SSDProductType>(ssdProperties)
+   const [pictureUrls, setPictureUrls] = useState<PictureUrlType[]>([])
+   const [validationErrors, setValidationErrors] = useState<ValidationError[]>([
+      { location: '', msg: '', param: '' },
+   ])
+
    return (
-      <div>
-         <h1>SSD</h1>
-      </div>
+      <AdminContext.Provider
+         value={{
+            productInputs: SsdProducts,
+            setProductInputs: setSsdProducts,
+            validationErrors,
+            setValidationErrors,
+            selectedProductPictureUrls: pictureUrls,
+            setSelectedProductPictureUrls: setPictureUrls,
+         }}
+      >
+         <BaseProductInsert productProperties={ssdProperties} productType='ssd' title='SSD meghajtók'>
+            <BaseSSDInputFields />
+         </BaseProductInsert>
+      </AdminContext.Provider>
    )
 }
 
