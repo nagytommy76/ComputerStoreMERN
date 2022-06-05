36a5153ddd9882fd0d50f39890aa35946d71e85e
nagytommy76 <nagytommy76@gmail.com>
2022-06-05 14:41:57 +0200

    Created the insert SSD controller

--- node-api/controllers/Admin/SSD/SSD.ts
+++ node-api/controllers/Admin/SSD/SSD.ts
@@ -2,10 +2,39 @@ import { Request, Response } from 'express'
 import { SSDDetailsType, SSDProduct } from '../../../models/Products/SSD/SSD'
 import baseAdminController, { BaseProductProperties } from '../BaseController'
 
+const AdminController = baseAdminController(SSDProduct)
+
 export const insertSSDProductController = async (request: Request, response: Response) => {
    try {
-      response.status(200).json({ msg: 'sikeres SSD bevitel!!!' })
+      const {
+         details,
+         inStockQuantity,
+         manufacturer,
+         pictureUrls,
+         price,
+         type,
+         isHighlighted,
+         itemNumber,
+         typeCode,
+      } = request.body as InsertBodyType
+      await AdminController.insert(details, {
+         inStockQuantity,
+         manufacturer,
+         pictureUrls,
+         price,
+         type,
+         isHighlighted,
+         itemNumber,
+         typeCode,
+      })
+
+      response.status(201).json({ msg: 'sikeres SSD bevitel!!!' })
    } catch (error) {
       response.status(500).json(error)
    }
+}
+
+type InsertBodyType = BaseProductProperties & {
+   _id: string
+   details: SSDDetailsType
 }
