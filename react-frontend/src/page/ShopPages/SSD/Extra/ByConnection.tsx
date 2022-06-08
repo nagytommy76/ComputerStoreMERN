9f59cc0857ec96862a7ba8aaf2cab0bbed4c117d
nagytommy76 <nagytommy76@gmail.com>
2022-06-08 19:53:43 +0200

    Created the base connection select field

---
+++ react-frontend/src/page/ShopPages/SSD/Extra/ByConnection.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useAppSelector } from '../../../../app/hooks'
+import { setSelectedConnections } from '../../../../app/slices/Filter/SsdFilterSlice'
+
+const BaseConnectionSelect = React.lazy(() => import('../../BaseComponents/SideFilter/Base/BaseSelect'))
+
+const ByConnection = () => {
+   const { allConnection, selectedConnection } = useAppSelector(state => state.ssdFilter)
+   return (
+      <BaseConnectionSelect
+         allOption={allConnection}
+         helperText='Csatlakozás'
+         selectedOption={selectedConnection}
+         setSelectedDispatchValue={setSelectedConnections}
+         labelText='Csatlakozás'
+      >
+         <option value='all'>Összes</option>
+      </BaseConnectionSelect>
+   )
+}
+
+export default ByConnection
