ca0f5c5e75f84edee2c2981a9244ad646f7058fe
nagytommy76 <nagytommy76@gmail.com>
2022-06-11 14:46:56 +0200

    Added the get ssd ratings controllers to the route

--- node-api/routes/api/SSD/SSD.ts
+++ node-api/routes/api/SSD/SSD.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express'
 import SSDProduct from '../../../controllers/Products/SSD/SSD'
+import {
+   getSSDRatingSummaryController,
+   getAllSSDComments,
+} from '../../../controllers/Products/SSD/SSDRatings'
 
 const ssdProduct = new SSDProduct()
 
@@ -7,5 +11,10 @@ const router = Router()
 
 router.get('/', ssdProduct.getAllSSDProductController)
 router.get('/filter-data', ssdProduct.getSSDFilterDataController)
+router.get('/details', ssdProduct.getSSDDetailsController)
+
+// Ratings
+router.get('/get-ssd-rates', getSSDRatingSummaryController)
+router.get('/get-ssd-comments', getAllSSDComments)
 
 module.exports = router
