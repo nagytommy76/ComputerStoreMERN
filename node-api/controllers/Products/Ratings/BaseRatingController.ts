ac9a03650ec694d0a20b300578425b163141d2c2
nagytommy76 <nagytommy76@gmail.com>
2022-06-13 18:54:52 +0200

    Created a BaseRatingController to replace every productRatingController

---
+++ node-api/controllers/Products/Ratings/BaseRatingController.ts
@@ -0,0 +1,133 @@
+import { Response } from 'express'
+import { Model } from 'mongoose'
+import BaseRatingController from '../Ratings/BaseRating'
+
+import { RequestWithQueryId } from '../../Types'
+import {
+   LikeQuery,
+   RateQueryRequest,
+   RemoveRatingRequest,
+   SaveRequesType,
+   RemoveRequesType,
+} from '../Ratings/RatingTypes'
+
+export default class BaseRating {
+   private ProductModel: Model<any>
+   private BaseRatingHelper
+
+   constructor(ProductModel: Model<any>) {
+      this.ProductModel = ProductModel
+      this.BaseRatingHelper = BaseRatingController(ProductModel)
+   }
+
+   getRatingSummaryController = async (req: RequestWithQueryId, res: Response) => {
+      try {
+         const returnRatingValues = await this.BaseRatingHelper.getProductRatingSummary(req.query._id)
+         return res.status(200).json(returnRatingValues)
+      } catch (error) {
+         return res.status(500).json(error)
+      }
+   }
+
+   getAllComments = async (req: RequestWithQueryId, res: Response) => {
+      try {
+         const allComments = await this.ProductModel.find({ _id: req.query._id }, 'ratingValues')
+         return res.status(200).json(allComments[0].ratingValues)
+      } catch (error) {
+         return res.status(500).json(error)
+      }
+   }
+
+   rateProductController = async (req: RateQueryRequest, res: Response) => {
+      try {
+         const modifiedProduct = await this.BaseRatingHelper.saveRateProductHelper(
+            req.body.productId,
+            req.body.rating,
+            req.body.comment,
+            req.body.userName,
+            req.user?._id
+         )
+         if (modifiedProduct !== undefined) {
+            modifiedProduct.save()
+            return res.sendStatus(201)
+         } else return res.sendStatus(422)
+      } catch (error) {
+         return res.status(500).json({ error })
+      }
+   }
+
+   removeUsersRatingController = async (req: RemoveRatingRequest, res: Response) => {
+      try {
+         const {
+            body: { productId, commentIdToDelete },
+         } = req as RemoveRatingRequest
+         const result = await this.BaseRatingHelper.removeUsersRating(
+            productId,
+            commentIdToDelete,
+            req.user?._id
+         )
+         switch (result.statusCode) {
+            case 200:
+               return res.status(result.statusCode).json(result)
+            case 404:
+               return res.sendStatus(404)
+         }
+      } catch (error) {
+         return res.status(500).json(error)
+      }
+   }
+
+   // Rating answers
+   saveAnswerController = async (req: SaveRequesType, res: Response) => {
+      try {
+         const { foundProduct, newCommentAnswers } = await this.BaseRatingHelper.saveProductAnswerController(
+            req.body.productId,
+            req.body.commentId,
+            req.body.answer,
+            req.user
+         )
+         foundProduct.save()
+         return res.status(201).json(newCommentAnswers)
+      } catch (error) {
+         res.status(500).json({ error })
+      }
+   }
+
+   removeSingleCommentAnswer = async (req: RemoveRequesType, res: Response) => {
+      try {
+         const { foundComment, foundProduct } = await this.BaseRatingHelper.removeProductAnswerController(
+            req.body.productId,
+            req.body.commentId,
+            req.body.answerId
+         )
+         if (foundProduct) {
+            foundProduct.save()
+            return res.status(200).json(foundComment)
+         }
+         return res.sendStatus(404)
+      } catch (error) {
+         return res.status(500).json(error)
+      }
+   }
+
+   likeDislikeCommentController = async (req: LikeQuery, res: Response) => {
+      try {
+         const result = await this.BaseRatingHelper.likeDislikeComment(
+            req.body.productId,
+            req.body.commentId,
+            req.user?._id,
+            req.body.isLike
+         )
+         switch (result.statusCode) {
+            case 201:
+               return res.status(result.statusCode).json({ responses: result.responses })
+            case 405:
+               return res.status(result.statusCode).json(result)
+            case 404:
+               return res.sendStatus(result.statusCode)
+         }
+      } catch (error) {
+         return res.status(500).json(error)
+      }
+   }
+}
