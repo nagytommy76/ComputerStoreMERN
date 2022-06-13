b6f59134051814e13301e861f21bd9e1c8db2773
nagytommy76 <nagytommy76@gmail.com>
2022-06-13 18:55:55 +0200

    Added the new BaseRating functions to HDD/SSD route

--- node-api/routes/api/HDD/HDD.ts
+++ node-api/routes/api/HDD/HDD.ts
@@ -1,17 +1,11 @@
 import { Router } from 'express'
 import { authenticateAccessToken } from '../../../middlewares/AuthenticateAccessOrRefreshTokens'
 import HDDProduct from '../../../controllers/Products/HDD/HDD'
-import {
-   getAllHDDComments,
-   getHDDRatingSummaryController,
-   rateHDDProductController,
-   likeDislikeHDDCommentController,
-   removeUsersRatingInHDDController,
-   saveHddAnswerController,
-   removeSingleHddCommentAnswer,
-} from '../../../controllers/Products/HDD/HDDRating'
+import { HddProduct as HDDModel } from '../../../models/Products/HDD/Hdd'
+import BaseRatingController from '../../../controllers/Products/Ratings/BaseRatingController'
 
 const hddProduct = new HDDProduct()
+const BaseRating = new BaseRatingController(HDDModel)
 
 const router = Router()
 
@@ -20,15 +14,15 @@ router.get('/filter-data', hddProduct.getHDDFilterData)
 router.get('/details', hddProduct.getHDDDetailsController)
 
 // Ratings
-router.get('/get-hdd-rates', getHDDRatingSummaryController)
-router.get('/get-hdd-comments', getAllHDDComments)
-router.delete('/hdd-comment-remove', authenticateAccessToken, removeUsersRatingInHDDController)
+router.get('/get-hdd-rates', BaseRating.getRatingSummaryController)
+router.get('/get-hdd-comments', BaseRating.getAllComments)
+router.delete('/hdd-comment-remove', authenticateAccessToken, BaseRating.removeUsersRatingController)
 
-router.post('/rate-hdd', authenticateAccessToken, rateHDDProductController)
-router.post('/hdd-comment-like', authenticateAccessToken, likeDislikeHDDCommentController)
+router.post('/rate-hdd', authenticateAccessToken, BaseRating.rateProductController)
+router.post('/hdd-comment-like', authenticateAccessToken, BaseRating.likeDislikeCommentController)
 
 // Rating answers
-router.post('/save-hdd-answer', authenticateAccessToken, saveHddAnswerController)
-router.delete('/hdd-answer-remove', authenticateAccessToken, removeSingleHddCommentAnswer)
+router.post('/save-hdd-answer', authenticateAccessToken, BaseRating.saveAnswerController)
+router.delete('/hdd-answer-remove', authenticateAccessToken, BaseRating.removeSingleCommentAnswer)
 
 module.exports = router
--- node-api/routes/api/SSD/SSD.ts
+++ node-api/routes/api/SSD/SSD.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express'
+import { authenticateAccessToken } from '../../../middlewares/AuthenticateAccessOrRefreshTokens'
 import SSDProduct from '../../../controllers/Products/SSD/SSD'
-import {
-   getSSDRatingSummaryController,
-   getAllSSDComments,
-} from '../../../controllers/Products/SSD/SSDRatings'
+import { SSDProduct as SSDModel } from '../../../models/Products/SSD/SSD'
+import BaseRatingController from '../../../controllers/Products/Ratings/BaseRatingController'
 
 const ssdProduct = new SSDProduct()
+const BaseRating = new BaseRatingController(SSDModel)
 
 const router = Router()
 
@@ -14,7 +14,15 @@ router.get('/filter-data', ssdProduct.getSSDFilterDataController)
 router.get('/details', ssdProduct.getSSDDetailsController)
 
 // Ratings
-router.get('/get-ssd-rates', getSSDRatingSummaryController)
-router.get('/get-ssd-comments', getAllSSDComments)
+router.get('/get-ssd-rates', BaseRating.getRatingSummaryController)
+router.get('/get-ssd-comments', BaseRating.getAllComments)
+router.delete('/ssd-comment-remove', authenticateAccessToken, BaseRating.removeUsersRatingController)
+
+router.post('/rate-ssd', authenticateAccessToken, BaseRating.rateProductController)
+router.post('/ssd-comment-like', authenticateAccessToken, BaseRating.likeDislikeCommentController)
+
+// Rating answers
+router.post('/save-ssd-answer', authenticateAccessToken, BaseRating.saveAnswerController)
+router.delete('/ssd-answer-remove', authenticateAccessToken, BaseRating.removeSingleCommentAnswer)
 
 module.exports = router
