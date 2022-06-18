e8e96de631b62caa425dd00c9dc7ef161f1b2cd9
nagytommy76 <nagytommy76@gmail.com>
2022-06-18 16:14:30 +0200

    Using the BaseSelect component

--- react-frontend/src/page/ShopPages/BaseComponents/SideFilter/Includes/ByManufacturer.tsx
+++ react-frontend/src/page/ShopPages/BaseComponents/SideFilter/Includes/ByManufacturer.tsx
@@ -1,41 +1,22 @@
 import React from 'react'
-import { InputContainer } from '../FilterStyle'
-import { TextField } from '@mui/material'
-import { useAppSelector, useAppDispatch } from '../../../../../app/hooks'
-import { setIsPriceRangeSet, setSelectedManufacturer } from '../../../../../app/slices/Filter/BaseFilterDataSlice'
+import { useAppSelector } from '../../../../../app/hooks'
+import { setSelectedManufacturer } from '../../../../../app/slices/Filter/BaseFilterDataSlice'
+
+const BaseSelect = React.lazy(() => import('../Base/BaseSelect'))
 
 const ByManufacturer: React.FC = () => {
-   const dispatch = useAppDispatch()
    const { allManufacturer, selectedManufacturer } = useAppSelector(state => state.filter.filterData)
 
-   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      dispatch(setSelectedManufacturer(event.target.value))
-      dispatch(setIsPriceRangeSet(true))
-   }
    return (
-      <InputContainer>
-         <TextField
-            id='manufacturer'
-            fullWidth
-            select
-            label='Gyártó'
-            onChange={handleChange}
-            helperText='Szűrés gyártók szerint'
-            variant='filled'
-            color='primary'
-            value={selectedManufacturer}
-            SelectProps={{
-               native: true,
-            }}
-         >
-            <option value='all'>Összes</option>
-            {allManufacturer.map((man, index) => (
-               <option key={index} value={man}>
-                  {man}
-               </option>
-            ))}
-         </TextField>
-      </InputContainer>
+      <BaseSelect
+         allOption={allManufacturer}
+         selectedOption={selectedManufacturer}
+         helperText='Szűrés gyártók szerint'
+         labelText='Gyártó'
+         setSelectedDispatchValue={setSelectedManufacturer}
+      >
+         <option value='all'>Összes</option>
+      </BaseSelect>
    )
 }
 
