8c5899229a29f9e0802d04f554d9e87ee44d2800
nagytommy76 <nagytommy76@gmail.com>
2022-06-18 12:14:51 +0200

    Used the baseSelect component

--- react-frontend/src/page/ShopPages/BaseComponents/SideFilter/Includes/ByWarranity.tsx
+++ react-frontend/src/page/ShopPages/BaseComponents/SideFilter/Includes/ByWarranity.tsx
@@ -1,41 +1,23 @@
 import React from 'react'
-import { InputContainer } from '../FilterStyle'
-import { TextField } from '@mui/material'
-import { useAppSelector, useAppDispatch } from '../../../../../app/hooks'
-import { setSelectedWarranty, setIsPriceRangeSet } from '../../../../../app/slices/Filter/BaseFilterDataSlice'
+import { useAppSelector } from '../../../../../app/hooks'
+import { setSelectedWarranty } from '../../../../../app/slices/Filter/BaseFilterDataSlice'
+
+const BaseSelect = React.lazy(() => import('../Base/BaseSelect'))
 
 const ByWarranity = () => {
-   const dispatch = useAppDispatch()
    const { allWarranties, selectedWarranty } = useAppSelector(state => state.filter.filterData)
 
-   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      dispatch(setSelectedWarranty(event.target.value))
-      dispatch(setIsPriceRangeSet(true))
-   }
    return (
-      <InputContainer>
-         <TextField
-            id='warranity'
-            fullWidth
-            select
-            label='Garancia'
-            onChange={handleChange}
-            helperText='Szűrés garancia idő szerint'
-            variant='filled'
-            color='primary'
-            value={selectedWarranty}
-            SelectProps={{
-               native: true,
-            }}
-         >
-            <option value='all'>Összes</option>
-            {allWarranties.map((warranty, index) => (
-               <option key={index} value={warranty}>
-                  {warranty}
-               </option>
-            ))}
-         </TextField>
-      </InputContainer>
+      <BaseSelect
+         allOption={allWarranties}
+         selectedOption={selectedWarranty}
+         helperText='Szűrés garancia idő szerint'
+         labelText='Garancia'
+         setSelectedDispatchValue={setSelectedWarranty}
+         postFix=' hónap'
+      >
+         <option value='all'>Összes</option>
+      </BaseSelect>
    )
 }
 
