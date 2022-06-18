043eca6d13a921fab6f3b009641aec39c7f16164
nagytommy76 <nagytommy76@gmail.com>
2022-06-18 16:13:50 +0200

    Created a dialog to confirm a comment delete

---
+++ react-frontend/src/page/ShopPages/BaseComponents/ProductDetailsPage/Ratings/Comments/Includes/DeleteDialog.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogContentText from '@mui/material/DialogContentText'
+import DialogTitle from '@mui/material/DialogTitle'
+import Button from '@mui/material/Button'
+
+const DeleteDialog: React.FC<{
+   setDialogAnswer: React.Dispatch<React.SetStateAction<boolean>>
+   setOpenDialog: React.Dispatch<React.SetStateAction<boolean>>
+   openDialog: boolean
+}> = ({ setOpenDialog, setDialogAnswer, openDialog }) => {
+   const handleClose = () => {
+      setOpenDialog(false)
+   }
+
+   const handleDialogAnswerFalse = () => {
+      setDialogAnswer(false)
+      setOpenDialog(false)
+   }
+
+   const handleDialogAnswerTrue = () => {
+      setDialogAnswer(true)
+      setOpenDialog(false)
+   }
+   return (
+      <Dialog open={openDialog} onClose={handleClose}>
+         <DialogTitle>Komment törlése</DialogTitle>
+         <DialogContent>
+            <DialogContentText>Biztos vagy benne, hogy törölni szeretnéd a kommentet?</DialogContentText>
+         </DialogContent>
+         <DialogActions>
+            <Button variant='outlined' color='error' onClick={handleDialogAnswerFalse}>
+               Nem
+            </Button>
+            <Button variant='outlined' color='success' onClick={handleDialogAnswerTrue}>
+               Igen
+            </Button>
+         </DialogActions>
+      </Dialog>
+   )
+}
+
+export default DeleteDialog
