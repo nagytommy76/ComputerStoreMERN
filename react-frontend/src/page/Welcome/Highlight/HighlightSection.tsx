7c0921e68fc27ed1ad0b558b8c4c94292f7d3276
nagytommy76 <nagytommy76@gmail.com>
2022-06-30 19:28:06 +0200

    Added the Hdd/Ssd base highlight

--- react-frontend/src/page/Welcome/Highlight/HighlightSection.tsx
+++ react-frontend/src/page/Welcome/Highlight/HighlightSection.tsx
@@ -9,12 +9,16 @@ const HighlightSection = () => {
    const [CpuHighlight, setCpuHighlight] = useState<HighlightDataType>([])
    const [VgaHighlight, setVgaHighlight] = useState<HighlightDataType>([])
    const [MemoryHighlight, setMemoryHighlight] = useState<HighlightDataType>([])
+   const [HDDHighlight, setHDDHighlight] = useState<HighlightDataType>([])
+   const [SSDHighlight, setSSDHighlight] = useState<HighlightDataType>([])
    const getHighlights = async () => {
       try {
          const response = await axiosInstance.get('/highlight/get-highlight')
          setCpuHighlight(response.data.CpuHighlights)
          setVgaHighlight(response.data.VgaHighlights)
          setMemoryHighlight(response.data.MemoryHighlights)
+         setHDDHighlight(response.data.HddHighlights)
+         setSSDHighlight(response.data.SSDHighlights)
       } catch (error) {
          if (isAxiosError(error)) {
             console.log(error.response?.data)
@@ -30,6 +34,8 @@ const HighlightSection = () => {
          <BaseHighlight highlightData={CpuHighlight} productType='cpu' />
          <BaseHighlight highlightData={VgaHighlight} productType='vga' borderColor='#7B241C' />
          <BaseHighlight highlightData={MemoryHighlight} productType='memory' borderColor='#566573 ' />
+         <BaseHighlight highlightData={HDDHighlight} productType='hdd' borderColor='#07b42d ' />
+         <BaseHighlight highlightData={SSDHighlight} productType='ssd' borderColor='#c70323 ' />
       </ContainerStyle>
    )
 }
