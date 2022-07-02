99e367a09fa490b28837bdd92f9e188e61cb1f38
nagytommy76 <nagytommy76@gmail.com>
2022-07-02 14:08:33 +0200

    Created the delete/getToDelete/modify/getAll SSD admin controllers

--- node-api/controllers/Admin/SSD/SSD.ts
+++ node-api/controllers/Admin/SSD/SSD.ts
@@ -4,6 +4,15 @@ import baseAdminController, { BaseProductProperties } from '../BaseController'
 
 const AdminController = baseAdminController(SSDProduct)
 
+export const getAllSSDController = async (request: Request, response: Response) => {
+   try {
+      const ssdProducts = await AdminController.getAllProduct()
+      response.status(200).json({ allProducts: ssdProducts })
+   } catch (error) {
+      response.status(500).json(error)
+   }
+}
+
 export const insertSSDProductController = async (request: Request, response: Response) => {
    try {
       const {
@@ -29,6 +38,59 @@ export const insertSSDProductController = async (request: Request, response: Res
       })
 
       response.status(201).json({ msg: 'sikeres SSD bevitel!!!' })
+   } catch (error) {
+      response.status(500).json(error)
+   }
+}
+
+export const modifySSDProductController = async (request: Request, response: Response) => {
+   try {
+      const {
+         _id,
+         details,
+         inStockQuantity,
+         manufacturer,
+         pictureUrls,
+         price,
+         type,
+         isHighlighted,
+         itemNumber,
+         typeCode,
+      } = request.body as InsertBodyType
+      const foundSSDProduct = await AdminController.getProductToModify(_id)
+      if (foundSSDProduct) {
+         foundSSDProduct.details = details
+         AdminController.modifyChartData(foundSSDProduct.details, price)
+         foundSSDProduct.inStockQuantity = inStockQuantity
+         foundSSDProduct.manufacturer = manufacturer
+         foundSSDProduct.pictureUrls = pictureUrls
+         foundSSDProduct.price = price
+         foundSSDProduct.type = type
+         foundSSDProduct.isHighlighted = isHighlighted
+         foundSSDProduct.itemNumber = itemNumber
+         foundSSDProduct.typeCode = typeCode
+         foundSSDProduct.save()
+         return response.sendStatus(201)
+      }
+      response.sendStatus(404)
+   } catch (error) {
+      response.status(500).json(error)
+   }
+}
+
+export const getAllSSDItemsForDeleteController = async (request: Request, response: Response) => {
+   try {
+      const allSSDs = await AdminController.getAllToDeleteProducts()
+      return response.status(200).json({ allProducts: allSSDs })
+   } catch (error) {
+      response.status(500).json(error)
+   }
+}
+
+export const deleteSSDProductByIdController = async (request: Request, response: Response) => {
+   try {
+      await AdminController.delete(request.body.productID)
+      response.status(200).json({ msg: 'sikeres törlés', deleted: true })
    } catch (error) {
       response.status(500).json(error)
    }
