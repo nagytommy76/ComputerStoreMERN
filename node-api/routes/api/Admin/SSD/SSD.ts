fefc694869d8bfe3425350cc26dfc8477435324f
nagytommy76 <nagytommy76@gmail.com>
2022-07-02 14:08:55 +0200

    Added the controllers to the admin route

--- node-api/routes/api/Admin/SSD/SSD.ts
+++ node-api/routes/api/Admin/SSD/SSD.ts
@@ -1,13 +1,24 @@
 import { Router } from 'express'
 
-import { insertSSDProductController } from '../../../../controllers/Admin/SSD/SSD'
 import { checkUserIsAdmin } from '../../../../middlewares/AuthenticateAccessOrRefreshTokens'
 import { checkErrors } from '../../../../middlewares/CheckValidationErrors'
+import {
+   insertSSDProductController,
+   getAllSSDController,
+   deleteSSDProductByIdController,
+   getAllSSDItemsForDeleteController,
+   modifySSDProductController,
+} from '../../../../controllers/Admin/SSD/SSD'
 
 import { insertSSDValidator } from './Validator/SsdValidator'
 
 const router = Router()
 
+router.get('get-all', checkUserIsAdmin, getAllSSDController)
+router.get('get-to-delete', checkUserIsAdmin, getAllSSDItemsForDeleteController)
+
 router.post('/insert', insertSSDValidator, checkUserIsAdmin, checkErrors, insertSSDProductController)
+router.post('/modify', insertSSDValidator, checkUserIsAdmin, checkErrors, modifySSDProductController)
+router.delete('/delete', checkUserIsAdmin, deleteSSDProductByIdController)
 
 module.exports = router
