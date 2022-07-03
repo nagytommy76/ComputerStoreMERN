a6f81c416d54003c03554b071d1fd25f70e22145
nagytommy76 <nagytommy76@gmail.com>
2022-07-03 14:10:53 +0200

    Created the remove single user controller

--- node-api/controllers/Admin/Users/Users.ts
+++ node-api/controllers/Admin/Users/Users.ts
@@ -9,9 +9,24 @@ import { User } from '../../../models/User/User'
 
 export const getAllUsers = async (request: Request, response: Response) => {
    try {
-      const allUsers = await User.find({})
+      const allUsers = await User.find().select({ userName: 1, email: 1, isAdmin: 1, isEmailConfirmed: 1 })
       response.status(200).json(allUsers)
    } catch (error) {
       response.status(500).json(error)
+   }
+}
+
+export const removeSingleUser = async (request: DeleteRequest, response: Response) => {
+   try {
+      const user = await User.findByIdAndDelete(request.body.userID)
+      response.status(200).json(user)
+   } catch (error) {
+      response.status(500).json(error)
+   }
+}
+
+type DeleteRequest = Request & {
+   body: {
+      userID: string
    }
 }
