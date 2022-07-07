826e1927dfc89c3fcc2753e6d2cd4b8d879b9f56
nagytommy76 <nagytommy76@gmail.com>
2022-07-07 17:50:47 +0200

    Added the get-all-rating route

--- node-api/routes/api/Admin/Users/Users.ts
+++ node-api/routes/api/Admin/Users/Users.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express'
 import { checkUserIsAdmin } from '../../../../middlewares/AuthenticateAccessOrRefreshTokens'
 
-import { getAllUsers, removeSingleUser } from '../../../../controllers/Admin/Users/Users'
+import {
+   getAllUsers,
+   removeSingleUser,
+   getAllRatingValuesByUserID,
+} from '../../../../controllers/Admin/Users/Users'
 
 const router = Router()
 
 router.get('/get-all', checkUserIsAdmin, getAllUsers)
+router.get('/get-all-rating', checkUserIsAdmin, getAllRatingValuesByUserID)
 
-router.delete('/remove-user', checkUserIsAdmin, removeSingleUser)
+router.delete('/delete', checkUserIsAdmin, removeSingleUser)
 
 module.exports = router
