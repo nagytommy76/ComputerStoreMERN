3aa2a8410677a8a5faac1b84418349256bea21ac
nagytommy76 <nagytommy76@gmail.com>
2022-07-11 20:04:30 +0200

    Finished the context provider, added default values

--- react-frontend/src/page/Admin/Users/Context/CommentContext.tsx
+++ react-frontend/src/page/Admin/Users/Context/CommentContext.tsx
@@ -1,17 +1,44 @@
-import { createContext, ReactNode } from 'react'
+import { createContext, ReactNode, useState } from 'react'
+
+import { SnackbarStateTypes } from '../../Components/DeleteComponents/Types'
 import { UserTypes } from '../UserTypes'
 
 export const CommentContext = createContext<CommentsContextType>({
    users: [],
+   usersLength: 0,
    setUsers: () => {},
+   selectedUserIdAndName: { userID: null, userName: '' },
+   setSelectedUserIdAndName: () => {},
+   isModalOpen: false,
+   setIsModalOpen: () => {},
+   isSnackOpen: { isOpen: false, deletedProductName: '' },
+   setIsSnackOpen: () => {},
 })
 
 export const CommentContextProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+   const [users, setUsers] = useState<UserTypes[]>([])
+   const [selectedUserIdAndName, setSelectedUserIdAndName] = useState<{
+      userID: string | null
+      userName: string
+   }>({ userID: null, userName: '' })
+   const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+   const [isSnackOpen, setIsSnackOpen] = useState<SnackbarStateTypes>({
+      isOpen: false,
+      deletedProductName: '',
+   })
+
    return (
       <CommentContext.Provider
          value={{
-            users: [],
-            setUsers: () => {},
+            users,
+            usersLength: users.length,
+            setUsers,
+            selectedUserIdAndName,
+            setSelectedUserIdAndName,
+            isModalOpen,
+            setIsModalOpen,
+            isSnackOpen,
+            setIsSnackOpen,
          }}
       >
          {children}
@@ -21,7 +48,22 @@ export const CommentContextProvider: React.FC<{ children: ReactNode }> = ({ chil
 
 export type CommentsContextType = {
    users: UserTypes[]
+   usersLength: number
    setUsers: React.Dispatch<React.SetStateAction<UserTypes[]>>
+   selectedUserIdAndName: {
+      userID: string | null
+      userName: string
+   }
+   setSelectedUserIdAndName: React.Dispatch<
+      React.SetStateAction<{
+         userID: string | null
+         userName: string
+      }>
+   >
+   isModalOpen: boolean
+   setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>
+   isSnackOpen: SnackbarStateTypes
+   setIsSnackOpen: React.Dispatch<React.SetStateAction<SnackbarStateTypes>>
 }
 
 // https://www.youtube.com/watch?v=NKsVV7wJcDM&t=178s&ab_channel=TheNetNinja
