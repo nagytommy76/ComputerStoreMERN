c02de2c7f53fe2b580e585f7a98a59643c3cd7df
nagytommy76 <nagytommy76@gmail.com>
2022-07-18 19:43:01 +0200

    Created a style for the navigation

---
+++ react-frontend/src/page/Admin/Users/Components/CommentsModal/Includes/Navigation/Include/Style.ts
@@ -0,0 +1,12 @@
+import { styled } from '@mui/material'
+import BottomNavigation from '@mui/material/BottomNavigation'
+
+export const StyledBottomNavigation = styled(BottomNavigation)({
+   position: 'fixed',
+   bottom: 0,
+   right: 0,
+   left: 0,
+
+   height: 90,
+   backgroundColor: 'transparent',
+})
