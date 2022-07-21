3b6b8724a9ede216054faa2a5790d7589dab6eab
nagytommy76 <nagytommy76@gmail.com>
2022-07-21 19:51:59 +0200

    Using the newly created useGetComments hook

--- react-frontend/src/page/Admin/Users/Components/CommentsModal/CommentsModal.tsx
+++ react-frontend/src/page/Admin/Users/Components/CommentsModal/CommentsModal.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState, useContext } from 'react'
+import React, { useContext } from 'react'
 import { CommentContext } from '../../Context/CommentContext'
-import { axiosInstance } from '../../../../../AxiosSetup/AxiosInstance'
+import useGetComments from './Hooks/useGetComments'
+import { IncomingCommentType } from './Reducer/ModalReducer'
 
 import Typography from '@mui/material/Typography'
 import Backdrop from '@mui/material/Backdrop'
@@ -8,31 +9,13 @@ import Modal from '@mui/material/Modal'
 import Fade from '@mui/material/Fade'
 
 import { StyledBox, StyledCommentSection } from './Styles'
-import { RateState } from '../../../../ShopPages/BaseComponents/ProductDetailsPage/Ratings/Comments/Helpers'
 
 const SingleCommentCard = React.lazy(() => import('./Includes/SingleCommentCard'))
 const BottomNavigation = React.lazy(() => import('./Includes/Navigation/Navigation'))
 
 const CommentsModal: React.FC = () => {
    const { selectedUserIdAndName, isModalOpen, setIsModalOpen } = useContext(CommentContext)
-   const [cpuComments, setCpuComments] = useState<IncomingCommentType[]>([])
-
-   useEffect(() => {
-      const fetchUserComments = async () => {
-         try {
-            const response = await axiosInstance.get(`/admin/users/get-all-rating`, {
-               params: {
-                  userID: selectedUserIdAndName.userID,
-               },
-            })
-            console.log(response.data)
-            setCpuComments(response.data.cpu)
-         } catch (error) {
-            console.log(error)
-         }
-      }
-      selectedUserIdAndName.userID && fetchUserComments()
-   }, [selectedUserIdAndName.userID])
+   const { commentsState } = useGetComments(selectedUserIdAndName?.userID)
 
    return (
       <Modal
@@ -57,7 +40,7 @@ const CommentsModal: React.FC = () => {
                   Kommentek {selectedUserIdAndName.userName} felhasználótól
                </Typography>
                <StyledCommentSection>
-                  {cpuComments.map((comment: IncomingCommentType) => (
+                  {commentsState.cpu.map((comment: IncomingCommentType) => (
                      <SingleCommentCard
                         key={comment._id}
                         leftTitle={`${comment.manufacturer} ${comment.type}`}
@@ -73,10 +56,3 @@ const CommentsModal: React.FC = () => {
 }
 
 export default CommentsModal
-
-type IncomingCommentType = {
-   manufacturer: string
-   _id: string
-   type: string
-   ratingValues: RateState[]
-}
