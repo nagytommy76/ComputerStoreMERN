945201a15b5625e66d898424f676eee00e58ee85
nagytommy76 <nagytommy76@gmail.com>
2022-07-23 15:43:55 +0200

    Solved the styling when the content is bigger than the modal (flex space-between)

--- react-frontend/src/page/Admin/Users/Components/CommentsModal/Includes/Navigation/Style.ts
+++ react-frontend/src/page/Admin/Users/Components/CommentsModal/Includes/Navigation/Style.ts
@@ -2,11 +2,6 @@ import { styled } from '@mui/material'
 import BottomNavigation from '@mui/material/BottomNavigation'
 
 export const StyledBottomNavigation = styled(BottomNavigation)({
-   position: 'fixed',
-   bottom: 0,
-   right: 0,
-   left: 0,
-
-   height: 90,
+   height: 70,
    backgroundColor: 'transparent',
 })
--- react-frontend/src/page/Admin/Users/Components/CommentsModal/Styles.ts
+++ react-frontend/src/page/Admin/Users/Components/CommentsModal/Styles.ts
@@ -2,22 +2,29 @@ import { styled } from '@mui/material'
 import { mobileWindowSize } from '../../../../../Theme/GlobalStyles'
 
 import CardContent from '@mui/material/CardContent'
+import Paper from '@mui/material/Paper'
 
-export const StyledBox = styled('section')(({ theme }) => ({
-   position: 'absolute',
+export const StyledBox = styled(Paper)(({ theme }) => ({
+   position: 'relative',
    top: '50%',
    left: '50%',
    transform: 'translate(-50%, -50%)',
 
+   display: 'flex',
+   flexDirection: 'column',
+   justifyContent: 'space-between',
+
    outline: 'none',
    padding: '1rem',
-   width: '60%',
-   minHeight: '60%',
+   width: '50%',
+   height: '60%',
    borderRadius: '5px',
+   overflow: 'auto',
 
    boxShadow: '2px 0px 20px rgba(0, 0,0, 0.2)',
    background: 'linear-gradient(to right, hsl(210, 60%, 20%), hsl(210, 20%, 25%))',
    color: '#FFF',
+
    // backgroundColor: theme.palette.primary.dark,
    // color: theme.palette.mode === 'dark' ? '#fff' : '#000',
 
@@ -26,7 +33,6 @@ export const StyledBox = styled('section')(({ theme }) => ({
 
 export const StyledCommentSection = styled('section')(({ theme }) => ({
    width: '100%',
-   height: '100%',
 }))
 
 export const StyledCommentCard = styled(CardContent)(({ theme }) => ({
