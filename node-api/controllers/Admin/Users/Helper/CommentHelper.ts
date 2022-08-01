5c6bc9b1200101cec7cb9b2530152ac24f0bb33a
nagytommy76 <nagytommy76@gmail.com>
2022-08-01 19:56:33 +0200

    Saving the product model

--- node-api/controllers/Admin/Users/Helper/CommentHelper.ts
+++ node-api/controllers/Admin/Users/Helper/CommentHelper.ts
@@ -1,5 +1,4 @@
 import { Model, ObjectId } from 'mongoose'
-
 import { RatingValues } from '../../../../models/Products/BaseTypes'
 
 // Itt szintén nem jó az any, Megoldani!!!
@@ -11,13 +10,16 @@ export const removeSingleCommentFromRatingValues = async (
    try {
       const product = await ProductModel.findOne({ _id: productID })
       if (product === null) {
-         return { msg: 'Nincs ilyen komment', ratingValues: null }
+         return { statusCode: 404, msg: 'Nincs ilyen termék' }
       }
       product.ratingValues = product.ratingValues.filter((rating: RatingValues) => {
          return rating._id != commentID
       })
-      // product.save()
-      return { msg: 'sikeres törlés', ratingValues: product.ratingValues as RatingValues[] }
+      product.save()
+      return {
+         msg: 'sikeres törlés',
+         statusCode: 200,
+      }
    } catch (error) {
       throw new Error('Valami gond van a komment törlésekor')
    }
