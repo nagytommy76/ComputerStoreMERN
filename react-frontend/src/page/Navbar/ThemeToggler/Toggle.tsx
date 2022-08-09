7832071431c441f0d99a955f0b29cafa8bcf822b
nagytommy76 <nagytommy76@gmail.com>
2022-08-09 17:28:12 +0200

    Called the set preferred them dispatch

--- react-frontend/src/page/Navbar/ThemeToggler/Toggle.tsx
+++ react-frontend/src/page/Navbar/ThemeToggler/Toggle.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyledToggle } from './ToggleContainer'
-import { setTheme } from '../../../app/slices/ThemeSlice'
+import { setTheme, setIsPreferredThemeSetByUser } from '../../../app/slices/ThemeSlice'
 
 import { ReactComponent as Moon } from './icons/moon.svg'
 import { ReactComponent as Sun } from './icons/sun.svg'
@@ -8,9 +8,10 @@ import { useAppSelector, useAppDispatch } from '../../../app/hooks'
 
 const Toggle: React.FC = () => {
    const dispatch = useAppDispatch()
-   const currentTheme = useAppSelector((state) => state.theme.isDarkTheme)
+   const currentTheme = useAppSelector(state => state.theme.isDarkTheme)
 
    const toggleTheme = () => {
+      dispatch(setIsPreferredThemeSetByUser(true))
       currentTheme ? dispatch(setTheme(false)) : dispatch(setTheme(true))
    }
 
