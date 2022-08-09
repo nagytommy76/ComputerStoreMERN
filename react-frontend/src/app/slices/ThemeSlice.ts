56e80d9e32b67dc30905a7ab6f14320a28b7355d
nagytommy76 <nagytommy76@gmail.com>
2022-08-09 17:26:25 +0200

    Renamed the preferred theme state

--- react-frontend/src/app/slices/ThemeSlice.ts
+++ react-frontend/src/app/slices/ThemeSlice.ts
@@ -2,10 +2,12 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 export type ThemeType = {
    isDarkTheme: boolean
+   isPreferredThemeSetByUser: boolean
 }
 
 const initialState: ThemeType = {
-   isDarkTheme: false
+   isDarkTheme: false,
+   isPreferredThemeSetByUser: false,
 }
 
 export const ThemeSlice = createSlice({
@@ -14,9 +16,12 @@ export const ThemeSlice = createSlice({
    reducers: {
       setTheme: (state, action: PayloadAction<boolean>) => {
          state.isDarkTheme = action.payload
-      }
-   }
+      },
+      setIsPreferredThemeSetByUser: (state, action: PayloadAction<boolean>) => {
+         state.isPreferredThemeSetByUser = action.payload
+      },
+   },
 })
 
-export const { setTheme } = ThemeSlice.actions
+export const { setTheme, setIsPreferredThemeSetByUser } = ThemeSlice.actions
 export default ThemeSlice.reducer
