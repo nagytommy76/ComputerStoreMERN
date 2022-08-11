7c743ad64231eedf94a6a00c1e26336653a03709
nagytommy76 <nagytommy76@gmail.com>
2022-08-11 16:43:57 +0200

    Created a cart button skeleton in mobile view

--- react-frontend/src/SuspenseComponents/ProductCard/ProductCard.tsx
+++ react-frontend/src/SuspenseComponents/ProductCard/ProductCard.tsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import Skeleton from '@mui/material/Skeleton'
 import { Card } from '@mui/material'
+import { useAppSelector } from '../../app/hooks'
 
 const ProductCard = () => {
+   const isMobileView = useAppSelector(state => state.mobile.isMobile)
    return (
       <Card sx={{ width: 250 }}>
          <Skeleton sx={{ height: 175 }} animation='wave' variant='rectangular' />
-         <Skeleton sx={{ marginTop: 1, marginLeft: 1 }} animation='wave' height={20} width='80%' />
-         <Skeleton sx={{ marginLeft: 1 }} animation='wave' height={20} width='60%' />
-         <Skeleton sx={{ marginLeft: 1 }} animation='wave' height={20} width='40%' />
-         <Skeleton sx={{ marginLeft: 'auto', marginTop: 3, marginRight: 1 }} animation='wave' height={20} width='50%' />
+         <Skeleton sx={{ marginTop: 3, marginLeft: 2.5 }} animation='wave' height={20} width='80%' />
+         <Skeleton sx={{ marginLeft: 2.5 }} animation='wave' height={20} width='60%' />
+         <Skeleton sx={{ marginLeft: 2.5 }} animation='wave' height={20} width='40%' />
+         <Skeleton
+            sx={{ marginLeft: 2.5, marginTop: 1.5, marginRight: 1 }}
+            animation='wave'
+            height={32}
+            width='52%'
+         />
+         {isMobileView && (
+            <Skeleton
+               sx={{ marginLeft: 2.5, marginTop: 2 }}
+               animation='wave'
+               variant='rectangular'
+               height={42}
+               width='55%'
+            />
+         )}
       </Card>
    )
 }
