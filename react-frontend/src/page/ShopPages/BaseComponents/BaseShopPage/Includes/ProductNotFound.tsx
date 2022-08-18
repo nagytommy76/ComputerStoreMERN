abed9cff5f38a464d7313ecc63fe3deb5196fea0
nagytommy76 <nagytommy76@gmail.com>
2022-08-18 16:38:12 +0200

    Created a productNotFound component

---
+++ react-frontend/src/page/ShopPages/BaseComponents/BaseShopPage/Includes/ProductNotFound.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { styled } from '@mui/material'
+
+import SadImage from '../Asset/sad.png'
+import Tpyography from '@mui/material/Typography'
+import { mobileWindowSize } from '../../../../../Theme/GlobalStyles'
+
+const ProductNotFound = () => {
+   return (
+      <StyledProductNotFound>
+         <StyledImage src={SadImage} alt='sad image :(' />
+         <StyledTypography variant='h2' color='secondary'>
+            Sajnos nincsen megjeleníthető találat
+         </StyledTypography>
+      </StyledProductNotFound>
+   )
+}
+
+export default ProductNotFound
+
+const StyledProductNotFound = styled('div')({
+   position: 'absolute',
+   display: 'flex',
+   flexDirection: 'column',
+   alignItems: 'center',
+})
+
+const StyledTypography = styled(Tpyography)({
+   [`@media(max-width: ${mobileWindowSize})`]: {
+      textAlign: 'center',
+      fontSize: '1.5rem',
+   },
+})
+
+const StyledImage = styled('img')({
+   width: '25%',
+})
