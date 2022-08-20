47f88af626c76dc6d326f79b87adac36c46a1ffa
nagytommy76 <nagytommy76@gmail.com>
2022-08-20 10:50:54 +0200

    Set the fetching status to INIT when the pathname is changed

--- react-frontend/src/page/Navbar/DropMenu/ShopDropdown/DropMenu.tsx
+++ react-frontend/src/page/Navbar/DropMenu/ShopDropdown/DropMenu.tsx
@@ -3,6 +3,7 @@ import { DropStyle, DropLinkItem, CloseDropdownMenu } from '../DropMenuStyle'
 import { useAppSelector } from '../../../../app/hooks'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { NavbarContext } from '../../NavbarContext'
+import { useLocation } from 'react-router-dom'
 
 import { useAppDispatch } from '../../../../app/hooks'
 import { setProducts, setIsFetching } from '../../../../app/slices/ProductsSlice'
@@ -13,44 +14,50 @@ type Prop = {
 }
 
 const DropMenu: React.FC<Prop> = ({ reference }) => {
+   const location = useLocation()
    const dispatch = useAppDispatch()
    const isMobileSize = useAppSelector(state => state.mobile.isMobile)
    const { setIsShopDropOpen, setIsNavbarOpen } = useContext(NavbarContext)
-   const clickEvent = (event: React.MouseEvent) => {
+   const locationPathArray = ['/vga', '/cpu', '/memory', '/hdd', '/ssd']
+
+   const closeDropMenu = (event: React.MouseEvent) => {
       event.stopPropagation()
       setIsShopDropOpen(false)
    }
-   const closeNavbar = (event: React.MouseEvent) => {
+
+   const closeNavbar = (event: React.MouseEvent<HTMLAnchorElement>, linkTo: string) => {
       if (isMobileSize) {
          event.stopPropagation()
          setIsShopDropOpen(false)
          setIsNavbarOpen(false)
       }
       // A products oldal suspense miatt kell
-      dispatch(setIsFetching('INIT'))
-      dispatch(setProducts([]))
-      dispatch(setProductName(''))
+      if (locationPathArray.includes(location.pathname) && location.pathname !== linkTo) {
+         dispatch(setIsFetching('INIT'))
+         dispatch(setProducts([]))
+         dispatch(setProductName(''))
+      }
    }
    return (
       <DropStyle ref={reference}>
          {isMobileSize && (
-            <CloseDropdownMenu onClick={clickEvent}>
+            <CloseDropdownMenu onClick={closeDropMenu}>
                <FontAwesomeIcon icon={['fas', 'times']} size='2x' />
             </CloseDropdownMenu>
          )}
-         <DropLinkItem to='/vga' onClick={closeNavbar}>
+         <DropLinkItem to='/vga' onClick={e => closeNavbar(e, '/vga')}>
             Videókártya
          </DropLinkItem>
-         <DropLinkItem to='/cpu' onClick={closeNavbar}>
+         <DropLinkItem to='/cpu' onClick={e => closeNavbar(e, '/cpu')}>
             Processzor
          </DropLinkItem>
-         <DropLinkItem to='/memory' onClick={closeNavbar}>
+         <DropLinkItem to='/memory' onClick={e => closeNavbar(e, '/memory')}>
             Memória
          </DropLinkItem>
-         <DropLinkItem to='/hdd' onClick={closeNavbar}>
+         <DropLinkItem to='/hdd' onClick={e => closeNavbar(e, '/hdd')}>
             Merevlemez
          </DropLinkItem>
-         <DropLinkItem to='/ssd' onClick={closeNavbar}>
+         <DropLinkItem to='/ssd' onClick={e => closeNavbar(e, '/ssd')}>
             SSD
          </DropLinkItem>
       </DropStyle>
