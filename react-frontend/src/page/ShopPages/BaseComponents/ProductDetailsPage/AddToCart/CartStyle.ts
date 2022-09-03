f3cc4c87014495f13e89141bdd32d3e50f155603
nagytommy76 <nagytommy76@gmail.com>
2022-09-03 16:59:28 +0200

    Removed some unnecessary styles

--- react-frontend/src/page/ShopPages/BaseComponents/ProductDetailsPage/AddToCart/CartStyle.ts
+++ react-frontend/src/page/ShopPages/BaseComponents/ProductDetailsPage/AddToCart/CartStyle.ts
@@ -15,50 +15,15 @@ export const InputAndLabelContainer = styled.div`
    text-align: center;
 `
 
-export const CartQuantityStyle = styled.input`
-   width: 30px;
-   padding: 0.7rem;
-   font-size: 1.1rem;
-   text-align: center;
-   border: 1px solid hsl(115, 0%, 62%);
-   border-radius: 2px;
-   &::-webkit-outer-spin-button,
-   &::-webkit-inner-spin-button {
-      -webkit-appearance: none;
-      margin: 0;
-   }
-
-   &:focus {
-      font-weight: 600;
-      outline: none;
-   }
-`
-
 export const StyledNumberInput = styled(TextField)({
-   ['::-webkit-outer-spin-button']: {
+   width: '75px',
+   ['::webkit-outer-spin-button']: {
       backgroundColor: 'red',
       webkitAppearance: 'none',
       margin: 0,
    },
-   ['::-webkit-inner-spin-button']: {
+   ['::webkit-inner-spin-button']: {
       webkitAppearance: 'none',
       margin: 0,
    },
 })
-
-export const StyledLabel = styled.label`
-   color: hsl(115, 0%, 62%);
-`
-
-// Add to cart button
-
-export const AddToCartButton = styled.button`
-   width: 85px;
-   height: 65px;
-   margin-left: 0.8rem;
-   border-radius: 2px;
-   border: none;
-   background-color: #29c400;
-   cursor: pointer;
-   color: white;
-`
