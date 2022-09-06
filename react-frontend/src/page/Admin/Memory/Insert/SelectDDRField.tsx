44f0c1b764fd9d2381ab93128267fa00318da652
nagytommy76 <nagytommy76@gmail.com>
2022-09-06 19:25:16 +0200

    Using the base MUI select component

--- react-frontend/src/page/Admin/Memory/Insert/SelectDDRField.tsx
+++ react-frontend/src/page/Admin/Memory/Insert/SelectDDRField.tsx
@@ -1,24 +1,22 @@
 import React from 'react'
+import { MEMORY_TYPES } from '../MemoryProperties'
 
-import FormControl from '@mui/material/FormControl'
-import InputLabel from '@mui/material/InputLabel'
-import Select, { SelectChangeEvent } from '@mui/material/Select'
+import { SelectChangeEvent } from '@mui/material/Select'
+
+const BaseMUISelect = React.lazy(() => import('../../Components/InputFields/Select/MUISelectFeild'))
 
 const SelectDDRField: React.FC<{ onChangeEvent: (event: SelectChangeEvent) => void; value: string }> = ({
    onChangeEvent,
    value,
 }) => {
    return (
-      <FormControl margin='normal'>
-         <InputLabel id='memoryType'>Memória Típusa</InputLabel>
-         <Select value={value} id='memoryType' onChange={onChangeEvent} native variant='filled'>
-            <option value='DDR'>DDR</option>
-            <option value='DDR2'>DDR2</option>
-            <option value='DDR3'>DDR3</option>
-            <option value='DDR4'>DDR4</option>
-            <option value='DDR5'>DDR5</option>
-         </Select>
-      </FormControl>
+      <BaseMUISelect
+         id='memoryType'
+         labelText='Memória Típusa'
+         onChangeEvent={onChangeEvent}
+         value={value}
+         selectableItems={MEMORY_TYPES}
+      />
    )
 }
 
