638d5e885ecf448dcc5a2299809cf530f5855c05
nagytommy76 <nagytommy76@gmail.com>
2022-09-07 19:29:47 +0200

    Created a few constant variables

--- react-frontend/src/page/Admin/Vga/VgaProperties.ts
+++ react-frontend/src/page/Admin/Vga/VgaProperties.ts
@@ -25,3 +25,38 @@ export const vgaProperties: VgaType = {
       streamProcessors: 0,
    },
 }
+
+export const POWER_PINS = [
+   '1 * 6pin',
+   '1 * 6pin + 1 * 8pin',
+   '1 * 6pin + 2 * 8pin',
+   '1 * 8pin',
+   '2 * 8pin',
+   '3 * 8pin',
+]
+export const VRAM_TYPES = ['GDDR6', 'GDDR6X', 'GDDR5', 'DDR4', 'HBM2']
+export const PCIE_TYPE = ['PCI-E 16x 5.0', 'PCI-E 16x 4.0', 'PCI-E 16x 3.0', 'PCI-E 16x 2.0']
+export const GPU_MANUFACTURERS = ['AMD', 'NVIDIA']
+export const VGA_MANUFACTURERS = [
+   'AMD',
+   'ASROCK',
+   'ASUS',
+   'BIOSTAR',
+   'COLORFUL',
+   'EVGA',
+   'GAINWARD',
+   'GIGABYTE',
+   'HP',
+   'INNO3D',
+   'KFA2',
+   'LEADTEK',
+   'LENOVO',
+   'MANLI',
+   'MSI',
+   'PALIT',
+   'PNY',
+   'POWERCOLOR',
+   'SAPPHIRE',
+   'XFX',
+   'ZOTAC',
+]
