edbb4749ff1113899835d85760b9885cbd268a2f
nagytommy76 <nagytommy76@gmail.com>
2022-09-13 20:05:34 +0200

    Removed unnecessary code

--- node-api/middlewares/AuthenticateAccessOrRefreshTokens.ts
+++ node-api/middlewares/AuthenticateAccessOrRefreshTokens.ts
@@ -5,33 +5,16 @@ import { JWTUserType } from '../controllers/Types'
 
 type GetUserAuthInfoRequest = Request & {
    user?: JWTUserType | JwtPayload | string
-   accessToken?: string
 }
 
 const getTokenFromAuthorizationHeader = (authHeader?: string) => {
    return authHeader && authHeader?.split(' ')[1]
 }
 
-// export const authenticateAccessToken = (req: GetUserAuthInfoRequest, res: Response, next: NextFunction) => {
-//    const token = getTokenFromAuthorizationHeader(req.headers.authorization)
-//    if (!token) return res.sendStatus(401)
-//    jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
-//       if (err) return res.status(403).json({ errorMessage: 'accessToken token expired' })
-//       if (!user) return res.status(404).json({ errorMessage: 'user not found' })
-
-//       req.user = user
-//       next()
-//    })
-// }
 export const authenticateAccessToken = (req: GetUserAuthInfoRequest, res: Response, next: NextFunction) => {
    // az access token-re van itt szükségem
-
-   console.log(req.headers.cookie)
-   const token = req.headers.cookie?.split('=')[1]
-   console.log()
-   console.log(token)
+   const token = getTokenFromAuthorizationHeader(req.headers.authorization)
    if (!token) return res.sendStatus(401)
-   // console.log(token)
    jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
       if (err) return res.status(403).json({ errorMessage: 'accessToken token expired' })
       if (!user) return res.status(404).json({ errorMessage: 'user not found' })
