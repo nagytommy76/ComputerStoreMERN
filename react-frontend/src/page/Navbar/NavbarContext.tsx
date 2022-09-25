4950ae90b89191c3edaf9d1115b3c1ce3a28bd0d
nagytommy76 <nagytommy76@gmail.com>
2022-09-25 14:18:53 +0200

    Using a provider component & using the reducer

--- react-frontend/src/page/Navbar/NavbarContext.tsx
+++ react-frontend/src/page/Navbar/NavbarContext.tsx
@@ -1,27 +1,20 @@
-import React from 'react'
+import React, { createContext, useReducer } from 'react'
+import navbarReducer, { initialNavbarState, InitialState, Action } from './Reducer/NavbarReducer'
 
-export const NavbarContext = React.createContext<NavbarContextTypes>({
-   isShopDropOpen: false,
-   isUserDropOpen: false,
-   isNavbarOpen: false,
-   setIsShopDropOpen: () => false,
-   setIsUserDropOpen: () => false,
-   setIsNavbarOpen: () => false,
-   isCartOpen: false,
-   setIsCartOpen: () => false,
-   anchorEl: null,
-   setAnchorEl: () => null,
+export const NavbarContext = createContext<NavbarContextTypes>({
+   state: initialNavbarState,
+   dispatch: () => null,
 })
 
 export type NavbarContextTypes = {
-   anchorEl: HTMLElement | null
-   isShopDropOpen: boolean
-   isUserDropOpen: boolean
-   isNavbarOpen: boolean
-   isCartOpen: boolean
-   setAnchorEl: React.Dispatch<React.SetStateAction<HTMLElement | null>>
-   setIsShopDropOpen: React.Dispatch<React.SetStateAction<boolean>>
-   setIsUserDropOpen: React.Dispatch<React.SetStateAction<boolean>>
-   setIsNavbarOpen: React.Dispatch<React.SetStateAction<boolean>>
-   setIsCartOpen: React.Dispatch<React.SetStateAction<boolean>>
+   state: InitialState
+   dispatch: React.Dispatch<Action>
 }
+
+const NavbarContextProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+   const [state, dispatch] = useReducer(navbarReducer, initialNavbarState)
+
+   return <NavbarContext.Provider value={{ state, dispatch }}>{children}</NavbarContext.Provider>
+}
+
+export default NavbarContextProvider
