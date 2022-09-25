2621d1fa40f025d65d069116858c85a01b06c882
nagytommy76 <nagytommy76@gmail.com>
2022-09-25 14:18:10 +0200

    Created a navbar reducer

---
+++ react-frontend/src/page/Navbar/Reducer/NavbarReducer.tsx
@@ -0,0 +1,82 @@
+export enum NavbarActionTypes {
+   SET_IS_NAVBAR_OPEN = 'SET_IS_NAVBAR_OPEN',
+   SET_IS_SHOP_DROP_OPEN = 'SET_IS_SHOP_DROP_OPEN',
+   SET_IS_USER_DROP_OPEN = 'SET_IS_USER_DROP_OPEN',
+   SET_IS_CART_OPEN = 'SET_IS_CART_OPEN',
+   SET_SHOP_ANCHOR_EL = 'SET_SHOP_ANCHOR_EL',
+   SET_USER_ANCHOR_EL = 'SET_USER_ANCHOR_EL',
+}
+
+type ProductPayload = {
+   [NavbarActionTypes.SET_IS_NAVBAR_OPEN]: boolean
+   [NavbarActionTypes.SET_IS_SHOP_DROP_OPEN]: boolean
+   [NavbarActionTypes.SET_IS_USER_DROP_OPEN]: boolean
+   [NavbarActionTypes.SET_IS_CART_OPEN]: boolean
+   [NavbarActionTypes.SET_SHOP_ANCHOR_EL]: null | HTMLElement
+   [NavbarActionTypes.SET_USER_ANCHOR_EL]: null | HTMLElement
+}
+
+export interface Action {
+   type: NavbarActionTypes
+   //    payload: ProductPayload
+   payload: null | HTMLElement | boolean
+}
+
+export interface InitialState {
+   isNavbarOpen: boolean
+   isShopDropOpen: boolean
+   isUserDropOpen: boolean
+   isCartOpen: boolean
+   shopAnchorEl: null | HTMLElement
+   userAnchorEl: null | HTMLElement
+}
+
+export const initialNavbarState: InitialState = {
+   isNavbarOpen: false,
+   isShopDropOpen: false,
+   isUserDropOpen: false,
+   isCartOpen: false,
+   shopAnchorEl: null,
+   userAnchorEl: null,
+}
+
+// Később folytatni ANY....
+const navbarReducer = (state: InitialState, action: any) => {
+   const { payload, type } = action
+   switch (type) {
+      case NavbarActionTypes.SET_IS_NAVBAR_OPEN:
+         return {
+            ...state,
+            isNavbarOpen: payload,
+         }
+      case NavbarActionTypes.SET_IS_SHOP_DROP_OPEN:
+         return {
+            ...state,
+            isShopDropOpen: payload,
+         }
+      case NavbarActionTypes.SET_IS_USER_DROP_OPEN:
+         return {
+            ...state,
+            isUserDropOpen: payload,
+         }
+      case NavbarActionTypes.SET_IS_CART_OPEN:
+         return {
+            ...state,
+            isCartOpen: payload,
+         }
+      case NavbarActionTypes.SET_SHOP_ANCHOR_EL:
+         return {
+            ...state,
+            shopAnchorEl: payload,
+         }
+      case NavbarActionTypes.SET_USER_ANCHOR_EL:
+         return {
+            ...state,
+            userAnchorEl: payload,
+         }
+      default:
+         return state
+   }
+}
+
+export default navbarReducer
