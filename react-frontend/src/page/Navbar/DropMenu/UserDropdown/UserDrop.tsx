7ec10693f0f6faee39584216e8d4d3c10f80e045
nagytommy76 <nagytommy76@gmail.com>
2022-09-26 20:14:20 +0200

    Created the mui based user drop menu

--- react-frontend/src/page/Navbar/DropMenu/UserDropdown/UserDrop.tsx
+++ react-frontend/src/page/Navbar/DropMenu/UserDropdown/UserDrop.tsx
@@ -1,49 +1,75 @@
 import React, { useContext } from 'react'
 import useLogout from '../../../../Hooks/useLogout'
-import { DropStyle, DropLinkItem, CloseDropdownMenu } from '../DropMenuStyle'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { DropLinkItem } from '../DropMenuStyle'
 import { NavbarContext } from '../../NavbarContext'
+import { NavbarActionTypes } from '../../Reducer/NavbarReducer'
 
 import { useAppSelector } from '../../../../app/hooks'
+import { StyledMenuItem } from '../DropMenuStyle'
+import Menu from '@mui/material/Menu'
 
 const DropMenu: React.FC = () => {
    const logout = useLogout()
    const isAdmin = useAppSelector(state => state.auth.isAdmin)
    const isMobileSize = useAppSelector(state => state.mobile.isMobile)
-   const { setIsUserDropOpen, setIsNavbarOpen } = useContext(NavbarContext)
+   const {
+      state: { userAnchorEl },
+      dispatch,
+   } = useContext(NavbarContext)
+   const open = Boolean(userAnchorEl)
 
    const clickEvent = (event: React.MouseEvent) => {
       event.stopPropagation()
-      setIsUserDropOpen(false)
-      isMobileSize && setIsNavbarOpen(false)
+      dispatch({ type: NavbarActionTypes.SET_IS_USER_DROP_OPEN, payload: false })
+      dispatch({ type: NavbarActionTypes.SET_USER_ANCHOR_EL, payload: null })
+      isMobileSize && dispatch({ type: NavbarActionTypes.SET_IS_NAVBAR_OPEN, payload: false })
    }
 
    const closeDropMenu = (event: React.MouseEvent) => {
       event.stopPropagation()
-      setIsUserDropOpen(false)
+      dispatch({ type: NavbarActionTypes.SET_IS_USER_DROP_OPEN, payload: false })
+      dispatch({ type: NavbarActionTypes.SET_USER_ANCHOR_EL, payload: null })
    }
 
    const logoutHandler = () => logout()
 
    return (
-      <DropStyle>
-         {isMobileSize && (
-            <CloseDropdownMenu onClick={closeDropMenu}>
-               <FontAwesomeIcon icon={['fas', 'times']} size='2x' />
-            </CloseDropdownMenu>
-         )}
-         <DropLinkItem onClick={clickEvent} to='/orders'>
-            Korábbi rendelések
-         </DropLinkItem>
-         <DropLinkItem onClick={logoutHandler} to='#'>
-            Kijelentkezés
-         </DropLinkItem>
-         {isAdmin && (
-            <DropLinkItem onClick={clickEvent} to={`/${process.env.REACT_APP_PROTECTED_ADMIN_ROUTE}`}>
-               Admin Felület
+      <Menu
+         anchorEl={userAnchorEl}
+         id='shop-menu'
+         open={open}
+         onClose={closeDropMenu}
+         onClick={closeDropMenu}
+         MenuListProps={{ onMouseLeave: closeDropMenu }}
+         PaperProps={{
+            elevation: 0,
+            sx: {
+               overflow: 'visible',
+               filter: 'drop-shadow(0px 2px 8px #27272751)',
+               mt: 1.5,
+            },
+         }}
+         transformOrigin={{ horizontal: 'center', vertical: 'top' }}
+         anchorOrigin={{ horizontal: 'center', vertical: 'bottom' }}
+      >
+         <StyledMenuItem sx={{ padding: 0 }}>
+            <DropLinkItem to='/orders' onClick={clickEvent}>
+               Korábbi rendelések
             </DropLinkItem>
-         )}
-      </DropStyle>
+         </StyledMenuItem>
+         <StyledMenuItem sx={{ padding: 0 }}>
+            <DropLinkItem to='#' onClick={logoutHandler}>
+               Kijelentkezés
+            </DropLinkItem>
+         </StyledMenuItem>
+         <StyledMenuItem sx={{ padding: 0 }}>
+            {isAdmin && (
+               <DropLinkItem onClick={clickEvent} to={`/${process.env.REACT_APP_PROTECTED_ADMIN_ROUTE}`}>
+                  Admin Felület
+               </DropLinkItem>
+            )}
+         </StyledMenuItem>
+      </Menu>
    )
 }
 
