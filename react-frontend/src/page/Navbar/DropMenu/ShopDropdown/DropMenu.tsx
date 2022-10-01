179f7f80af035107808bc4288cad418ca69a0078
nagytommy76 <nagytommy76@gmail.com>
2022-10-01 14:11:17 +0200

    Using the menu link items components

--- react-frontend/src/page/Navbar/DropMenu/ShopDropdown/DropMenu.tsx
+++ react-frontend/src/page/Navbar/DropMenu/ShopDropdown/DropMenu.tsx
@@ -1,37 +1,16 @@
 import React, { useContext } from 'react'
-import { DropLinkItem } from '../DropMenuStyle'
-import { useAppSelector } from '../../../../app/hooks'
 import { NavbarContext } from '../../NavbarContext'
-import { useLocation } from 'react-router-dom'
 import { NavbarActionTypes } from '../../Reducer/NavbarReducer'
 
-import { useAppDispatch } from '../../../../app/hooks'
-import { setProducts, setIsFetching } from '../../../../app/slices/ProductsSlice'
-import { setProductName } from '../../../../app/slices/Filter/BaseFilterDataSlice'
-
-import { StyledMenuItem } from '../DropMenuStyle'
+import ShopMenuLinkItems from './ShopMenuLinkItems'
 import Menu from '@mui/material/Menu'
 
 const DropMenu: React.FC = () => {
-   const location = useLocation()
-   const ReducDispatch = useAppDispatch()
-   const isMobileSize = useAppSelector(state => state.mobile.isMobile)
    const {
       state: { shopAnchorEl },
       dispatch,
    } = useContext(NavbarContext)
    const open = Boolean(shopAnchorEl)
-   const locationPathArray = [
-      '/vga',
-      '/cpu',
-      '/memory',
-      '/hdd',
-      '/ssd',
-      '/',
-      '/orders',
-      '/login',
-      '/register',
-   ]
 
    const closeDropMenu = (event: React.MouseEvent) => {
       event.stopPropagation()
@@ -39,20 +18,6 @@ const DropMenu: React.FC = () => {
       dispatch({ type: NavbarActionTypes.SET_SHOP_ANCHOR_EL, payload: null })
    }
 
-   const closeNavbar = (event: React.MouseEvent<HTMLAnchorElement>, linkTo: string) => {
-      if (isMobileSize) {
-         event.stopPropagation()
-         dispatch({ type: NavbarActionTypes.SET_IS_SHOP_DROP_OPEN, payload: false })
-         dispatch({ type: NavbarActionTypes.SET_IS_NAVBAR_OPEN, payload: false })
-      }
-      // A products oldal suspense miatt kell
-      if (locationPathArray.includes(location.pathname) && location.pathname !== linkTo) {
-         ReducDispatch(setIsFetching('INIT'))
-         ReducDispatch(setProducts([]))
-         ReducDispatch(setProductName(''))
-      }
-      dispatch({ type: NavbarActionTypes.SET_SHOP_ANCHOR_EL, payload: null })
-   }
    return (
       <Menu
          anchorEl={shopAnchorEl}
@@ -72,31 +37,7 @@ const DropMenu: React.FC = () => {
          transformOrigin={{ horizontal: 'center', vertical: 'top' }}
          anchorOrigin={{ horizontal: 'center', vertical: 'bottom' }}
       >
-         <StyledMenuItem sx={{ padding: 0 }}>
-            <DropLinkItem onClick={e => closeNavbar(e, '/vga')} to='/vga'>
-               Videókártya
-            </DropLinkItem>
-         </StyledMenuItem>
-         <StyledMenuItem sx={{ padding: 0 }}>
-            <DropLinkItem onClick={e => closeNavbar(e, '/cpu')} to='/cpu'>
-               Processzor
-            </DropLinkItem>
-         </StyledMenuItem>
-         <StyledMenuItem sx={{ padding: 0 }}>
-            <DropLinkItem onClick={e => closeNavbar(e, '/memory')} to='/memory'>
-               Memória
-            </DropLinkItem>
-         </StyledMenuItem>
-         <StyledMenuItem sx={{ padding: 0 }}>
-            <DropLinkItem onClick={e => closeNavbar(e, '/hdd')} to='/hdd'>
-               Merevlemez
-            </DropLinkItem>
-         </StyledMenuItem>
-         <StyledMenuItem sx={{ padding: 0 }}>
-            <DropLinkItem onClick={e => closeNavbar(e, '/ssd')} to='/ssd'>
-               SSD
-            </DropLinkItem>
-         </StyledMenuItem>
+         <ShopMenuLinkItems />
       </Menu>
    )
 }
--- react-frontend/src/page/Navbar/DropMenu/UserDropdown/UserDrop.tsx
+++ react-frontend/src/page/Navbar/DropMenu/UserDropdown/UserDrop.tsx
@@ -1,29 +1,16 @@
 import React, { useContext } from 'react'
-import useLogout from '../../../../Hooks/useLogout'
-import { DropLinkItem } from '../DropMenuStyle'
-import { NavbarContext } from '../../NavbarContext'
 import { NavbarActionTypes } from '../../Reducer/NavbarReducer'
+import { NavbarContext } from '../../NavbarContext'
 
-import { useAppSelector } from '../../../../app/hooks'
-import { StyledMenuItem } from '../DropMenuStyle'
+import UserMenuLinkItems from './UserMenuLinkItems'
 import Menu from '@mui/material/Menu'
 
 const DropMenu: React.FC = () => {
-   const logout = useLogout()
-   const isAdmin = useAppSelector(state => state.auth.isAdmin)
-   const isMobileSize = useAppSelector(state => state.mobile.isMobile)
    const {
       state: { userAnchorEl },
       dispatch,
    } = useContext(NavbarContext)
    const open = Boolean(userAnchorEl)
-
-   const clickEvent = (event: React.MouseEvent) => {
-      event.stopPropagation()
-      dispatch({ type: NavbarActionTypes.SET_IS_USER_DROP_OPEN, payload: false })
-      dispatch({ type: NavbarActionTypes.SET_USER_ANCHOR_EL, payload: null })
-      isMobileSize && dispatch({ type: NavbarActionTypes.SET_IS_NAVBAR_OPEN, payload: false })
-   }
 
    const closeDropMenu = (event: React.MouseEvent) => {
       event.stopPropagation()
@@ -31,12 +18,10 @@ const DropMenu: React.FC = () => {
       dispatch({ type: NavbarActionTypes.SET_USER_ANCHOR_EL, payload: null })
    }
 
-   const logoutHandler = () => logout()
-
    return (
       <Menu
          anchorEl={userAnchorEl}
-         id='shop-menu'
+         id='user-menu'
          open={open}
          onClose={closeDropMenu}
          onClick={closeDropMenu}
@@ -52,23 +37,7 @@ const DropMenu: React.FC = () => {
          transformOrigin={{ horizontal: 'center', vertical: 'top' }}
          anchorOrigin={{ horizontal: 'center', vertical: 'bottom' }}
       >
-         <StyledMenuItem sx={{ padding: 0 }}>
-            <DropLinkItem to='/orders' onClick={clickEvent}>
-               Korábbi rendelések
-            </DropLinkItem>
-         </StyledMenuItem>
-         <StyledMenuItem sx={{ padding: 0 }}>
-            <DropLinkItem to='#' onClick={logoutHandler}>
-               Kijelentkezés
-            </DropLinkItem>
-         </StyledMenuItem>
-         <StyledMenuItem sx={{ padding: 0 }}>
-            {isAdmin && (
-               <DropLinkItem onClick={clickEvent} to={`/${process.env.REACT_APP_PROTECTED_ADMIN_ROUTE}`}>
-                  Admin Felület
-               </DropLinkItem>
-            )}
-         </StyledMenuItem>
+         <UserMenuLinkItems />
       </Menu>
    )
 }
