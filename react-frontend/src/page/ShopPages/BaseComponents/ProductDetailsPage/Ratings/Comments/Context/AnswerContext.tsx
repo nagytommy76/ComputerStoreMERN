37e1e42abaf6ed2d747fce1f3f554e61602d5b78
nagytommy76 <nagytommy76@gmail.com>
2022-10-12 20:08:59 +0200

    Created a group function to organize the comment answers

--- react-frontend/src/page/ShopPages/BaseComponents/ProductDetailsPage/Ratings/Comments/Context/AnswerContext.tsx
+++ react-frontend/src/page/ShopPages/BaseComponents/ProductDetailsPage/Ratings/Comments/Context/AnswerContext.tsx
@@ -1,11 +1,52 @@
-import { createContext } from 'react'
+import React, { createContext, useState, useEffect, useMemo, Dispatch, SetStateAction } from 'react'
 import { CommentAnswerType } from '../Helpers'
 
 export const AnswerContext = createContext<{
    commentAnswers: CommentAnswerType[]
-   setCommentAnswer: React.Dispatch<React.SetStateAction<CommentAnswerType[]>>
+   setCommentAnswer: Dispatch<SetStateAction<CommentAnswerType[]>>
    // CommentId-t hozzáadni
 }>({
    commentAnswers: [],
-   setCommentAnswer: () => {}
+   setCommentAnswer: () => {},
 })
+
+export const CommentAnswerProvider: React.FC<{
+   children: React.ReactNode
+   commentAnswersProp: CommentAnswerType[]
+}> = ({ children, commentAnswersProp }) => {
+   const [commentAnswers, setCommentAnswers] = useState<CommentAnswerType[]>([])
+   useEffect(() => {
+      setCommentAnswers(commentAnswersProp)
+   }, [commentAnswersProp])
+
+   const commentsByParentId = useMemo(() => {
+      const group: any = {}
+      commentAnswersProp.forEach(answer => {
+         if (answer.parentCommentId !== null) {
+            group[answer.parentCommentId] ||= []
+            group[answer.parentCommentId].push(answer)
+         }
+      })
+      return group
+   }, [commentAnswersProp])
+   // useEffect(() => {
+   //    console.log(commentsByParentId)
+   // }, [commentsByParentId])
+
+   return (
+      <AnswerContext.Provider
+         value={{
+            commentAnswers,
+            setCommentAnswer: setCommentAnswers,
+         }}
+      >
+         {children}
+      </AnswerContext.Provider>
+   )
+}
+/**
+ * Tennivalók:
+ *  - A group-nak valami type-ot csinálni
+ *  - a commentAnswers majd a commentsByParentId function visszatérési értéke lesz (group)
+ *  - a useState nem fog kelleni: [commentAnswers, setCommentAnswers]
+ */
