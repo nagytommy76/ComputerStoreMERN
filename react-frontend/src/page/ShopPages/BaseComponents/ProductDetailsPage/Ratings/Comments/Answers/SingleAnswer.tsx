f7c8bb976e3052ed6da7f158713bb305af526335
nagytommy76 <nagytommy76@gmail.com>
2022-10-16 16:51:29 +0200

    Separated the answer element & added the child replies to it.

---
+++ react-frontend/src/page/ShopPages/BaseComponents/ProductDetailsPage/Ratings/Comments/Answers/SingleAnswer.tsx
@@ -0,0 +1,45 @@
+import React, { useContext, useState, useEffect } from 'react'
+import { CommentAnswerType } from '../Helpers'
+import { formatDate } from '../../../../../../Helpers/FormatDate'
+
+import { SingleAnswerStyle, LeftAnswerStyle, RightAnswerStyle } from './AnswerStyle'
+import Typography from '@mui/material/Typography'
+
+import AnswerList from './AnswerList'
+import { AnswerContext } from '../Context/AnswerContext'
+const DeleteAnswer = React.lazy(() => import('./AnswerDelete'))
+const CreateAnswer = React.lazy(() => import('./CreateAnswer'))
+
+const SingleAnswer: React.FC<{ answer: CommentAnswerType; commentId: string }> = ({ answer, commentId }) => {
+   const { getReplies } = useContext(AnswerContext)
+   const [childReplies, setChildReplies] = useState<CommentAnswerType[]>([])
+   useEffect(() => {
+      const replies = getReplies(answer._id)
+      setChildReplies(replies)
+      console.log(replies)
+   }, [answer.parentCommentId, getReplies])
+
+   return (
+      <>
+         <SingleAnswerStyle>
+            <LeftAnswerStyle>
+               <Typography variant='h5'>{answer.userName}</Typography>
+               <Typography variant='subtitle1'>{formatDate(answer.answeredAt)}</Typography>
+            </LeftAnswerStyle>
+            <RightAnswerStyle>
+               <Typography variant='body1'>{answer.answer}</Typography>
+            </RightAnswerStyle>
+            <DeleteAnswer commentId={commentId} answerId={answer._id} answerUserName={answer.userName} />
+         </SingleAnswerStyle>
+         <CreateAnswer
+            userName={answer.userName}
+            commentId={commentId}
+            parentCommentId={answer._id}
+            commentDepth={answer.commentDepth + 1}
+         />
+         {childReplies && <AnswerList commentId={commentId} answers={childReplies} />}
+      </>
+   )
+}
+
+export default SingleAnswer
