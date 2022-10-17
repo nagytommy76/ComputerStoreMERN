c5c8aee9e1eaf561f87ac6f33c27132a8af8a8ff
nagytommy76 <nagytommy76@gmail.com>
2022-10-17 20:02:49 +0200

    Added a collapse transition group to the list

--- react-frontend/src/page/ShopPages/BaseComponents/ProductDetailsPage/Ratings/Comments/Answers/AnswerList.tsx
+++ react-frontend/src/page/ShopPages/BaseComponents/ProductDetailsPage/Ratings/Comments/Answers/AnswerList.tsx
@@ -2,15 +2,22 @@ import React from 'react'
 import { CommentAnswerType } from '../Helpers'
 import SingleAnswer from './SingleAnswer'
 
+import Collapse from '@mui/material/Collapse'
+import { TransitionGroup } from 'react-transition-group'
+
 const AnswerList: React.FC<{ answers: CommentAnswerType[]; commentId?: string }> = ({
    answers,
    commentId = '',
 }) => {
    return (
       <>
-         {answers.map(answer => (
-            <SingleAnswer key={answer._id} answer={answer} commentId={commentId} />
-         ))}
+         <TransitionGroup component={null}>
+            {answers.map(answer => (
+               <Collapse key={answer._id} timeout={150}>
+                  <SingleAnswer key={answer._id} answer={answer} commentId={commentId} />
+               </Collapse>
+            ))}
+         </TransitionGroup>
       </>
    )
 }
