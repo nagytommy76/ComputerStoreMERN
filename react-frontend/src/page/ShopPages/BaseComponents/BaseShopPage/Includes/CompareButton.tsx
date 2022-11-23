74a70bd5a7ae284665cfe26e51d574e01e616c40
nagytommy76 <nagytommy76@gmail.com>
2022-11-23 20:08:16 +0100

    imported the productCompare array length

--- react-frontend/src/page/ShopPages/BaseComponents/BaseShopPage/Includes/CompareButton.tsx
+++ react-frontend/src/page/ShopPages/BaseComponents/BaseShopPage/Includes/CompareButton.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { styled } from '@mui/material'
 import Fab from '@mui/material/Fab'
 import CompareArrowsIcon from '@mui/icons-material/CompareArrows'
+import { useAppSelector } from '../../../../../app/hooks'
 
 const Compare = () => {
+   const compareLength = useAppSelector(state => state.productCompare.productIdsToComare.length) || 0
    return (
       <Fab color='info' variant='extended' sx={{ zIndex: 3, position: 'fixed', right: '50px', bottom: 150 }}>
-         <StyledCompareCount>5</StyledCompareCount>
+         <StyledCompareCount>{compareLength}</StyledCompareCount>
          <CompareArrowsIcon />
       </Fab>
    )
