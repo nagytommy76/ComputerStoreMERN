38a278af7e5d6470ca5b2188b30070b6eb41213c
nagytommy76 <nagytommy76@gmail.com>
2022-11-27 13:58:30 +0100

    Created a context provider with reducer to handle the snackbar message

--- react-frontend/src/page/ShopPages/BaseComponents/Context/MessageContext.tsx
+++ react-frontend/src/page/ShopPages/BaseComponents/Context/MessageContext.tsx
@@ -1,16 +1,58 @@
-import { createContext } from 'react'
+import { createContext, useReducer } from 'react'
+
+export enum MessageTypes {
+   SET_ISACTIVE = 'SET_ISACTIVE',
+   INIT = 'INIT',
+}
 
 interface IMessage {
+   isActive: boolean
    severity: 'error' | 'success' | 'info' | 'warning'
-}
-const initialValue: IMessage = {
-   severity: 'success',
+   message: string
 }
 
-export const MessageContext = createContext<IMessage>(initialValue)
+interface MessageAction {
+   type: MessageTypes
+   payload: IMessage
+}
+
+const messageReducer = (state: IMessage, action: MessageAction): IMessage => {
+   const { type, payload } = action
+   switch (type) {
+      case MessageTypes.SET_ISACTIVE:
+         return {
+            severity: payload.severity,
+            message: payload.message,
+            isActive: payload.isActive,
+         }
+      case MessageTypes.INIT:
+         return {
+            message: '',
+            severity: 'success',
+            isActive: false,
+         }
+      default:
+         return state
+   }
+}
+
+type MessageContextType = {
+   state: IMessage
+   dispatch: React.Dispatch<MessageAction>
+}
+// ez egy state legyen
+const initialValue: IMessage = {
+   isActive: false,
+   severity: 'success',
+   message: '',
+}
+
+export const MessageContext = createContext<MessageContextType>({ state: initialValue, dispatch: () => null })
 
 const MessageContextProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-   return <MessageContext.Provider value={initialValue}>{children}</MessageContext.Provider>
+   const [state, dispatch] = useReducer(messageReducer, initialValue)
+
+   return <MessageContext.Provider value={{ state, dispatch }}>{children}</MessageContext.Provider>
 }
 
 export default MessageContextProvider
