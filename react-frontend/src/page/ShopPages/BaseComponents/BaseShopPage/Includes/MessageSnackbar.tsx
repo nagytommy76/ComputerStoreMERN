192935e99935093fcb0ad5426f6897147dc0901c
nagytommy76 <nagytommy76@gmail.com>
2022-11-28 20:03:34 +0100

    Added a portal & an alert component

--- react-frontend/src/page/ShopPages/BaseComponents/BaseShopPage/Includes/MessageSnackbar.tsx
+++ react-frontend/src/page/ShopPages/BaseComponents/BaseShopPage/Includes/MessageSnackbar.tsx
@@ -1,13 +1,24 @@
 import React, { useContext } from 'react'
+import { createPortal } from 'react-dom'
 import { MessageContext, MessageTypes } from '../../Context/MessageContext'
 
+import Alert from '@mui/material/Alert'
 import Snackbar from '@mui/material/Snackbar'
 
 const MessageSnackbar = () => {
    const { state, dispatch } = useContext(MessageContext)
+   const handleClose = () => {
+      dispatch({ type: MessageTypes.INIT, payload: { isActive: false, message: '', severity: 'success' } })
+   }
 
-   const handleClose = () => {}
-   return <Snackbar open={state.isActive} onClose={handleClose} message={state.message} />
+   return createPortal(
+      <Snackbar open={state.isActive} onClose={handleClose}>
+         <Alert variant='filled' onClose={handleClose} severity={state.severity} sx={{ width: '100%' }}>
+            {state.message}
+         </Alert>
+      </Snackbar>,
+      document.getElementById('snackbar') as HTMLElement
+   )
 }
 
 export default MessageSnackbar
