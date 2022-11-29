f48b62385c549a6d18fe4da7e47c809d0aada87e
nagytommy76 <nagytommy76@gmail.com>
2022-11-29 19:57:20 +0100

    Added a popover to preview the compared items

--- react-frontend/src/page/ShopPages/BaseComponents/BaseShopPage/Includes/CompareButton.tsx
+++ react-frontend/src/page/ShopPages/BaseComponents/BaseShopPage/Includes/CompareButton.tsx
@@ -1,16 +1,54 @@
 import React from 'react'
 import { styled } from '@mui/material'
-import Fab from '@mui/material/Fab'
-import CompareArrowsIcon from '@mui/icons-material/CompareArrows'
 import { useAppSelector } from '../../../../../app/hooks'
+
+import Fab from '@mui/material/Fab'
+import Popover from '@mui/material/Popover'
+import CompareArrowsIcon from '@mui/icons-material/CompareArrows'
 
 const Compare = () => {
    const compareLength = useAppSelector(state => state.productCompare.productIdsToComare.length) || 0
+   const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null)
+   const open = Boolean(anchorEl)
+   const id = open ? 'simple-popover' : undefined
+
+   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+      setAnchorEl(event.currentTarget)
+   }
+
+   const handleClose = () => {
+      setAnchorEl(null)
+   }
+
    return (
-      <Fab color='info' variant='extended' sx={{ zIndex: 3, position: 'fixed', right: '50px', bottom: 150 }}>
-         <StyledCompareCount>{compareLength}</StyledCompareCount>
-         <CompareArrowsIcon />
-      </Fab>
+      <>
+         <Fab
+            onClick={handleClick}
+            color='info'
+            variant='extended'
+            sx={{ zIndex: 3, position: 'fixed', right: '50px', bottom: 150 }}
+         >
+            <StyledCompareCount>{compareLength}</StyledCompareCount>
+            <CompareArrowsIcon />
+         </Fab>
+         <Popover
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+               vertical: 'bottom',
+               horizontal: 'left',
+            }}
+            transformOrigin={{
+               vertical: 'bottom',
+               horizontal: 'right',
+            }}
+         >
+            {/* Ide kéne megjeleníteni a termék nevet egy képpel */}
+            <div>semmi</div>
+         </Popover>
+      </>
    )
 }
 
