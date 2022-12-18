2dd5b2a496df7dc583bfb41e5766154769c78653
nagytommy76 <nagytommy76@gmail.com>
2022-12-18 14:28:14 +0100

    Created a types file

---
+++ react-frontend/src/page/ShopPages/ComparePage/CompareTypes.ts
@@ -0,0 +1,26 @@
+export interface HeaderTypes {
+   manufacturer: string
+   type: string
+   pictureURL: string
+}
+
+export enum VgaDetailProperties {
+   DVI = 'DVI',
+   HDMI = 'HDMI',
+   displayPort = 'Display Portok szána',
+   gpuBaseClock = 'Alap GPU Órajel',
+   gpuPeakClock = 'Emelt GPU órajel',
+   gpuManufacturer = 'GPU gyártó',
+   length = 'Hosszúság',
+   manufacturerPageUrl = 'Gyártói honlap',
+   minPowerSupply = 'Minimum ajánlott tápegység',
+   pcieType = 'PCI-E típusa',
+   powerConsuption = 'Fogyasztás (TDP)',
+   powerPin = 'Tápcsatlakozók',
+   streamProcessors = 'Stream processzorok',
+   vramBandwidth = 'VRAM sávszélesség',
+   vramCapacity = 'VRAM mennyiség',
+   vramSpeed = 'VRAM sebesség',
+   vramType = 'VRAM típusa',
+   warranity = 'Garancia',
+}
