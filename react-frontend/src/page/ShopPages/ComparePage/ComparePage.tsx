e44166ce7105001da48885bca95da8c9e33e813d
nagytommy76 <nagytommy76@gmail.com>
2022-12-18 13:19:04 +0100

    Displayed the details data

--- react-frontend/src/page/ShopPages/ComparePage/ComparePage.tsx
+++ react-frontend/src/page/ShopPages/ComparePage/ComparePage.tsx
@@ -6,12 +6,34 @@ import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
-import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
 
+import TableHeader from './TableHeader/TableHeader'
+
 import { ComparePageStyle } from './Styles/CompareStyle'
 import { BaseProductType } from '../BaseTypes'
+
+enum VgaDetailProperties {
+   DVI = 'DVI',
+   HDMI = 'HDMI',
+   displayPort = 'Display Portok szána',
+   gpuBaseClock = 'Alap GPU Órajel',
+   gpuPeakClock = 'Emelt GPU órajel',
+   gpuManufacturer = 'GPU gyártó',
+   length = 'Hosszúság',
+   manufacturerPageUrl = 'Gyártói honlap',
+   minPowerSupply = 'Minimum ajánlott tápegység',
+   pcieType = 'PCI-E típusa',
+   powerConsuption = 'Fogyasztás (TDP)',
+   powerPin = 'Tápcsatlakozók',
+   streamProcessors = 'Stream processzorok',
+   vramBandwidth = 'VRAM sávszélesség',
+   vramCapacity = 'VRAM mennyiség',
+   vramSpeed = 'VRAM sebesség',
+   vramType = 'VRAM típusa',
+   warranity = 'Garancia',
+}
 
 const ComparePage = () => {
    const {
@@ -36,52 +58,19 @@ const ComparePage = () => {
    return (
       <ComparePageStyle>
          <TableContainer component={Paper}>
-            <Table sx={{ Width: 650 }} aria-label='simple table'>
-               <TableHead>
-                  <TableRow>
-                     <TableCell>Ide majd kitalálom mi legyen</TableCell>
-                     {compareProducts.map(details => (
-                        <TableCell key={details._id} align='left'>
-                           {details.type}
-                        </TableCell>
-                     ))}
-                  </TableRow>
-               </TableHead>
+            <Table aria-label='compare table'>
+               <TableHeader compareProducts={compareProducts} />
                <TableBody>
-                  <TableRow hover>
-                     <TableCell>GPU Órajel</TableCell>
-                     {compareProducts.map(product => (
-                        <TableCell component='td' scope='row'>
-                           {product.details.gpuBaseClock} MHz
-                        </TableCell>
-                     ))}
-                  </TableRow>
-                  {/* <TableRow hover>
-                     <TableCell component='th' scope='row'>
-                        Gyártó
-                     </TableCell>
-                     <TableCell component='td' scope='row'>
-                        teszt
-                     </TableCell>
-                  </TableRow>
-                  <TableRow hover>
-                     <TableCell component='th' scope='row'>
-                        DVI
-                     </TableCell>
-                  </TableRow>
-                  <TableRow hover>
-                     <TableCell component='th' scope='row'>
-                        Vram
-                     </TableCell>
-                  </TableRow> */}
-                  {/* {selectIdsFromCompareItems.map(row => (
-                     <TableRow hover key={row}>
-                        <TableCell component='th' scope='row'>
-                           {row}
-                        </TableCell>
-                        <TableCell align='right'>{row}</TableCell>
+                  {Object.entries(VgaDetailProperties).map(keyValuePair => (
+                     <TableRow hover>
+                        <>
+                           <TableCell>{keyValuePair[1]}</TableCell>
+                           {compareProducts.map(product => (
+                              <TableCell>{product.details[keyValuePair[0]]}</TableCell>
+                           ))}
+                        </>
                      </TableRow>
-                  ))} */}
+                  ))}
                </TableBody>
             </Table>
          </TableContainer>
