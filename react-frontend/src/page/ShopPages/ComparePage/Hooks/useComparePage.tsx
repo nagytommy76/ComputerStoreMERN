bf76704849226cc2c8f1704a619c7776612df605
nagytommy76 <nagytommy76@gmail.com>
2022-12-23 16:11:25 +0100

    Created a base hook for the ComparePage

---
+++ react-frontend/src/page/ShopPages/ComparePage/Hooks/useComparePage.tsx
@@ -0,0 +1,45 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import useGetCompare from './useGetCompare'
+import useConvertVGA from './ConvertProducts/useConvertVGA'
+
+import { HeaderTypes, VgaCompareProduct } from '../CompareTypes'
+
+const useComparePage = () => {
+   const [headerInfo, setHeaderInfo] = useState<HeaderTypes[]>([])
+   const [convertedProductDetails, setConvertedProductDetails] = useState<any[]>([])
+   const getCompareResult = useGetCompare()
+   const converVGADataToStringWithUnits = useConvertVGA()
+
+   // productDetails-hez majd VgaCompareProduct | CpuCompareProduct | SSDCompareProduct stb jön
+   const getAndSetHeaderInfo = (productDetails: VgaCompareProduct[]) => {
+      let helperArray: any = []
+      setHeaderInfo(
+         productDetails.map(product => {
+            converVGADataToStringWithUnits(helperArray, product)
+            return {
+               productID: product._id,
+               productDisplayName: `${product.manufacturer} ${product.type}`,
+               pictureUrl: product.pictureUrls[0],
+               price: product.price,
+            }
+         })
+      )
+      setConvertedProductDetails(helperArray)
+   }
+
+   const callCompareResult = useCallback(async () => {
+      const productDetails = await getCompareResult()
+      productDetails !== null && getAndSetHeaderInfo(productDetails)
+   }, [getCompareResult])
+
+   useEffect(() => {
+      callCompareResult()
+   }, [callCompareResult])
+
+   return {
+      convertedProductDetails,
+      headerInfo,
+   }
+}
+
+export default useComparePage
