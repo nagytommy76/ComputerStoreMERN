214cef6e2c4b505388fda0d8fe596df1f83c74f3
nagytommy76 <nagytommy76@gmail.com>
2022-12-23 16:12:48 +0100

    Using the useLocation

--- react-frontend/src/page/ShopPages/ComparePage/Hooks/useGetCompare.tsx
+++ react-frontend/src/page/ShopPages/ComparePage/Hooks/useGetCompare.tsx
@@ -1,13 +1,19 @@
 import React, { useCallback } from 'react'
+import { useLocation } from 'react-router-dom'
 import { axiosInstance as axios, AxiosResponse } from '../../../../AxiosSetup/AxiosInstance'
-import { BaseProductType } from '../../BaseTypes'
+import { VgaCompareProduct } from '../CompareTypes'
 
-const useGetCompare = (productType: string, selectIdsFromCompareItems: string[]) => {
+const useGetCompare = () => {
+   const {
+      state: { selectIdsFromCompareItems, productType },
+   } = useLocation() as { state: { selectIdsFromCompareItems: string[]; productType: string } }
+
    const getCompareResult = useCallback(async () => {
       try {
          const compare = (await axios.get(
             `/${productType}/compare?productId=${selectIdsFromCompareItems}`
-         )) as AxiosResponse<{ productDetails: BaseProductType[] }, any>
+         )) as AxiosResponse<{ productDetails: VgaCompareProduct[] }, any>
+         // productDetails: VgaCompareProduct[] | CpuCompare[] stb jön majd!!!
          return compare.data.productDetails
          // Létrehozni egy details statet ami már tartalmazza a magyar KEY-t és az egységekkel kibővített VALUE-kat
       } catch (error) {
