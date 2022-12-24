edba77e70567ceb578bc4a64951945a248725bed
nagytommy76 <nagytommy76@gmail.com>
2022-12-24 11:50:08 +0100

    Created a cpu detail enum

--- react-frontend/src/page/ShopPages/ComparePage/CompareTypes.ts
+++ react-frontend/src/page/ShopPages/ComparePage/CompareTypes.ts
@@ -37,3 +37,21 @@ export enum VgaDetailProperties {
    vramType = 'VRAM típusa',
    warranity = 'Garancia',
 }
+
+export enum CpuDetailProperties {
+   coreCount = 'Magok száma',
+   threadCount = 'Szálak száma',
+   baseClock = 'Alap órajel',
+   boostClock = 'Turbó órajel',
+   TDP = 'Fogyasztás',
+   architecture = 'Architektúra',
+   cpuCodeName = 'CPU kódneve',
+   integratedGraphicsName = 'Integrált GPU neve',
+   l2Cache = 'L2 cache',
+   l3Cache = 'L3 cache',
+   socket = 'Foglalat',
+   stockCooler = 'Gyári hűtő',
+   stockCoolerName = 'Gyári hűtő neve',
+   manufacturerPageUrl = 'Gyárói honlap',
+   warranity = 'Garancia',
+}
