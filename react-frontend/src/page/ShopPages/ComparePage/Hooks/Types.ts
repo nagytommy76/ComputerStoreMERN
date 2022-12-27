6eee9eae3a194307882d58e60e9031a4abfcbe39
nagytommy76 <nagytommy76@gmail.com>
2022-12-27 12:18:32 +0100

    Created a ConvertedVgaDetails type, with a [key:string] index signature

---
+++ react-frontend/src/page/ShopPages/ComparePage/Hooks/Types.ts
@@ -0,0 +1,21 @@
+export interface ConvertedVGADetailsType {
+   [key: string]: string
+   gpuBaseClock: string
+   gpuPeakClock: string
+   streamProcessors: string
+   vramBandwidth: string
+   vramCapacity: string
+   vramSpeed: string
+   vramType: string
+   minPowerSupply: string
+   powerConsuption: string
+   powerPin: string
+   pcieType: string
+   gpuManufacturer: string
+   length: string
+   DVI: string
+   HDMI: string
+   displayPort: string
+   manufacturerPageUrl: string
+   warranity: string
+}
