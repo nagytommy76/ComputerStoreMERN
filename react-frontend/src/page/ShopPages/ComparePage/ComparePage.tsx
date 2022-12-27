6354c087cbce36a39fe5b59e85a8fb43f0e65b0f
nagytommy76 <nagytommy76@gmail.com>
2022-12-27 12:44:33 +0100

    Using the new components

--- react-frontend/src/page/ShopPages/ComparePage/ComparePage.tsx
+++ react-frontend/src/page/ShopPages/ComparePage/ComparePage.tsx
@@ -1,16 +1,13 @@
 import useComparePage from './Hooks/useComparePage'
 
 import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
-import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
 
 import TableHeader from './TableHeader/TableHeader'
+import TableBodyComponent from './TableBody/TableBody'
 
 import { ComparePageStyle } from './Styles/CompareStyle'
-import { VgaDetailProperties } from './CompareTypes'
 
 const ComparePage = () => {
    const { convertedProductDetails, headerInfo } = useComparePage()
@@ -20,26 +17,7 @@ const ComparePage = () => {
          <TableContainer component={Paper}>
             <Table aria-label='compare table'>
                <TableHeader compareProducts={headerInfo} />
-               <TableBody>
-                  {Object.entries(VgaDetailProperties).map((keyValuePair, index) => (
-                     <TableRow hover key={index}>
-                        <>
-                           <TableCell>{keyValuePair[1]}</TableCell>
-                           {convertedProductDetails.map(details => {
-                              return keyValuePair[0] !== 'manufacturerPageUrl' ? (
-                                 <TableCell>{details[keyValuePair[0]]}</TableCell>
-                              ) : (
-                                 <TableCell>
-                                    <a target='_blank' href={details[keyValuePair[0]]}>
-                                       Gyártó honlap
-                                    </a>
-                                 </TableCell>
-                              )
-                           })}
-                        </>
-                     </TableRow>
-                  ))}
-               </TableBody>
+               <TableBodyComponent convertedProductDetails={convertedProductDetails} />
             </Table>
          </TableContainer>
       </ComparePageStyle>
