9601871ef17adc00f74b47f1039762f78000a572
nagytommy76 <nagytommy76@gmail.com>
2022-12-28 10:55:42 +0100

    Using a button component to open the link

--- react-frontend/src/page/ShopPages/ComparePage/TableBody/ManufacturerAnchor.tsx
+++ react-frontend/src/page/ShopPages/ComparePage/TableBody/ManufacturerAnchor.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
+import LaunchIcon from '@mui/icons-material/Launch'
 import TableCell from '@mui/material/TableCell'
+import Button from '@mui/material/Button'
+import Link from '@mui/material/Link'
 
 const ManufacturerAnchor: React.FC<{ pageURL: string }> = ({ pageURL }) => {
    return (
       <TableCell>
-         <a target='_blank' href={pageURL}>
-            Gyártó honlap
-         </a>
+         <Link underline='none' target='_blank' href={pageURL}>
+            <Button variant='text' color='info' endIcon={<LaunchIcon />}>
+               Gyártó honlapja
+            </Button>
+         </Link>
       </TableCell>
    )
 }
