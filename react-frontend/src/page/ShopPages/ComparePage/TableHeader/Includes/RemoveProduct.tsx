e5e8b8d56dd27c62c3d8e5e73fe131745c598c64
nagytommy76 <nagytommy76@gmail.com>
2022-12-28 12:58:13 +0100

    Added a remove product icon

---
+++ react-frontend/src/page/ShopPages/ComparePage/TableHeader/Includes/RemoveProduct.tsx
@@ -0,0 +1,22 @@
+import react from 'react'
+import { useAppDispatch } from '../../../../../app/hooks'
+import { removeSingleItemByID } from '../../../../../app/slices/ProductCompareSlice'
+
+import IconButton from '@mui/material/IconButton'
+import DeleteIcon from '@mui/icons-material/Delete'
+
+const RemoveProduct: React.FC<{ productID: string }> = ({ productID }) => {
+   const dispatch = useAppDispatch()
+   const handleItemDelete = () => {
+      console.log('Törlés')
+      //dispatch(removeSingleItemByID(productID))
+   }
+
+   return (
+      <IconButton onClick={handleItemDelete} color='error' aria-label='delete'>
+         <DeleteIcon />
+      </IconButton>
+   )
+}
+
+export default RemoveProduct
