ed43b7977db68b214d160936f7f48782fd6587fd
nagytommy76 <nagytommy76@gmail.com>
2022-12-30 13:28:03 +0100

    Using a helper object to diplay the correct product type

--- react-frontend/src/page/ShopPages/ComparePage/TableBody/TableBody.tsx
+++ react-frontend/src/page/ShopPages/ComparePage/TableBody/TableBody.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { VgaDetailProperties } from '../CompareTypes'
-import { ConvertedVGADetailsType } from '../Hooks/Types'
+import { VgaDetailProperties, CpuDetailProperties } from '../CompareTypes'
+import { ConvertedCPUDetailsType, ConvertedVGADetailsType } from '../Hooks/Types'
+import { useAppSelector } from '../../../../app/hooks'
 
 import { StyledTableCell } from '../Styles/TableBodyStyle'
 import TableBody from '@mui/material/TableBody'
@@ -8,15 +9,24 @@ import TableRow from '@mui/material/TableRow'
 
 import ManufacturerAnchor from './ManufacturerAnchor'
 
-const TableBodyComponent: React.FC<{ convertedProductDetails: ConvertedVGADetailsType[] }> = ({
-   convertedProductDetails,
-}) => {
+const TableBodyComponent: React.FC<{
+   convertedProductDetails: ConvertedVGADetailsType[] | ConvertedCPUDetailsType[]
+}> = ({ convertedProductDetails }) => {
+   const productType = useAppSelector((state) => state.productCompare.selectedProductsByType[0].productType)
+   const Empty: {
+      [key: string]: any
+      vga: typeof VgaDetailProperties
+      cpu: typeof CpuDetailProperties
+   } = {
+      vga: VgaDetailProperties,
+      cpu: CpuDetailProperties,
+   }
    return (
       <TableBody>
-         {Object.entries(VgaDetailProperties).map((keyValuePair, index) => (
+         {Object.entries(Empty[productType]).map((keyValuePair, index) => (
             <TableRow hover key={index}>
                <>
-                  <StyledTableCell>{keyValuePair[1]}</StyledTableCell>
+                  <StyledTableCell>{keyValuePair[1] as string}</StyledTableCell>
                   {convertedProductDetails.map((details, index) => {
                      return keyValuePair[0] !== 'manufacturerPageUrl' ? (
                         <StyledTableCell key={index}>{details[keyValuePair[0]]}</StyledTableCell>
