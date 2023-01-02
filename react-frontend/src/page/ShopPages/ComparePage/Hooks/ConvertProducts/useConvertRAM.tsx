36e88e0ddc63d28aed5df31b762e3216107b3e8d
nagytommy76 <nagytommy76@gmail.com>
2023-01-02 11:31:25 +0100

    Created the Ram converter hook

---
+++ react-frontend/src/page/ShopPages/ComparePage/Hooks/ConvertProducts/useConvertRAM.tsx
@@ -0,0 +1,41 @@
+import { RamCompareProduct } from '../../CompareTypes'
+import { ConvertedRAMDetailsType } from '../Types'
+import { RamDetailProperties } from '../../Enums'
+
+const useConvertRAM = () => {
+   const converCPUDataToStringWithUnits = (
+      helperArray: ConvertedRAMDetailsType[],
+      product: RamCompareProduct
+   ) => {
+      const keyValuePair = Object.entries(RamDetailProperties)
+      let obj: any = {}
+      for (const [key] of keyValuePair) {
+         switch (key) {
+            case 'capacity':
+               obj[key] = `${product.details[key]} GB`
+               break
+            case 'frequency':
+               obj[key] = `${product.details[key]} MHz`
+               break
+            case 'warranity':
+               obj[key] = `${product.details[key]} hónap`
+               break
+            case 'moduleNumber':
+               obj[key] = `${product.details[key]} DB`
+               break
+            case 'voltage':
+               obj[key] = `${product.details[key]} Volt`
+               break
+            case 'warranity':
+               obj[key] = `${product.details[key]} hónap`
+               break
+            default:
+               obj[key] = product.details[key as 'memoryType' | 'manufacturerPageUrl' | 'latency']
+         }
+      }
+      helperArray.push(obj)
+   }
+   return converCPUDataToStringWithUnits
+}
+
+export default useConvertRAM
