47018a86a03267009aae4c9538c0c7af86225f28
nagytommy76 <nagytommy76@gmail.com>
2023-01-02 11:07:15 +0100

    Moved the detail enums to a separated file

--- react-frontend/src/page/ShopPages/ComparePage/CompareTypes.ts
+++ react-frontend/src/page/ShopPages/ComparePage/CompareTypes.ts
@@ -1,5 +1,6 @@
 import { VgaDetailType } from '../Vga/VgaTypes'
 import { CpuDetailsType } from '../Cpu/CpuTypes'
+import { MemoryDetailsType } from '../Memory/MemoryTypes'
 
 interface BaseCompare {
    _id: string
@@ -23,42 +24,6 @@ export interface VgaCompareProduct extends BaseCompare {
 export interface CpuCompareProduct extends BaseCompare {
    details: CpuDetailsType
 }
-
-export enum VgaDetailProperties {
-   gpuBaseClock = 'Alap GPU Órajel',
-   gpuPeakClock = 'Emelt GPU órajel',
-   streamProcessors = 'Stream processzorok',
-   vramBandwidth = 'VRAM sávszélesség',
-   vramCapacity = 'VRAM mennyiség',
-   vramSpeed = 'VRAM sebesség',
-   vramType = 'VRAM típusa',
-   minPowerSupply = 'Minimum ajánlott tápegység',
-   powerConsuption = 'Fogyasztás (TDP)',
-   powerPin = 'Tápcsatlakozók',
-   pcieType = 'PCI-E típusa',
-   gpuManufacturer = 'GPU gyártó',
-   length = 'Hosszúság',
-   DVI = 'DVI',
-   HDMI = 'HDMI',
-   displayPort = 'Display Portok szána',
-   manufacturerPageUrl = 'Gyártói honlap',
-   warranity = 'Garancia',
-}
-
-export enum CpuDetailProperties {
-   coreCount = 'Magok száma',
-   threadCount = 'Szálak száma',
-   baseClock = 'Alap órajel',
-   boostClock = 'Turbó órajel',
-   TDP = 'Fogyasztás',
-   architecture = 'Architektúra',
-   cpuCodeName = 'CPU kódneve',
-   integratedGraphicsName = 'Integrált GPU neve',
-   l2Cache = 'L2 cache',
-   l3Cache = 'L3 cache',
-   socket = 'Foglalat',
-   stockCooler = 'Gyári hűtő',
-   stockCoolerName = 'Gyári hűtő neve',
-   manufacturerPageUrl = 'Gyárói honlap',
-   warranity = 'Garancia',
+export interface RamCompareProduct extends BaseCompare {
+   details: MemoryDetailsType
 }
