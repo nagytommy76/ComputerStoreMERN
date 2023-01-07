9e523bf2bfa7552f568162607156139c3433ebc4
nagytommy76 <nagytommy76@gmail.com>
2023-01-07 14:47:29 +0100

    Added the new backend api's url

--- react-frontend/src/AxiosSetup/AxiosInstance.tsx
+++ react-frontend/src/AxiosSetup/AxiosInstance.tsx
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 const DEVELOPMENT_URL = 'http://localhost:3000'
-// const PRODUCTION_URL = 'https://computerstorebackend.firebaseapp.com'
+//const PRODUCTION_URL = 'https://computerstorebackend.firebaseapp.com'
 
 const isAxiosError = axios.isAxiosError
 const axiosInstance = axios.create({
-   // baseURL: 'https://computerstoredeployedbackend-production.up.railway.app/api',
+   //baseURL: 'https://node-api-q6rm.onrender.com/api',
    // docker port
    // baseURL: 'http://localhost:5040/api',
    baseURL: 'http://localhost:5050/api',
@@ -13,7 +13,7 @@ const axiosInstance = axios.create({
    headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': DEVELOPMENT_URL,
-      // 'Access-Control-Allow-Origin': PRODUCTION_URL,
+      //'Access-Control-Allow-Origin': PRODUCTION_URL,
       'Access-Control-Allow-Credentials': true,
    },
 })
