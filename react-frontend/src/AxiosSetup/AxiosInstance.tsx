0c8d29ae65ee16ed771a867a18285a914a51c995
nagytommy76 <nagytommy76@gmail.com>
2025-10-13 19:00:44 +0200

    Add production backend URL

--- react-frontend/src/AxiosSetup/AxiosInstance.tsx
+++ react-frontend/src/AxiosSetup/AxiosInstance.tsx
@@ -1,20 +1,16 @@
 import axios from 'axios'
 
 const DEVELOPMENT_URL = 'http://localhost:3000'
-//const PRODUCTION_URL = 'https://computerstorebackend.firebaseapp.com'
+const PRODUCTION_URL = 'https://computerstore.up.railway.app/api'
 
 const isAxiosError = axios.isAxiosError
 const axiosInstance = axios.create({
-   //baseURL: 'https://node-api-q6rm.onrender.com/api',
    // docker port
-   // baseURL: 'http://localhost:5040/api',
-   baseURL: 'http://localhost:5050/api',
+   // baseURL: DEVELOPMENT_URL,
+   baseURL: PRODUCTION_URL,
    withCredentials: true,
    headers: {
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': DEVELOPMENT_URL,
-      //'Access-Control-Allow-Origin': PRODUCTION_URL,
-      'Access-Control-Allow-Credentials': true,
    },
 })
 
