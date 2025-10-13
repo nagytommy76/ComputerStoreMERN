e17f2b985ee9905b65422ff2c76c9d1002270457
nagytommy76 <nagytommy76@gmail.com>
2025-10-13 19:42:15 +0200

    Exclude .test files

--- react-frontend/vite.config.ts
+++ react-frontend/vite.config.ts
@@ -6,15 +6,20 @@ import svgrPlugin from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
-   build: { outDir: 'build' },
+   build: {
+      outDir: 'build',
+      rollupOptions: {
+         external: [/.*\.test\.tsx?$/, /.*\.spec\.tsx?$/],
+      },
+   },
    // Amikor Dockert használok OPEN: FALSE -> nincs hiba
    server: {
       port: 3000,
       open: true,
       // DOCKER-hez
-      watch: {
-         usePolling: false,
-      },
+      // watch: {
+      //    usePolling: false,
+      // },
       // host: true, // needed for the Docker Container port mapping to work
       // strictPort: true,
    },
